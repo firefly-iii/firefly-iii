8f2ba84ccc442d6dcf102c9575d49357d68216ed
Sander Dorigo <sander.dorigo@sycomm.nl>
2014-06-30 13:22:47 +0200

    Small changes to ignore files, and show environment in guest.blade

--- app/views/layouts/guest.blade.php
+++ app/views/layouts/guest.blade.php
@@ -22,6 +22,7 @@
     @include('partials.flashes')
     @yield('content')
 </div>
+<!-- {{App::environment()}} -->
 
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
