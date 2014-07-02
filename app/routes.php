d2f751b8668fcce00ddbe906970cb5f1a9e17479
Sander Dorigo <s@nder.be>
2014-07-02 21:22:13 +0200

    Updated some controllers and routes to exclude not yet finished stuff.

--- app/routes.php
+++ app/routes.php
@@ -5,10 +5,10 @@ Route::get('/', ['uses' => 'HomeController@index','as' => 'index','before' => 'a
 Route::get('/start', ['uses' => 'HomeController@start','as' => 'start','before' => 'auth']);
 
 // migration controller:
-Route::get('/migrate/index', ['uses' => 'MigrationController@index','as' => 'migrate.index', 'before' => 'auth']);
-Route::get('/migrate/select-user', ['uses' => 'MigrationController@selectUser','as' => 'migrate.select-user', 'before' => 'auth']);
-Route::post('/migrate/select-user', ['uses' => 'MigrationController@postSelectUser','before' => 'csrf|auth']);
-Route::get('/migrate/migrate/{userID}', ['uses' => 'MigrationController@migrate','as' => 'migrate.migrate', 'before' => 'auth']);
+//Route::get('/migrate/index', ['uses' => 'MigrationController@index','as' => 'migrate.index', 'before' => 'auth']);
+//Route::get('/migrate/select-user', ['uses' => 'MigrationController@selectUser','as' => 'migrate.select-user', 'before' => 'auth']);
+//Route::post('/migrate/select-user', ['uses' => 'MigrationController@postSelectUser','before' => 'csrf|auth']);
+//Route::get('/migrate/migrate/{userID}', ['uses' => 'MigrationController@migrate','as' => 'migrate.migrate', 'before' => 'auth']);
 
 // login, register, logout:
 Route::get('/login',['uses' => 'UserController@login','as' => 'login','before' => 'guest']);
