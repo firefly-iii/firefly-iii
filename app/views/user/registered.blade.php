b16444705a14e3a8bb1d6a0d3664819e47fd1d70
Sander Dorigo <s@nder.be>
2014-07-03 08:50:21 +0200

    Finished testing user controller

--- app/views/user/registered.blade.php
+++ app/views/user/registered.blade.php
@@ -3,7 +3,7 @@
 <div class="row">
     <div class="col-lg-12 col-md-12 col-sm-12">
         <h1>Firefly<br/>
-            <small>Registered!</small>
+            <small>Password sent!</small>
         </h1>
     </div>
 </div>
