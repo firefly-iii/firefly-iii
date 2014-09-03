b1e5913c7854251db85119f1c5aab61017340e6d
Sander Dorigo <s@nder.be>
2014-09-03 21:12:51 +0200

    Some tests fixed. But messy! [skip ci]

--- app/tests/CategoryTest.php
+++ app/tests/CategoryTest.php
@@ -17,93 +17,145 @@ class CategoryTest extends TestCase
     protected $_user;
     protected $_category;
 
+    /**
+     *
+     */
     public function setUp()
     {
         parent::setUp();
         Artisan::call('migrate');
         Artisan::call('db:seed');
         $this->_repository = $this->mock('Firefly\Storage\Category\CategoryRepositoryInterface');
-        $this->_category = $this->mock('Firefly\Helper\Controllers\CategoryInterface');
-        $this->_user = m::mock('User', 'Eloquent');
+        $this->_category   = $this->mock('Firefly\Helper\Controllers\CategoryInterface');
+        $this->_user       = m::mock('User', 'Eloquent');
 
     }
 
+    /**
+     *
+     */
     public function tearDown()
     {
         Mockery::close();
     }
 
+
+    /**
+     * @covers \CategoryController::create
+     */
     public function testCreate()
     {
+        // test the view:
+        View::shouldReceive('make')->with('categories.create')->once()->andReturn(m::self())
+            ->shouldReceive('with')->with('title', 'Create a new category')->once();
+
         $this->action('GET', 'CategoryController@create');
         $this->assertResponseOk();
 
     }
 
+    /**
+     * @covers \CategoryController::delete
+     */
     public function testDelete()
     {
 
         $category = f::create('Category');
 
-        // for successful binding:
-        Auth::shouldReceive('user')->andReturn($this->_user);
-        Auth::shouldReceive('check')->andReturn(true);
+        // test the view:
+        View::shouldReceive('make')->with('categories.delete')->once()->andReturn(m::self())
+            ->shouldReceive('with')->with('category', m::any())->once()->andReturn(m::self())
+            ->shouldReceive('with')->with('title', 'Delete category "' . $category->name . '"')->once();
+
+        // for successful binding with the category to delete:
+        Auth::shouldReceive('user')->andReturn($this->_user)->between(1, 3);
+        Auth::shouldReceive('check')->andReturn(true)->between(1, 2);
         $this->_user->shouldReceive('getAttribute')->with('id')->once()->andReturn($category->user_id);
-        $this->_user->shouldReceive('getAttribute')->with('email')->once()->andReturn('some@email');
+        $this->_user->shouldReceive('getAttribute')->with('email')->andReturn('some@email');
 
         $this->action('GET', 'CategoryController@delete', $category->id);
         $this->assertResponseOk();
     }
 
+    /**
+     * @covers \CategoryController::destroy
+     */
     public function testDestroy()
     {
         $category = f::create('Category');
 
-        // for successful binding:
-        Auth::shouldReceive('user')->andReturn($this->_user);
-        Auth::shouldReceive('check')->andReturn(true);
+        // for successful binding with the category to delete:
+        Auth::shouldReceive('user')->andReturn($this->_user)->between(1, 3);
+        Auth::shouldReceive('check')->andReturn(true)->between(1, 2);
         $this->_user->shouldReceive('getAttribute')->with('id')->once()->andReturn($category->user_id);
+        $this->_user->shouldReceive('getAttribute')->with('email')->andReturn('some@email');
+
+        // fire the repository:
         $this->_repository->shouldReceive('destroy')->once()->andReturn(true);
 
+        // fire and test:
         $this->action('POST', 'CategoryController@destroy', $category->id);
         $this->assertRedirectedToRoute('categories.index');
         $this->assertSessionHas('success');
     }
 
+    /**
+     * @covers \CategoryController::edit
+     */
     public function testEdit()
     {
         $category = f::create('Category');
 
-        // for successful binding.
-        Auth::shouldReceive('user')->andReturn($this->_user);
-        Auth::shouldReceive('check')->andReturn(true);
+        // for successful binding with the category to edit:
+        Auth::shouldReceive('user')->andReturn($this->_user)->between(1, 3);
+        Auth::shouldReceive('check')->andReturn(true)->between(1, 2);
         $this->_user->shouldReceive('getAttribute')->with('id')->once()->andReturn($category->user_id);
-        $this->_user->shouldReceive('getAttribute')->with('email')->once()->andReturn('some@email');
+        $this->_user->shouldReceive('getAttribute')->with('email')->andReturn('some@email'); //
+
+        // test the view:
+        View::shouldReceive('make')->with('categories.edit')->once()->andReturn(m::self())
+            ->shouldReceive('with')->with('category', m::any())->once()->andReturn(m::self())
+            ->shouldReceive('with')->with('title', 'Edit category "' . $category->name . '"')->once();
+
 
         $this->action('GET', 'CategoryController@edit', $category->id);
         $this->assertResponseOk();
     }
 
+    /**
+     * @covers \CategoryController::index
+     */
     public function testIndex()
     {
-        $category = f::create('Category');
+        $category   = f::create('Category');
         $collection = new Collection();
         $collection->add($category);
 
         $this->_repository->shouldReceive('get')->with()->once()->andReturn($collection);
+
+        View::shouldReceive('make')->with('categories.index')->once()->andReturn(m::self())
+            ->shouldReceive('with')->with('categories', $collection)->once()->andReturn(m::self())
+            ->shouldReceive('with')->with('title', 'All your categories')->once();
+
+
+
         $this->action('GET', 'CategoryController@index');
         $this->assertResponseOk();
     }
 
+    /**
+     * @covers \CategoryController::show
+     */
     public function testShow()
     {
         $category = f::create('Category');
 
-        // for successful binding.
-        Auth::shouldReceive('user')->andReturn($this->_user);
-        Auth::shouldReceive('check')->andReturn(true);
+        // for successful binding with the category to show:
+        Auth::shouldReceive('user')->andReturn($this->_user)->between(1, 3);
+        Auth::shouldReceive('check')->andReturn(true)->between(1, 2);
         $this->_user->shouldReceive('getAttribute')->with('id')->once()->andReturn($category->user_id);
-        $this->_user->shouldReceive('getAttribute')->with('email')->once()->andReturn($category->email);
+        $this->_user->shouldReceive('getAttribute')->with('email')->andReturn('some@email'); //
+
         $this->session(['start' => new Carbon, 'end' => new Carbon]);
 
 
