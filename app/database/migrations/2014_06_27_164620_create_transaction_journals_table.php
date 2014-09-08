eacbd038b72840b51f161116ea7ac4c18a0f88e6
James Cole <thegrumpydictator@gmail.com>
2014-09-08 10:38:14 +0200

    Updated migration to include recurring transactions.

--- app/database/migrations/2014_06_27_164620_create_transaction_journals_table.php
+++ app/database/migrations/2014_06_27_164620_create_transaction_journals_table.php
@@ -24,6 +24,7 @@ class CreateTransactionJournalsTable extends Migration
                 $table->timestamps();
                 $table->integer('user_id')->unsigned();
                 $table->integer('transaction_type_id')->unsigned();
+                $table->integer('recurring_transaction_id')->unsigned()->nullable();
                 $table->integer('transaction_currency_id')->unsigned();
                 $table->string('description', 255)->nullable();
                 $table->boolean('completed');
@@ -33,6 +34,11 @@ class CreateTransactionJournalsTable extends Migration
                 $table->foreign('transaction_type_id')
                     ->references('id')->on('transaction_types')
                     ->onDelete('cascade');
+
+                // connect transaction journals to recurring transactions
+                $table->foreign('recurring_transaction_id')
+                    ->references('id')->on('recurring_transactions')
+                    ->onDelete('set null');
 
                 // connect transaction journals to transaction currencies
                 $table->foreign('transaction_currency_id')
