f3b9759eb77117f9291159761e28c93465859cac
Sander Dorigo <s@nder.be>
2014-09-09 10:42:58 +0200

    Cleaned up the chart controller.

--- app/lib/Firefly/Helper/Controllers/ChartInterface.php
+++ app/lib/Firefly/Helper/Controllers/ChartInterface.php
@@ -85,7 +85,30 @@ interface ChartInterface
      */
     public function transactionsByJournals(array $set);
 
+    /**
+     * Get all limit (LimitRepetitions) for a budget falling in a certain date range.
+     *
+     * @param \Budget $budget
+     * @param Carbon $start
+     * @param Carbon $end
+     *
+     * @return Collection
+     */
+    public function limitsInRange(\Budget $budget, Carbon $start, Carbon $end);
 
+
+    /**
+     * We check how much money has been spend on the limitrepetition (aka: the current envelope) in the period denoted.
+     * Aka, we have a certain amount of money in an envelope and we wish to know how much we've spent between the dates
+     * entered. This can be a partial match with the date range of the envelope or no match at all.
+     *
+     * @param \LimitRepetition $repetition
+     * @param Carbon           $start
+     * @param Carbon           $end
+     *
+     * @return mixed
+     */
+    public function spentOnLimitRepetitionBetweenDates(\LimitRepetition $repetition, Carbon $start, Carbon $end);
 
 
 }
