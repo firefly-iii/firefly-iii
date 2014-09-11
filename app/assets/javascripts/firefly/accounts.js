9bcd27b847fe6120e920a819738ee15d0c15e2ae
James Cole <thegrumpydictator@gmail.com>
2014-09-11 21:58:08 +0200

    Cleanup and improve charts.

--- app/assets/javascripts/firefly/accounts.js
+++ app/assets/javascripts/firefly/accounts.js
@@ -12,42 +12,35 @@ if($('#chart').length == 1) {
 
             series: data.series,
             title: {
-                text: data.chart_title
+                text: null
             },
             yAxis: {
-                formatter: function () {
-                    return '$' + Highcharts.numberFormat(this.y, 0);
-                }
-            },
-            subtitle: {
-                text: data.subtitle,
-                useHTML: true
-            },
+                allowDecimals: false,
+                labels: {
+                    formatter: function () {
+                        if(this.value >= 1000 || this.value <= -1000) {
+                            return '\u20AC ' + (this.value / 1000) + 'k';
+                        }
+                        return '\u20AC ' + this.value;
 
+                    }
+                },
+                title: {text: null}
+            },
             xAxis: {
-                floor: 0,
                 type: 'datetime',
                 dateTimeLabelFormats: {
                     day: '%e %b',
-                    year: '%b'
+                    week: '%e %b'
                 },
                 title: {
-                    text: 'Date'
+                    text: null
                 }
             },
+            legend: {enabled:false},
             tooltip: {
-                shared: true,
-                crosshairs: false,
                 formatter: function () {
-                    var str = '<span style="font-size:80%;">' + Highcharts.dateFormat("%A, %e %B", this.x) + '</span><br />';
-                    for (x in this.points) {
-                        var point = this.points[x];
-                        var colour = point.point.pointAttr[''].fill;
-                        str += '<span style="color:' + colour + '">' + point.series.name + '</span>: \u20AC ' + Highcharts.numberFormat(point.y, 2) + '<br />';
-                    }
-                    //console.log();
-                    return str;
-                    return '<span style="font-size:80%;">' + this.series.name + ' on ' + Highcharts.dateFormat("%e %B", this.x) + ':</span><br /> \u20AC ' + Highcharts.numberFormat(this.y, 2);
+                    return this.series.name + ': \u20AC ' + Highcharts.numberFormat(this.y,2);
                 }
             },
             plotOptions: {
@@ -60,23 +53,12 @@ if($('#chart').length == 1) {
                     threshold: 0,
                     lineWidth: 1,
                     marker: {
-                        radius: 2
+                        radius: 0
                     },
                     point: {
                         events: {
                             click: function (e) {
-                                hs.htmlExpand(null, {
-                                        src: 'chart/home/info/' + this.series.name + '/' + Highcharts.dateFormat("%d/%m/%Y", this.x),
-                                        pageOrigin: {
-                                            x: e.pageX,
-                                            y: e.pageY
-                                        },
-                                        objectType: 'ajax',
-                                        headingText: '<a href="#">' + this.series.name + '</a>',
-                                        width: 250
-                                    }
-                                )
-                                ;
+                                alert('click!');
                             }
                         }
                     }
