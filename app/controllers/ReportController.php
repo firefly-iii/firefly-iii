78e80530d3f48f0f9b792191336b5b8e5991bba9
James Cole <thegrumpydictator@gmail.com>
2014-09-27 07:06:19 +0200

    Place holder for reports.

--- app/controllers/ReportController.php
+++ app/controllers/ReportController.php
@@ -12,7 +12,7 @@ class ReportController extends BaseController
      */
     public function index()
     {
-
+        return View::make('reports.index')->with('title','Reports')->with('mainTitleIcon','fa-line-chart');
     }
 
 }
