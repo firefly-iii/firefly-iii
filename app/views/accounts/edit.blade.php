55a0d4da00e0d100142e984fd50823182424ef74
Sander Dorigo <thegrumpydictator@gmail.com>
2014-10-28 05:59:14 +0100

    New views and layouts for the account controller.

--- app/views/accounts/edit.blade.php
+++ app/views/accounts/edit.blade.php
@@ -11,79 +11,46 @@
 {{Form::model($account, ['class' => 'form-horizontal','url' => route('accounts.update',$account->id)])}}
 <div class="row">
     <div class="col-lg-6 col-md-6 col-sm-12">
-        <h4>Mandatory fields</h4>
-
-        <div class="form-group">
-            {{ Form::label('name', 'Account name', ['class' => 'col-sm-4 control-label'])}}
-            <div class="col-sm-8">
-                {{ Form::text('name', Input::old('name'), ['class' => 'form-control']) }}
-                @if($errors->has('name'))
-                <p class="text-danger">{{$errors->first('name')}}</p>
-                @else
-                <span
-                    class="help-block">Use something descriptive such as "checking account" or "Albert Heijn".</span>
-                @endif
-
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                <i class="fa {{{$subTitleIcon}}}"></i> Mandatory fields
+            </div>
+            <div class="panel-body">
+                {{Form::ffText('name')}}
             </div>
         </div>
-
+        <p>
+            <button type="submit" class="btn btn-lg btn-success">
+                Update account
+            </button>
+        </p>
     </div>
     <div class="col-lg-6 col-md-6 col-sm-12">
-        @if($account->accounttype->type == 'Default account' || $account->accounttype->type == 'Asset account')
-        <h4>Optional fields</h4>
-
-        <div class="form-group">
-            {{ Form::label('openingbalance', 'Opening balance', ['class' => 'col-sm-4 control-label'])}}
-            <div class="col-sm-8">
-                <div class="input-group">
-                    <span class="input-group-addon">&euro;</span>
-                    @if(!is_null($openingBalance))
-                        {{Form::input('number','openingbalance', Input::old('openingbalance') ?: $openingBalance->transactions[1]->amount, ['step' => 'any', 'class' => 'form-control'])}}
-                    @else
-                        {{Form::input('number','openingbalance', Input::old('openingbalance'), ['step' => 'any', 'class' => 'form-control'])}}
-                    @endif
-
-                </div>
-
-                @if($errors->has('openingbalance'))
-                <p class="text-danger">{{$errors->first('openingbalance')}}</p>
-                @else
-                <span class="help-block">What's the current balance of this new account?</span>
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <i class="fa fa-smile-o"></i> Optional fields
+            </div>
+            <div class="panel-body">
+                {{Form::ffCheckbox('active','1')}}
+                @if($account->accounttype->type == 'Default account' || $account->accounttype->type == 'Asset account')
+                {{Form::ffBalance('openingbalance')}}
+                {{Form::ffDate('openingbalancedate')}}
                 @endif
             </div>
         </div>
-        <div class="form-group">
-            {{ Form::label('openingbalancedate', 'Opening balance date', ['class' => 'col-sm-4 control-label'])}}
-            <div class="col-sm-8">
-                @if(!is_null($openingBalance))
-                    {{ Form::input('date','openingbalancedate', Input::old('openingbalancedate') ?: $openingBalance->date->format('Y-m-d'), ['class' => 'form-control']) }}
-                @else
-                    {{ Form::input('date','openingbalancedate', Input::old('openingbalancedate') ?: '', ['class' => 'form-control']) }}
-                @endif
-                @if($errors->has('openingbalancedate'))
-                <p class="text-danger">{{$errors->first('openingbalancedate')}}</p>
-                @else
-                <span class="help-block">When was this the balance of the new account? Since your bank statements may lag behind, update this date to match the date of the last known balance of the account.</span>
-                @endif
+
+        <!-- panel for options -->
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <i class="fa fa-bolt"></i> Options
+            </div>
+            <div class="panel-body">
+                {{Form::ffOptionsList('create','account')}}
             </div>
         </div>
-        @endif
 
     </div>
-
-
 </div>
-
-<div class="row">
-    <div class="col-lg-6">
-        <div class="form-group">
-            <div class="col-sm-offset-4 col-sm-8">
-                <button type="submit" class="btn btn-default btn-success">Update {{{$account->name}}}</button>
-            </div>
-        </div>
-    </div>
-</div>
-
 
 {{Form::close()}}
 @stop
--- app/views/accounts/show.blade.php
+++ app/views/accounts/show.blade.php
@@ -4,68 +4,86 @@
     <div class="col-lg-8 col-md-6 col-sm-12">
         <div class="panel panel-default">
             <div class="panel-heading">
-                <i class="fa {{$subTitleIcon}} fa-fw"></i> {{{$account->name}}}
+                <i class="fa fa-fw {{$subTitleIcon}} fa-fw"></i> {{{$account->name}}}
             </div>
             <div class="panel-body">
-                <div id="chart"></div>
+                <div id="accountTransactionsTable"></div>
             </div>
         </div>
     </div>
     <div class="col-lg-4 col-md-6 col-sm-12">
         <!-- time based navigation -->
         @include('partials.date_nav')
+
+        <!-- summary of the selected period -->
+        <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <i class="fa fa-fw fa-align-justify"></i> Summary
+                    </div>
+                    <div class="panel-body">
+                    {{--
+                        <table class="table table-striped table-condensed">
+                            <tr>
+                                <th></th>
+                                <th>Expense / income</th>
+                                <th>Transfers</th>
+                            </tr>
+                            <tr>
+                                <td>Out</td>
+                                <td>
+                                    {{mf($show['statistics']['period']['out'])}}
+                                    <a href="{{route('accounts.show',$account->id)}}?type=transactions&amp;show=expenses"><span class="glyphicon glyphicon-circle-arrow-right"></span></a>
+                                </td>
+                                <td>
+                                    {{mf($show['statistics']['period']['t_out'])}}
+                                    <a href="{{route('accounts.show',$account->id)}}?type=transfers&amp;show=out"><span class="glyphicon glyphicon-circle-arrow-right"></span></a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>In</td>
+                                <td>
+                                    {{mf($show['statistics']['period']['in'])}}
+                                    <a href="{{route('accounts.show',$account->id)}}?type=transactions&amp;show=income"><span class="glyphicon glyphicon-circle-arrow-right"></span></a>
+                                </td>
+                                <td>
+                                    {{mf($show['statistics']['period']['t_in'])}}
+                                    <a href="{{route('accounts.show',$account->id)}}?type=transfers&amp;show=in"><span class="glyphicon glyphicon-circle-arrow-right"></span></a>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Difference</td>
+                                <td>{{mf($show['statistics']['period']['diff'])}}</td>
+                                <td>{{mf($show['statistics']['period']['t_diff'])}}</td>
+                            </tr>
+                        </table>
+                        --}}
+                    </div>
+                </div>
     </div>
 </div>
 <div class="row">
-    <div class="col-lg-6 col-md-6 col-sm-12">
+    <div class="col-lg-12 col-md-12 col-sm-12">
         <div class="panel panel-default">
             <div class="panel-heading">
-                Summary
+                <i class="fa fa-repeat fa-fw"></i> Transaction
             </div>
             <div class="panel-body">
-                <table class="table table-striped table-condensed">
-                    <tr>
-                        <th></th>
-                        <th>Expense / income</th>
-                        <th>Transfers</th>
-                    </tr>
-                    <tr>
-                        <td>Out</td>
-                        <td>
-                            {{mf($show['statistics']['period']['out'])}}
-                            <a href="{{route('accounts.show',$account->id)}}?type=transactions&amp;show=expenses"><span class="glyphicon glyphicon-circle-arrow-right"></span></a>
-                        </td>
-                        <td>
-                            {{mf($show['statistics']['period']['t_out'])}}
-                            <a href="{{route('accounts.show',$account->id)}}?type=transfers&amp;show=out"><span class="glyphicon glyphicon-circle-arrow-right"></span></a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>In</td>
-                        <td>
-                            {{mf($show['statistics']['period']['in'])}}
-                            <a href="{{route('accounts.show',$account->id)}}?type=transactions&amp;show=income"><span class="glyphicon glyphicon-circle-arrow-right"></span></a>
-                        </td>
-                        <td>
-                            {{mf($show['statistics']['period']['t_in'])}}
-                            <a href="{{route('accounts.show',$account->id)}}?type=transfers&amp;show=in"><span class="glyphicon glyphicon-circle-arrow-right"></span></a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Difference</td>
-                        <td>{{mf($show['statistics']['period']['diff'])}}</td>
-                        <td>{{mf($show['statistics']['period']['t_diff'])}}</td>
-                    </tr>
-                </table>
-            </div>
-        </div>
-    </div>
-    <div class="col-lg-6 col-md-6 col-sm-12">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                Related
-            </div>
-            <div class="panel-body">
+
+            <table id="transactionByAccountTable" class="table table-striped table-bordered" >
+            <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Description</th>
+                    <th>Amount (&euro;)</th>
+                    <th>From</th>
+                    <th>To</th>
+                    <th>Budget / category</th>
+                    <th>ID</th>
+                </tr>
+            </thead>
+            </table>
+
+            {{--
                 <table class="table table-striped table-condensed">
                     @if(count($show['statistics']['accounts']) > 0)
                     <tr>
@@ -98,30 +116,49 @@
                     </tr>
                     @endif
                 </table>
+                --}}
             </div>
         </div>
     </div>
 </div>
 
 <div class="row">
+<div class="col-lg-12 col-md-12 col-sm-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <i class="fa fa-repeat fa-fw"></i> Transaction
+            </div>
+            <div class="panel-body">
+
+            </div>
+        </div>
+    </div>
+</div>
+
+{{--
+<div class="row">
     <div class="col-lg-12 col-md-12 col-sm-12">
-
-
         <h4>Transactions <small> For selected account and period</small></h4>
         @include('paginated.transactions',['journals' => $show['journals'],'sum' => true])
     </div>
 </div>
-
+--}}
 @stop
 
 @section('styles')
 {{HTML::style('assets/stylesheets/highslide/highslide.css')}}
+{{HTML::style('assets/stylesheets/datatables/dataTables.bootstrap.css')}}
 @stop
 
 @section('scripts')
 <script type="text/javascript">
-    var accountID = {{$account->id}};
+    var accountID = {{{$account->id}}};
+    var URL = 'bla bla bla';
+    var container = 'transactionByAccountTable';
 </script>
+{{HTML::script('assets/javascript/datatables/jquery.dataTables.min.js')}}
+{{HTML::script('assets/javascript/datatables/dataTables.bootstrap.js')}}
+{{HTML::script('assets/javascript/datatables/transactions.js')}}
 {{HTML::script('assets/javascript/highcharts/highcharts.js')}}
 {{HTML::script('assets/javascript/firefly/accounts.js')}}
 @stop
