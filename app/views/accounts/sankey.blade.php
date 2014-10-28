e5f8db78f9972aa522093b8c472dbc8d8dafcd6f
Sander Dorigo <thegrumpydictator@gmail.com>
2014-10-28 05:59:14 +0100

    New views and layouts for the account controller.

---
+++ app/views/accounts/sankey.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.default')
+@section('content')
+
+
+<div id="sankey_multiple" style="width: 900px; height: 400px;"></div>
+
+
+</body>
+</html>
+@stop
+@section('scripts')
+<script type="text/javascript" src="https://www.google.com/jsapi?autoload={'modules':[{'name':'visualization','version':'1.1','packages':['sankey']}]}">
+</script>
+<script type="text/javascript">
+google.setOnLoadCallback(drawChart);
+   function drawChart() {
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'From');
+    data.addColumn('string', 'To');
+    data.addColumn('number', 'Weight');
+    data.addRows([
+    <?php $c = 0;?>
+        @foreach($filtered as $index => $entry)
+        [ '{{{$entry['from']}}}', '{{{$entry['to']}}}', {{{$entry['amount']}}} ], // {{$c}}
+        <?php  $c++ ?>
+        @endforeach
+
+    ]);
+
+    // Set chart options
+    var options = {
+        width: 600,
+
+        sankey: {
+                link: { color: { fill: '#9fa8da', fillOpacity: 0.8 } },
+                node: { color: { fill: '#a61d4c' },
+                        label: { color: '#871b47' } }
+              }
+    };
+
+    // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.Sankey(document.getElementById('sankey_multiple'));
+    chart.draw(data, options);
+   }
+</script>
+@stop
--- public/assets/javascript/firefly/accounts.js
+++ public/assets/javascript/firefly/accounts.js
@@ -1,12 +1,72 @@
 $(function () {
-if($('#chart').length == 1) {
-    /**
-     * get data from controller for home charts:
-     */
+    if ($('#accountTable').length == 1) {
+        drawDatatable();
+    }
+    //if ($('#accountTransactionsTable').length == 1) {
+    //    drawTransactionsForAccount();
+    //}
+    if ($('#transactionByAccountTable').length == 1) {
+        renderTransactionsFromURL(URL, container);
+    }
+});
+
+function drawDatatable() {
+    var opt = {
+        serverSide: true,
+        ajax: URL,
+        paging: true,
+        processing: true,
+        columns: [
+            {
+                name: 'name',
+                data: 'name',
+                searchable: true,
+                render: function (data) {
+                    return '<a href="' + data.url + '">' + data.name + '</a>';
+                }
+
+            },
+            {
+                name: 'balance',
+                data: 'balance',
+                title: 'Amount (\u20AC)',
+                searchable: false,
+                sortable: true,
+                render: function (data) {
+                    var amount = parseInt(data);
+                    if (amount < 0) {
+                        '<span class="text-danger">\u20AC ' + data.toFixed(2) + '</span>'
+                    }
+                    if (amount > 0) {
+                        '<span class="text-info">\u20AC ' + data.toFixed(2) + '</span>'
+                    }
+                    return '<span class="text-info">\u20AC ' + data.toFixed(2) + '</span>'
+                }
+            },
+            {
+                name: 'id',
+                data: 'id',
+                title: '',
+                render: function (data) {
+                    return '<div class="btn-group btn-group-xs">' +
+                    '<a class="btn btn-default btn-xs" href="' + data.edit + '">' +
+                    '<span class="glyphicon glyphicon-pencil"</a>' +
+                    '<a class="btn btn-danger btn-xs" href="' + data.delete + '">' +
+                    '<span class="glyphicon glyphicon-trash"</a>' +
+                    '</a></div>';
+                }
+            }
+        ]
+    };
+    $('#accountTable').DataTable(opt);
+}
+
+
+function drawTransactionsForAccount() {
     $.getJSON('chart/home/account/' + accountID).success(function (data) {
         var options = {
             chart: {
-                renderTo: 'chart',
+                renderTo: 'accountTransactionsTable',
                 type: 'spline'
             },
 
@@ -18,7 +78,7 @@ if($('#chart').length == 1) {
                 allowDecimals: false,
                 labels: {
                     formatter: function () {
-                        if(this.value >= 1000 || this.value <= -1000) {
+                        if (this.value >= 1000 || this.value <= -1000) {
                             return '\u20AC ' + (this.value / 1000) + 'k';
                         }
                         return '\u20AC ' + this.value;
@@ -37,10 +97,10 @@ if($('#chart').length == 1) {
                     text: null
                 }
             },
-            legend: {enabled:false},
+            legend: {enabled: false},
             tooltip: {
                 formatter: function () {
-                    return this.series.name + ': \u20AC ' + Highcharts.numberFormat(this.y,2);
+                    return this.series.name + ': \u20AC ' + Highcharts.numberFormat(this.y, 2);
                 }
             },
             plotOptions: {
@@ -68,10 +128,84 @@ if($('#chart').length == 1) {
                 enabled: false
             }
         };
-        $('#chart').highcharts(options);
+        $('#accountTransactionsTable').highcharts(options);
     });
 }
 
-
-
-});
+//$(function () {
+//if($('#chart').length == 1) {
+//    /**
+//     * get data from controller for home charts:
+//     */
+//    $.getJSON('chart/home/account/' + accountID).success(function (data) {
+//        var options = {
+//            chart: {
+//                renderTo: 'chart',
+//                type: 'spline'
+//            },
+//
+//            series: data.series,
+//            title: {
+//                text: null
+//            },
+//            yAxis: {
+//                allowDecimals: false,
+//                labels: {
+//                    formatter: function () {
+//                        if(this.value >= 1000 || this.value <= -1000) {
+//                            return '\u20AC ' + (this.value / 1000) + 'k';
+//                        }
+//                        return '\u20AC ' + this.value;
+//
+//                    }
+//                },
+//                title: {text: null}
+//            },
+//            xAxis: {
+//                type: 'datetime',
+//                dateTimeLabelFormats: {
+//                    day: '%e %b',
+//                    week: '%e %b'
+//                },
+//                title: {
+//                    text: null
+//                }
+//            },
+//            legend: {enabled:false},
+//            tooltip: {
+//                formatter: function () {
+//                    return this.series.name + ': \u20AC ' + Highcharts.numberFormat(this.y,2);
+//                }
+//            },
+//            plotOptions: {
+//                line: {
+//                    shadow: true
+//                },
+//                series: {
+//                    cursor: 'pointer',
+//                    negativeColor: '#FF0000',
+//                    threshold: 0,
+//                    lineWidth: 1,
+//                    marker: {
+//                        radius: 0
+//                    },
+//                    point: {
+//                        events: {
+//                            click: function (e) {
+//                                alert('click!');
+//                            }
+//                        }
+//                    }
+//                }
+//            },
+//            credits: {
+//                enabled: false
+//            }
+//        };
+//        $('#chart').highcharts(options);
+//    });
+//}
+//
+//
+//
+//});
