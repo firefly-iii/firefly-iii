f0c0002a6ddcb50b1912ec3e58293678b52e084e
Sander Dorigo <thegrumpydictator@gmail.com>
2014-10-30 18:24:10 +0100

    Some cleanup

--- app/views/accounts/show.blade.php
+++ app/views/accounts/show.blade.php
@@ -14,52 +14,6 @@
     <div class="col-lg-4 col-md-6 col-sm-12">
         <!-- time based navigation -->
         @include('partials.date_nav')
-
-        <!-- summary of the selected period -->
-        <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <i class="fa fa-fw fa-align-justify"></i> Summary
-                    </div>
-                    <div class="panel-body">
-                    On the todo list.
-                    {{--
-                        <table class="table table-striped table-condensed">
-                            <tr>
-                                <th></th>
-                                <th>Expense / income</th>
-                                <th>Transfers</th>
-                            </tr>
-                            <tr>
-                                <td>Out</td>
-                                <td>
-                                    {{mf($show['statistics']['period']['out'])}}
-                                    <a href="{{route('accounts.show',$account->id)}}?type=transactions&amp;show=expenses"><span class="glyphicon glyphicon-circle-arrow-right"></span></a>
-                                </td>
-                                <td>
-                                    {{mf($show['statistics']['period']['t_out'])}}
-                                    <a href="{{route('accounts.show',$account->id)}}?type=transfers&amp;show=out"><span class="glyphicon glyphicon-circle-arrow-right"></span></a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>In</td>
-                                <td>
-                                    {{mf($show['statistics']['period']['in'])}}
-                                    <a href="{{route('accounts.show',$account->id)}}?type=transactions&amp;show=income"><span class="glyphicon glyphicon-circle-arrow-right"></span></a>
-                                </td>
-                                <td>
-                                    {{mf($show['statistics']['period']['t_in'])}}
-                                    <a href="{{route('accounts.show',$account->id)}}?type=transfers&amp;show=in"><span class="glyphicon glyphicon-circle-arrow-right"></span></a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Difference</td>
-                                <td>{{mf($show['statistics']['period']['diff'])}}</td>
-                                <td>{{mf($show['statistics']['period']['t_diff'])}}</td>
-                            </tr>
-                        </table>
-                        --}}
-                    </div>
-                </div>
     </div>
 </div>
 
@@ -101,19 +55,7 @@
 
 
 
-{{--
-<div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12">
-        <h4>Transactions <small> For selected account and period</small></h4>
-        @include('paginated.transactions',['journals' => $show['journals'],'sum' => true])
-    </div>
-</div>
---}}
 @stop
-
-@section('styles')
-@stop
-
 @section('scripts')
 <script type="text/javascript">
     var accountID = {{{$account->id}}};
@@ -122,8 +64,5 @@
 <script type="text/javascript" src="https://www.google.com/jsapi"></script>
 {{HTML::script('assets/javascript/firefly/gcharts.options.js')}}
 {{HTML::script('assets/javascript/firefly/gcharts.js')}}
-
-
-
 {{HTML::script('assets/javascript/firefly/accounts.js')}}
 @stop
