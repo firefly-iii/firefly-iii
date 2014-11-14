4bd38f97a20780b0b3a57ea8da760b5d86cc1d1f
James Cole <thegrumpydictator@gmail.com>
2014-11-14 10:41:35 +0100

    Whoops. But not to worry, changed it already.

--- app/config/homestead/mail.php
+++ app/config/homestead/mail.php
@@ -4,10 +4,10 @@ return [
     'driver'     => 'smtp',
     'host'       => 'smtp.gmail.com',
     'port'       => 587,
-    'from'       => ['address' => 'thegrumpydictator@gmail.com', 'name' => 'Firefly III'],
+    'from'       => ['address' => '@gmail.com', 'name' => 'Firefly III'],
     'encryption' => 'tls',
-    'username'   => 'thegrumpydictator@gmail.com',
-    'password'   => 'eyp-ort-ab-ig-york-ig-e-kne',
+    'username'   => '@gmail.com',
+    'password'   => '',
     'sendmail'   => '/usr/sbin/sendmail -bs',
     'pretend'    => false,
 ];
