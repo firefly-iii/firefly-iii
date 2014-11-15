8e3db1eb581e9d952865e17048d8ae8e47898a3b
James Cole <thegrumpydictator@gmail.com>
2014-11-15 09:23:07 +0100

    Cleaned up preferences view.

--- app/views/preferences/index.blade.php
+++ app/views/preferences/index.blade.php
@@ -3,87 +3,82 @@
 <!-- form -->
 {{Form::open(['class' => 'form-horizontal'])}}
 
-<!-- TODO cleanup for new forms and layout. -->
-<!-- home screen accounts -->
 <div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12">
-        <h3>Home screen accounts</h3>
-        <p class="text-info">Which accounts should be displayed on the home page?</p>
-        @foreach($accounts as $account)
-            <div class="form-group">
-                <div class="col-sm-10">
-                    <div class="checkbox">
-                        <label>
-                            @if(in_array($account->id,$frontpageAccounts->data) || count($frontpageAccounts->data) == 0)
-                                <input type="checkbox" name="frontpageAccounts[]" value="{{$account->id}}" checked> {{{$account->name}}}
-                            @else
-                            <input type="checkbox" name="frontpageAccounts[]" value="{{$account->id}}"> {{{$account->name}}}
-                            @endif
-                        </label>
+    <div class="col-lg-6 col-md-6 col-sm-6">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+                <i class="fa fa-credit-card fa-fw"></i> Home screen accounts
+            </div>
+            <div class="panel-body">
+                <p class="text-info">Which accounts should be displayed on the home page?</p>
+                 @foreach($accounts as $account)
+                    <div class="form-group">
+                        <div class="col-sm-10">
+                            <div class="checkbox">
+                                <label>
+                                    @if(in_array($account->id,$frontpageAccounts->data) || count($frontpageAccounts->data) == 0)
+                                        <input type="checkbox" name="frontpageAccounts[]" value="{{$account->id}}" checked> {{{$account->name}}}
+                                    @else
+                                    <input type="checkbox" name="frontpageAccounts[]" value="{{$account->id}}"> {{{$account->name}}}
+                                    @endif
+                                </label>
+                            </div>
+                        </div>
                     </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-6 col-md-6 col-sm-6">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+                <i class="fa fa-clock-o fa-fw"></i> Home view range
+            </div>
+            <div class="panel-body">
+                <p class="text-info">By default, Firefly will show you one month of data.</p>
+                <div class="radio">
+                    <label>
+                        <input type="radio" name="viewRange" value="1D" @if($viewRange == '1D') checked @endif>
+                        One day
+                    </label>
+                </div>
+
+                <div class="radio">
+                    <label>
+                        <input type="radio" name="viewRange" value="1W" @if($viewRange == '1W') checked @endif>
+                        One week
+                    </label>
+                </div>
+
+                <div class="radio">
+                    <label>
+                        <input type="radio" name="viewRange" value="1M" @if($viewRange == '1M') checked @endif>
+                        One month
+                    </label>
+                </div>
+
+                <div class="radio">
+                    <label>
+                        <input type="radio" name="viewRange" value="3M" @if($viewRange == '3M') checked @endif>
+                        Three months
+                    </label>
+                </div>
+
+                <div class="radio">
+                    <label>
+                        <input type="radio" name="viewRange" value="6M" @if($viewRange == '6M') checked @endif>
+                        Six months
+                    </label>
                 </div>
             </div>
-        @endforeach
-
-
+        </div>
     </div>
 </div>
-
-<!-- home screen accounts -->
 <div class="row">
     <div class="col-lg-12 col-md-12 col-sm-12">
-        <h3>Home view range</h3>
-        <p class="text-info">By default, Firefly will show you one month of data.</p>
-        <div class="radio">
-            <label>
-                <input type="radio" name="viewRange" value="1D" @if($viewRange == '1D') checked @endif>
-                One day
-            </label>
-        </div>
-
-        <div class="radio">
-            <label>
-                <input type="radio" name="viewRange" value="1W" @if($viewRange == '1W') checked @endif>
-                One week
-            </label>
-        </div>
-
-        <div class="radio">
-            <label>
-                <input type="radio" name="viewRange" value="1M" @if($viewRange == '1M') checked @endif>
-                One month
-            </label>
-        </div>
-
-        <div class="radio">
-            <label>
-                <input type="radio" name="viewRange" value="3M" @if($viewRange == '3M') checked @endif>
-                Three months
-            </label>
-        </div>
-
-        <div class="radio">
-            <label>
-                <input type="radio" name="viewRange" value="6M" @if($viewRange == '6M') checked @endif>
-                Six months
-            </label>
-        </div>
-
-
-
-
-    </div>
-</div>
-
-
-<!-- submit button -->
-
-<div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12">
-        <h3>Submit</h3>
         <div class="form-group">
             <div class="col-sm-12">
-                <button type="submit" class="btn btn-default">Save settings</button>
+                <button type="submit" class="btn btn-success btn-lg">Save settings</button>
             </div>
         </div>
     </div>
