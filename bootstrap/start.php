4f4e6fac167e8b1139ec37ab68c868793fb2b5d7
James Cole <thegrumpydictator@gmail.com>
2014-11-17 21:50:11 +0100

    Fixed a bug where limits and limit repetitions would not be updated and/or created because of double post calls and missing events.

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -89,6 +89,7 @@ require $framework . '/Illuminate/Foundation/start.php';
 //Event::subscribe('Firefly\Trigger\Recurring\EloquentRecurringTrigger');
 //Event::subscribe('Firefly\Trigger\Journals\EloquentJournalTrigger');
 Event::subscribe('FireflyIII\Event\Piggybank');
+Event::subscribe('FireflyIII\Event\Budget');
 
 // TODO event that creates a relationship between transaction journals and recurring events when created.
 // TODO event that updates the relationship between transaction journals and recurring events when edited.
