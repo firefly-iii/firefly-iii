69e7501d47a5642bb4253850105a3218aeb4e5a8
James Cole <thegrumpydictator@gmail.com>
2014-11-17 08:20:05 +0100

    Start with cleaning up the menu.

--- app/views/partials/menu.blade.php
+++ app/views/partials/menu.blade.php
@@ -1,77 +1,73 @@
 <!-- Navigation -->
 <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
-<div class="navbar-header">
-    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-    </button>
-    <a class="navbar-brand" href="{{route('index')}}">Firefly</a>
-</div>
-<!-- /.navbar-header -->
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="{{route('index')}}">Firefly</a>
+    </div>
+    <!-- /.navbar-header -->
 
-<ul class="nav navbar-top-links navbar-right">
-    <li class="dropdown">
-    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-        <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
-    </a>
-    <ul class="dropdown-menu dropdown-user">
-        <li><a href="{{route('profile')}}"><i class="fa fa-user fa-fw"></i> {{Auth::user()->email}}</a>
+    <ul class="nav navbar-top-links navbar-right">
+        <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
+            </a>
+            <ul class="dropdown-menu dropdown-user">
+                <li><a href="{{route('profile')}}"><i class="fa fa-user fa-fw"></i> {{Auth::user()->email}}</a></li>
+                <li><a href="{{route('preferences')}}"><i class="fa fa-gear fa-fw"></i> Preferences</a></li>
+                <li class="divider"></li>
+                <li><a href="{{route('logout')}}"><i class="fa fa-sign-out fa-fw"></i> Logout</a></li>
+            </ul>
+            <!-- /.dropdown-user -->
         </li>
-        <li><a href="{{route('preferences')}}"><i class="fa fa-gear fa-fw"></i> Preferences</a>
-        </li>
-        <li class="divider"></li>
-        <li><a href="{{route('logout')}}"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
-        </li>
+        <!-- /.dropdown -->
     </ul>
-    <!-- /.dropdown-user -->
-</li>
-<!-- /.dropdown -->
-</ul>
-<!-- /.navbar-top-links -->
-    <?php
-    $r = Route::getCurrentRoute()->getName();
-
-    ?>
-
-<div class="navbar-default sidebar" role="navigation">
-    <div class="sidebar-nav navbar-collapse">
-        <ul class="nav" id="side-menu">
-            <li class="sidebar-search">
-            <form action="{{route('search')}}" method="GET" class="form-inline">
-                <div class="input-group custom-search-form">
-                    <input type="text" name="q" class="form-control" value="@if(Input::get('q')){{{Input::get('q')}}}@endif" placeholder="Search...">
-                                <span class="input-group-btn">
-                                <button class="btn btn-default" type="submit">
-                                    <i class="fa fa-search"></i>
-                                </button>
+    <!-- /.navbar-top-links -->
+    <?php $r = Route::getCurrentRoute()->getName();?>
+    <div class="navbar-default sidebar" role="navigation">
+        <div class="sidebar-nav navbar-collapse">
+            <ul class="nav" id="side-menu">
+                <li class="sidebar-search">
+                <form action="{{route('search')}}" method="GET" class="form-inline">
+                    <div class="input-group custom-search-form">
+                        <input type="text" name="q" class="form-control" value="@if(Input::get('q')){{{Input::get('q')}}}@endif" placeholder="Search...">
+                            <span class="input-group-btn">
+                                <button class="btn btn-default" type="submit"><i class="fa fa-search"></i></button>
                             </span>
-
-                </div>
+                    </div>
                 </form>
                 <!-- /input-group -->
-            </li>
-            <li>
-                <a @if($r == 'index') class="active" @endif href="{{route('index')}}"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
-            </li>
-            <li>
-                <a href="#"><i class="fa fa-credit-card fa-fw"></i> Accounts <span class="fa arrow"></span></a>
-                <ul class="nav nav-second-level">
-                    <li>
-                        <a href="{{route('accounts.index','asset')}}"><i class="fa fa-money fa-fw"></i> Asset accounts</a>
-                    </li>
-                    <li>
-                        <a href="{{route('accounts.index','expense')}}"><i class="fa fa-shopping-cart fa-fw"></i> Expense accounts</a>
-                    </li>
-                    <li>
-                        <a href="{{route('accounts.index','revenue')}}"><i class="fa fa-download fa-fw"></i> Revenue accounts</a>
-                    </li>
+                </li>
+                <li>
+                    <a @if($r == 'index') class="active" @endif href="{{route('index')}}"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+                </li>
+                <?php
+                $isAccounts = $r == 'accounts.index';
+                $isAsset = $r == 'accounts.index' && isset($what) && $what == 'asset';
+                $isExpense = $r == 'accounts.index' && isset($what) && $what == 'expense';
+                $isRevenue = $r == 'accounts.index' && isset($what) && $what == 'revenue';
+                ?>
+                <li @if($isAccounts) class="active" @endif>
+                    <a href="#"><i class="fa fa-credit-card fa-fw"></i> Accounts <span class="fa arrow"></span></a>
+                    <ul class="nav nav-second-level">
+                        <li>
+                            <a @if($isAsset) class="active" @endif href="{{route('accounts.index','asset')}}"><i class="fa fa-money fa-fw"></i> Asset accounts</a>
+                        </li>
+                        <li>
+                            <a @if($isExpense) class="active" @endif href="{{route('accounts.index','expense')}}"><i class="fa fa-shopping-cart fa-fw"></i> Expense accounts</a>
+                        </li>
+                        <li>
+                            <a @if($isRevenue) class="active" @endif href="{{route('accounts.index','revenue')}}"><i class="fa fa-download fa-fw"></i> Revenue accounts</a>
+                        </li>
                 </ul>
                 <!-- /.nav-second-level -->
             </li>
             <li>
-                <a @if($r == 'budgets.index') class="index" @endif href="{{route('budgets.index')}}"><i class="fa fa-tasks fa-fw"></i> Budgets</a>
+                <a @if(!(strpos($r,'budget')) === false) class="active" @endif href="{{route('budgets.index')}}"><i class="fa fa-tasks fa-fw"></i> Budgets</a>
             </li>
             <li>
                 <a @if($r == 'categories.index') class="active" @endif href="{{route('categories.index')}}"><i class="fa fa-bar-chart fa-fw"></i> Categories</a>
