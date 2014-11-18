cca70560f824c9f27c534533797d5903c8435e72
James Cole <thegrumpydictator@gmail.com>
2014-11-18 10:18:50 +0100

    Fixed some things so coveralls will fire.

--- app/tests/TestCase.php
+++ app/tests/TestCase.php
@@ -1,12 +1,9 @@
 <?php
-use League\FactoryMuffin\Facade as f;
 
 /**
  * Class TestCase
  */
-class TestCase extends Illuminate\Foundation\Testing\TestCase
-{
-
+class TestCase extends Illuminate\Foundation\Testing\TestCase {
     /**
      * Creates the application.
      *
@@ -14,33 +11,8 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase
      */
     public function createApplication()
     {
-        /** @noinspection PhpUnusedLocalVariableInspection */
         $unitTesting = true;
-
-        /** @noinspection PhpUnusedLocalVariableInspection */
         $testEnvironment = 'testing';
-
-        return require __DIR__ . '/../../bootstrap/start.php';
+        return require __DIR__.'/../../bootstrap/start.php';
     }
-
-    public function setUp()
-    {
-        parent::setUp();
-        f::loadFactories(__DIR__ . '/factories');
-    }
-
-    /**
-     * @param $class
-     *
-     * @return \Mockery\MockInterface
-     */
-    public function mock($class)
-    {
-        $mock = Mockery::mock($class);
-
-        $this->app->instance($class, $mock);
-
-        return $mock;
-    }
-
 }
