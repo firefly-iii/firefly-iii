1523da2c3036971224a9ea4c74afb7f6ee333358
James Cole <thegrumpydictator@gmail.com>
2014-11-22 23:38:08 +0100

    Some debug information.

--- app/views/repeatedexpense/index.blade.php
+++ app/views/repeatedexpense/index.blade.php
@@ -26,10 +26,11 @@
                     <!-- {{$entry->currentRep->currentamount}} <= {{$entry->bars[$i]['amount']}} -->
                     <div class="col-lg-{{$entry->barCount}} col-md-{{$entry->barCount}} col-sm-{{$entry->barCount}}">
                         <div class="progress">
-
                             @if($entry->currentRep->currentamount <= $entry->bars[$i]['amount'])
+                                <!-- TRUE (smaller or equal) -->
                                 <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">{{mf($entry->bars[$i]['amount'],false)}}</div>
                             @else
+                                <!-- FALSE (larger) -->
                                 <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">{{mf($entry->bars[$i]['amount'],false)}}</div>
                             @endif
                         </div>
