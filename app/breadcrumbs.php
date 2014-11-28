439b6d323210dac5bb7ab8207dc70cbf255b9f53
James Cole <thegrumpydictator@gmail.com>
2014-11-28 15:52:52 +0100

    More bread crumbs.

--- app/breadcrumbs.php
+++ app/breadcrumbs.php
@@ -44,22 +44,6 @@ Breadcrumbs::register(
 );
 Breadcrumbs::register(
     'accounts.delete', function (Generator $breadcrumbs, \Account $account) {
-        switch ($account->accountType->type) {
-            default:
-                throw new FireflyException('Cannot handle account type "' . e($account->accountType->type) . '"');
-                break;
-            case 'Default account':
-            case 'Asset account':
-                $what = 'asset';
-                break;
-            case 'Expense account':
-            case 'Beneficiary account':
-                $what = 'expense';
-                break;
-            case 'Revenue account':
-                $what = 'revenue';
-                break;
-        }
         $breadcrumbs->parent('accounts.show', $account);
         $breadcrumbs->push('Delete ' . $account->name, route('accounts.delete', $account->id));
     }
@@ -67,23 +51,47 @@ Breadcrumbs::register(
 
 Breadcrumbs::register(
     'accounts.edit', function (Generator $breadcrumbs, \Account $account) {
-        switch ($account->accountType->type) {
-            default:
-                throw new FireflyException('Cannot handle account type "' . e($account->accountType->type) . '"');
-                break;
-            case 'Default account':
-            case 'Asset account':
-                $what = 'asset';
-                break;
-            case 'Expense account':
-            case 'Beneficiary account':
-                $what = 'expense';
-                break;
-            case 'Revenue account':
-                $what = 'revenue';
-                break;
-        }
         $breadcrumbs->parent('accounts.show', $account);
         $breadcrumbs->push('Edit ' . $account->name, route('accounts.edit', $account->id));
+    }
+);
+
+// budgets.
+Breadcrumbs::register(
+    'budgets.index', function (Generator $breadcrumbs) {
+        $breadcrumbs->parent('home');
+        $breadcrumbs->push('Budgets', route('budgets.index'));
+    }
+);
+Breadcrumbs::register(
+    'budgets.create', function (Generator $breadcrumbs) {
+        $breadcrumbs->parent('budgets.index');
+        $breadcrumbs->push('Create new budget', route('budgets.create'));
+    }
+);
+
+Breadcrumbs::register(
+    'budgets.edit', function (Generator $breadcrumbs, Budget $budget) {
+        $breadcrumbs->parent('budgets.index');
+        $breadcrumbs->push('Edit '.$budget->name, route('budgets.edit',$budget->id));
+    }
+);
+Breadcrumbs::register(
+    'budgets.delete', function (Generator $breadcrumbs, Budget $budget) {
+        $breadcrumbs->parent('budgets.index');
+        $breadcrumbs->push('Delete '.$budget->name, route('budgets.delete',$budget->id));
+    }
+);
+
+Breadcrumbs::register(
+    'budgets.show', function (Generator $breadcrumbs, Budget $budget, LimitRepetition $repetition = null) {
+        $breadcrumbs->parent('budgets.index');
+        $breadcrumbs->push($budget->name, route('budgets.show', $budget->id));
+        if (!is_null($repetition)) {
+            $breadcrumbs->push(
+                DateKit::periodShow($repetition->startdate, $repetition->limit->repeat_freq), route('budgets.show', $budget->id, $repetition->id)
+            );
+        }
+        //$breadcrumbs->push('Budgets', route('budgets.index'));
     }
 );
