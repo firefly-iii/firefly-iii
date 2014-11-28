40b6376175903e53be076db7f4023fd90ae12193
James Cole <thegrumpydictator@gmail.com>
2014-11-28 14:41:58 +0100

    New report

--- app/views/reports/unbalanced.blade.php
+++ app/views/reports/unbalanced.blade.php
@@ -1,74 +1,58 @@
 @extends('layouts.default')
 @section('content')
+@if(count($withdrawals) == 0 && count($deposits) == 0)
 <div class="row">
     <div class="col-lg-4 col-md-4 col-sm-6">
-        <h3>Transactions</h3>
+        <p class="text-success">Everything accounted for.</p>
+    </div>
+</div>
+@endif
+@if(count($withdrawals) > 0)
+<div class="row">
+    <div class="col-lg-4 col-md-4 col-sm-6">
+        <h3>Withdrawals</h3>
     </div>
 </div>
 
 <div class="row">
     @foreach($withdrawals as $journal)
         <div class="col-lg-4 col-md-4 col-sm-6">
-            <div class="panel
-            @if($journal->counters->count() > 0)
-            panel-default
-            @else
-            panel-danger
-            @endif
-            ">
+            <div class="panel panel-danger">
                 <div class="panel-heading">
                     <a href="{{route('transactions.show',$journal->id)}}">{{{$journal->description}}}</a>
                 </div>
                 <div class="panel-body">
                     <p>Spent {{mf($journal->getAmount())}}</p>
-                @if($journal->counters->count() > 0)
+                    <p class="text-danger">No counter transaction!</p>
                 </div>
-                <table class="table">
-                @foreach($journal->counters as $counter)
-                <tr>
-                    <td><i class="fa fa-fw fa-arrows-h"></i></td>
-                    <td><a href="{{route('transactions.show',$counter->id)}}">{{$counter->description}}</a></td>
-                    <td>{{mf($counter->getAmount())}}</td>
-                </tr>
-                <!-- {{$counter}} -->
-                @endforeach
-                </table>
-                @else
-                <p class="text-danger">No counter transaction!</p>
-                </div>
-                @endif
             </div>
         </div>
     @endforeach
 </div>
+@endif
+@if(count($deposits) > 0)
 <div class="row">
     <div class="col-lg-4 col-md-4 col-sm-6">
-        <h3>Transfers</h3>
+        <h3>Deposits</h3>
     </div>
 </div>
-
 <div class="row">
-    @foreach($transfers as $journal)
+    @foreach($deposits as $journal)
         <div class="col-lg-4 col-md-4 col-sm-6">
-            <div class="panel
-            @if($journal->counters->count() > 0)
-                panel-default
-            @else
-                panel-danger
-            @endif
-            ">
+            <div class="panel panel-danger">
                 <div class="panel-heading">
-                    {{{$journal->description}}}
+                    <a href="{{route('transactions.show',$journal->id)}}">{{{$journal->description}}}</a>
                 </div>
                 <div class="panel-body">
-                    Bla bla?
+                    <p>Received {{mf($journal->getAmount())}}</p>
+                    <p class="text-danger">No counter transaction!</p>
                 </div>
-
             </div>
-
         </div>
     @endforeach
 </div>
+@endif
+
 
 
 
