6bb54bfa85cb25889a6722df3fcd2ea82e81d2b3
James Cole <thegrumpydictator@gmail.com>
2014-11-28 15:46:44 +0100

    More bread crumbs.

--- app/breadcrumbs.php
+++ app/breadcrumbs.php
@@ -1,5 +1,6 @@
 <?php
 use DaveJamesMiller\Breadcrumbs\Generator;
+use FireflyIII\Exception\FireflyException;
 
 /*
  * Back home.
@@ -12,6 +13,7 @@ Breadcrumbs::register(
     }
 );
 
+// accounts
 Breadcrumbs::register(
     'accounts.index', function (Generator $breadcrumbs, $what) {
         $breadcrumbs->parent('home');
@@ -19,8 +21,69 @@ Breadcrumbs::register(
     }
 );
 Breadcrumbs::register(
-    'accounts.show', function (Generator $breadcrumbs, $what, \Account $account) {
-        $breadcrumbs->parent('accounts.index',$what);
+    'accounts.show', function (Generator $breadcrumbs, \Account $account) {
+        switch ($account->accountType->type) {
+            default:
+                throw new FireflyException('Cannot handle account type "' . e($account->accountType->type) . '"');
+                break;
+            case 'Default account':
+            case 'Asset account':
+                $what = 'asset';
+                break;
+            case 'Expense account':
+            case 'Beneficiary account':
+                $what = 'expense';
+                break;
+            case 'Revenue account':
+                $what = 'revenue';
+                break;
+        }
+        $breadcrumbs->parent('accounts.index', $what);
         $breadcrumbs->push($account->name, route('accounts.show', $account->id));
+    }
+);
+Breadcrumbs::register(
+    'accounts.delete', function (Generator $breadcrumbs, \Account $account) {
+        switch ($account->accountType->type) {
+            default:
+                throw new FireflyException('Cannot handle account type "' . e($account->accountType->type) . '"');
+                break;
+            case 'Default account':
+            case 'Asset account':
+                $what = 'asset';
+                break;
+            case 'Expense account':
+            case 'Beneficiary account':
+                $what = 'expense';
+                break;
+            case 'Revenue account':
+                $what = 'revenue';
+                break;
+        }
+        $breadcrumbs->parent('accounts.show', $account);
+        $breadcrumbs->push('Delete ' . $account->name, route('accounts.delete', $account->id));
+    }
+);
+
+Breadcrumbs::register(
+    'accounts.edit', function (Generator $breadcrumbs, \Account $account) {
+        switch ($account->accountType->type) {
+            default:
+                throw new FireflyException('Cannot handle account type "' . e($account->accountType->type) . '"');
+                break;
+            case 'Default account':
+            case 'Asset account':
+                $what = 'asset';
+                break;
+            case 'Expense account':
+            case 'Beneficiary account':
+                $what = 'expense';
+                break;
+            case 'Revenue account':
+                $what = 'revenue';
+                break;
+        }
+        $breadcrumbs->parent('accounts.show', $account);
+        $breadcrumbs->push('Edit ' . $account->name, route('accounts.edit', $account->id));
     }
 );
