54e0090f5fee70a80e0d239a72df09ac6afdbb70
James Cole <thegrumpydictator@gmail.com>
2014-11-28 15:41:32 +0100

    Some work on bread crumbs and reports.

--- app/breadcrumbs.php
+++ app/breadcrumbs.php
@@ -1,7 +1,26 @@
 <?php
+use DaveJamesMiller\Breadcrumbs\Generator;
+
 /*
  * Back home.
  */
-Breadcrumbs::register('home', function($breadcrumbs) {
-    $breadcrumbs->push('Home', route('index'));
-});
+Breadcrumbs::register(
+    'home',
+    function (Generator $breadcrumbs) {
+
+        $breadcrumbs->push('Home', route('index'));
+    }
+);
+
+Breadcrumbs::register(
+    'accounts.index', function (Generator $breadcrumbs, $what) {
+        $breadcrumbs->parent('home');
+        $breadcrumbs->push(ucfirst($what) . ' accounts', route('accounts.index', $what));
+    }
+);
+Breadcrumbs::register(
+    'accounts.show', function (Generator $breadcrumbs, $what, \Account $account) {
+        $breadcrumbs->parent('accounts.index',$what);
+        $breadcrumbs->push($account->name, route('accounts.show', $account->id));
+    }
+);
