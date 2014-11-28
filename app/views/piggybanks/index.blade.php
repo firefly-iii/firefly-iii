6b23e9392e5e6872bf023a1734e4012b94a680de
James Cole <thegrumpydictator@gmail.com>
2014-11-28 16:04:06 +0100

    More bread crumbs.

--- app/views/piggybanks/index.blade.php
+++ app/views/piggybanks/index.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.default')
 @section('content')
+{{ Breadcrumbs::renderIfExists(Route::getCurrentRoute()->getName()) }}
 <div class="row">
     @foreach($piggybanks as $piggybank)
         <div class="col-lg-6 col-md-6 col-sm-12">
