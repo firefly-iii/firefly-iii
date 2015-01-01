05ea8216ff67dc90d3b18c4014b7305d814a2fa0
James Cole <thegrumpydictator@gmail.com>
2015-01-01 22:51:38 +0100

    Fixed transaction coverage.

--- app/tests/factories/Transaction.php
+++ app/tests/factories/Transaction.php
@@ -5,6 +5,8 @@ League\FactoryMuffin\Facade::define(
                      'account_id'             => 'factory|Account',
                      'transaction_journal_id' => 'factory|TransactionJournal',
                      'description'            => 'sentence',
-                     'amount'                 => 'numberBetween:1,100',
+                     'amount'                 => function() {
+                         return round(rand(100,10000) / 100,2);
+                     }
                  ]
 );
