9a3aed80389fc5435156d97485893c319cf50a87
James Cole <thegrumpydictator@gmail.com>
2015-01-02 10:53:18 +0100

    Moved code to relate transfers to another class. Still needs some work.

--- app/lib/FireflyIII/Database/TransactionJournal/TransactionJournal.php
+++ app/lib/FireflyIII/Database/TransactionJournal/TransactionJournal.php
@@ -571,37 +571,4 @@ class TransactionJournal implements TransactionJournalInterface, CUDInterface, C
 
         return \Paginator::make($items, $count, $limit);
     }
-
-    /**
-     * @param string              $query
-     * @param \TransactionJournal $journal
-     *
-     * @return Collection
-     */
-    public function searchRelated($query, \TransactionJournal $journal)
-    {
-        $start = clone $journal->date;
-        $end   = clone $journal->date;
-        $start->startOfMonth();
-        $end->endOfMonth();
-
-        // get already related transactions:
-        $exclude = [$journal->id];
-        foreach ($journal->transactiongroups()->get() as $group) {
-            foreach ($group->transactionjournals() as $current) {
-                $exclude[] = $current->id;
-            }
-        }
-        $exclude = array_unique($exclude);
-
-        $query = $this->getUser()->transactionjournals()
-                      ->withRelevantData()
-                      ->before($end)
-                      ->after($start)
-                      ->whereNotIn('id', $exclude)
-                      ->where('description', 'LIKE', '%' . $query . '%')
-                      ->get();
-
-        return $query;
-    }
 }
