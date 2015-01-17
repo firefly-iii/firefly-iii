a0920e2094ab9f41c016e0fd00db8cc67d3b8c18
James Cole <thegrumpydictator@gmail.com>
2015-01-17 08:58:19 +0100

    Added inspections for all migrations.

--- app/database/migrations/2014_12_24_191544_changes_for_v322.php
+++ app/database/migrations/2014_12_24_191544_changes_for_v322.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
 /**
+ * @SuppressWarnings(PHPMD.ShortMethodName)
+ *
  * Class ChangesForV322
  */
 class ChangesForV322 extends Migration
