ca5c886e9e9d3ee56b7dc0ee5c79badde6c9e69c
James Cole <thegrumpydictator@gmail.com>
2015-01-17 08:58:30 +0100

    Cleaned up seeders.

--- app/database/seeds/AccountTypeSeeder.php
+++ app/database/seeds/AccountTypeSeeder.php
@@ -10,31 +10,14 @@ class AccountTypeSeeder extends Seeder
     {
         DB::table('account_types')->delete();
 
-        AccountType::create(
-            ['type' => 'Default account', 'editable' => true]
-        );
-        AccountType::create(
-            ['type' => 'Cash account', 'editable' => false]
-        );
-        AccountType::create(
-            ['type' => 'Asset account', 'editable' => true]
-        );
-        AccountType::create(
-            ['type' => 'Expense account', 'editable' => true]
-        );
-        AccountType::create(
-            ['type' => 'Revenue account', 'editable' => true]
-        );
-        AccountType::create(
-            ['type' => 'Initial balance account', 'editable' => false]
-        );
-        AccountType::create(
-            ['type' => 'Beneficiary account', 'editable' => true]
-        );
-
-        AccountType::create(
-            ['type' => 'Import account', 'editable' => false]
-        );
+        AccountType::create(['type' => 'Default account', 'editable' => true]);
+        AccountType::create(['type' => 'Cash account', 'editable' => false]);
+        AccountType::create(['type' => 'Asset account', 'editable' => true]);
+        AccountType::create(['type' => 'Expense account', 'editable' => true]);
+        AccountType::create(['type' => 'Revenue account', 'editable' => true]);
+        AccountType::create(['type' => 'Initial balance account', 'editable' => false]);
+        AccountType::create(['type' => 'Beneficiary account', 'editable' => true]);
+        AccountType::create(['type' => 'Import account', 'editable' => false]);
     }
 
 
