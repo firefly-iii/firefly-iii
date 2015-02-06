df165a817c3acefd87199f2bfc527225bfc8b2a7
James Cole <thegrumpydictator@gmail.com>
2015-02-06 04:39:52 +0100

    Fresh L5 installation.

---
+++ app/Providers/EventServiceProvider.php
@@ -0,0 +1,32 @@
+<?php namespace App\Providers;
+
+use Illuminate\Contracts\Events\Dispatcher as DispatcherContract;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class EventServiceProvider extends ServiceProvider {
+
+	/**
+	 * The event handler mappings for the application.
+	 *
+	 * @var array
+	 */
+	protected $listen = [
+		'event.name' => [
+			'EventListener',
+		],
+	];
+
+	/**
+	 * Register any other events for your application.
+	 *
+	 * @param  \Illuminate\Contracts\Events\Dispatcher  $events
+	 * @return void
+	 */
+	public function boot(DispatcherContract $events)
+	{
+		parent::boot($events);
+
+		//
+	}
+
+}
