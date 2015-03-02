56e36847a5e2a26ca79e6d0124358d24013dd621
James Cole <thegrumpydictator@gmail.com>
2015-03-02 12:35:14 +0100

    New code for new date range thing.

--- public/js/firefly.js
+++ public/js/firefly.js
@@ -1,6 +1,38 @@
 $(function () {
 
-    $('.currencySelect').click(currencySelect)
+    $('.currencySelect').click(currencySelect);
+
+        $('#daterange').daterangepicker(
+            {
+                ranges: {
+                    'This Month': [moment().startOf('month'), moment().endOf('month')],
+                    'Last Month': [moment().subtract('month', 1).startOf('month'), moment().subtract('month', 1).endOf('month')],
+                    'Next Month': [moment().add('month', 1).startOf('month'), moment().add('month', 1).endOf('month')]
+                },
+                opens: 'left',
+
+                format: 'DD-MM-YYYY',
+                startDate: start,
+                endDate: end
+            },
+            function(start, end, label) {
+
+                // send post.
+                $.post(dateRangeURL, {
+                    start: start.format('YYYY-MM-DD'),
+                    end: end.format('YYYY-MM-DD'),
+                    _token: token
+                }).success(function() {
+                    window.location.reload(true);
+                }).fail(function() {
+                    alert('Could not change date range');
+
+                });
+
+                //alert('A date range was chosen: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+            }
+
+        );
 
 });
 
@@ -22,3 +54,4 @@ function currencySelect(e) {
 
     return false;
 }
+
