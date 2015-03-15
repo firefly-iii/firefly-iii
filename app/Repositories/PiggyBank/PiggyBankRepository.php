66cd63a68d8eb90b1e4301d3c69afc81c75ba829
James Cole <thegrumpydictator@gmail.com>
2015-03-15 18:08:52 +0100

    Fixed a bug.

--- app/Repositories/PiggyBank/PiggyBankRepository.php
+++ app/Repositories/PiggyBank/PiggyBankRepository.php
@@ -112,7 +112,7 @@ class PiggyBankRepository implements PiggyBankRepositoryInterface
      */
     public function setOrder($id, $order)
     {
-        $piggyBank = PiggyBank::leftJoin('accounts', 'accounts.id', '=', 'piggy_banks.id')->where('accounts.user_id', Auth::user()->id)
+        $piggyBank = PiggyBank::leftJoin('accounts', 'accounts.id', '=', 'piggy_banks.account_id')->where('accounts.user_id', Auth::user()->id)
             ->where('piggy_banks.id',$id)->first(['piggy_banks.*']);
         if ($piggyBank) {
             $piggyBank->order = $order;
