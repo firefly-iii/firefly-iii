247175881f7e29491ad6e045468230e4b55ba3e6
James Cole <thegrumpydictator@gmail.com>
2015-03-27 13:24:04 +0100

    Moved some code.

--- public/js/transactions.js
+++ public/js/transactions.js
@@ -1,34 +1,29 @@
-if ($('input[name="expense_account"]').length > 0) {
-    $.getJSON('json/expense-accounts').success(function (data) {
-        $('input[name="expense_account"]').typeahead({source: data});
-    });
-}
-if ($('input[name="revenue_account"]').length > 0) {
-    $.getJSON('json/revenue-accounts').success(function (data) {
-        $('input[name="revenue_account"]').typeahead({source: data});
-    });
-}
-
-if ($('input[name="description"]').length > 0 && what != undefined) {
-    $.getJSON('json/transaction-journals/' + what).success(function (data) {
-        $('input[name="description"]').typeahead({source: data});
-    });
-}
-
-
-
-
-if ($('input[name="category"]').length > 0) {
-    $.getJSON('json/categories').success(function (data) {
-        $('input[name="category"]').typeahead({source: data});
-    });
-}
-
-
 $(document).ready(function () {
     if (typeof googleTablePaged != 'undefined') {
         googleTablePaged('table/transactions/' + what, 'transaction-table');
     }
 
+    if ($('input[name="expense_account"]').length > 0) {
+        $.getJSON('json/expense-accounts').success(function (data) {
+            $('input[name="expense_account"]').typeahead({source: data});
+        });
+    }
+    if ($('input[name="revenue_account"]').length > 0) {
+        $.getJSON('json/revenue-accounts').success(function (data) {
+            $('input[name="revenue_account"]').typeahead({source: data});
+        });
+    }
+
+    if ($('input[name="description"]').length > 0 && what != undefined) {
+        $.getJSON('json/transaction-journals/' + what).success(function (data) {
+            $('input[name="description"]').typeahead({source: data});
+        });
+    }
+
+    if ($('input[name="category"]').length > 0) {
+        $.getJSON('json/categories').success(function (data) {
+            $('input[name="category"]').typeahead({source: data});
+        });
+    }
 
 });
