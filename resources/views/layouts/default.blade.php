115499f3b2aa92fd18687021f93639d463a31ce4
James Cole <thegrumpydictator@gmail.com>
2015-03-27 11:59:07 +0100

    New favicon.

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -28,22 +28,24 @@
 
     @yield('styles')
 
-    <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png">
-    <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
-    <link rel="icon" type="image/png" href="/android-chrome-192x192.png" sizes="192x192">
-    <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
-    <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
-    <link rel="manifest" href="/android-chrome-manifest.json">
-    <meta name="msapplication-TileColor" content="#2b5797">
-    <meta name="msapplication-TileImage" content="/mstile-144x144.png">
+    <!-- favicons -->
+    <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png?v=xQw9GAO4MA">
+    <link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png?v=xQw9GAO4MA">
+    <link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png?v=xQw9GAO4MA">
+    <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png?v=xQw9GAO4MA">
+    <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png?v=xQw9GAO4MA">
+    <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png?v=xQw9GAO4MA">
+    <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png?v=xQw9GAO4MA">
+    <link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png?v=xQw9GAO4MA">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png?v=xQw9GAO4MA">
+    <link rel="icon" type="image/png" href="/favicon-32x32.png?v=xQw9GAO4MA" sizes="32x32">
+    <link rel="icon" type="image/png" href="/android-chrome-192x192.png?v=xQw9GAO4MA" sizes="192x192">
+    <link rel="icon" type="image/png" href="/favicon-96x96.png?v=xQw9GAO4MA" sizes="96x96">
+    <link rel="icon" type="image/png" href="/favicon-16x16.png?v=xQw9GAO4MA" sizes="16x16">
+    <link rel="manifest" href="/manifest.json?v=xQw9GAO4MA">
+    <link rel="shortcut icon" href="/favicon.ico?v=xQw9GAO4MA">
+    <meta name="msapplication-TileColor" content="#2d89ef">
+    <meta name="msapplication-TileImage" content="/mstile-144x144.png?v=xQw9GAO4MA">
     <meta name="theme-color" content="#ffffff">
 
     <!-- {{App::environment()}} -->
