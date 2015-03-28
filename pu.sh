95e34af6ef26dfa2c598fd82e13598947ec347b8
James Cole <thegrumpydictator@gmail.com>
2015-03-28 06:51:54 +0100

    Update script [skip ci]

--- pu.sh
+++ pu.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # create DB if not exists
-rm -f tests/database/db.sqlite
-touch tests/database/db.sqlite
-php artisan migrate --seed
+
+if [ ! -f tests/database/db.sqlite ]; then
+    touch tests/database/db.sqlite
+    php artisan migrate --seed
+fi
+
 phpunit
