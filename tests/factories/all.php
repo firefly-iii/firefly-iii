6a9ffae25de1dc7cb94ac2796cd27e58df6c1d41
James Cole <thegrumpydictator@gmail.com>
2015-04-03 08:31:50 +0200

    New tests and factories.

---
+++ tests/factories/all.php
@@ -0,0 +1,35 @@
+<?php
+use League\FactoryMuffin\Facade as FactoryMuffin;
+
+FactoryMuffin::define(
+    'FireflyIII\Models\Account', [
+                                   'user_id'         => 'factory|FireflyIII\User',
+                                   'account_type_id' => 'factory|FireflyIII\Models\AccountType',
+                                   'name'            => 'word',
+                                   'active'          => 'boolean',
+                                   'encrypted'       => 'boolean',
+                                   'virtual_balance' => 0
+                               ]
+);
+
+FactoryMuffin::define(
+    'FireflyIII\Models\Preference', [
+                                      'name'    => 'word',
+                                      'data'    => 'sentence',
+                                      'user_id' => 'factory|FireflyIII\User',
+                                  ]
+);
+
+FactoryMuffin::define(
+    'FireflyIII\Models\AccountType', [
+                                       'type'     => 'word',
+                                       'editable' => 1,
+                                   ]
+);
+
+FactoryMuffin::define(
+    'FireflyIII\User', [
+                         'email'    => 'email',
+                         'password' => bcrypt('james'),
+                     ]
+);
