714c13bdbfc22bd440f8550babdd690fd9fea6d9
James Cole <thegrumpydictator@gmail.com>
2015-04-11 15:04:16 +0200

    Include moment.js

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -39,6 +39,7 @@
 <script type="text/javascript" src="https://www.google.com/jsapi"></script>
 <script type="text/javascript" src="js/gcharts.options.js"></script>
 <script type="text/javascript" src="js/gcharts.js"></script>
+<script type="text/javascript" src="js/moment.min.js"></script>
 <script type="text/javascript" src="js/bootstrap-sortable.js"></script>
 <script type="text/javascript" src="js/categories.js"></script>
 @stop
