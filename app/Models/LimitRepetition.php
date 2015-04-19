0656ccbdd9147b0c432737cd591840376540b83f
James Cole <thegrumpydictator@gmail.com>
2015-04-19 13:57:43 +0200

    Fixed a bug where the query would include deleted transactions.

--- app/Models/LimitRepetition.php
+++ app/Models/LimitRepetition.php
@@ -1,5 +1,7 @@
 <?php namespace FireflyIII\Models;
 
+use Auth;
+use DB;
 use Illuminate\Database\Eloquent\Model;
 
 /**
@@ -26,18 +28,23 @@ class LimitRepetition extends Model
         return ['created_at', 'updated_at', 'startdate', 'enddate'];
     }
 
+    /**
+     * @return float
+     */
     public function spentInRepetition()
     {
-        $sum = \DB::table('transactions')
-                  ->leftJoin('transaction_journals', 'transaction_journals.id', '=', 'transactions.transaction_journal_id')
-                  ->leftJoin('budget_transaction_journal', 'budget_transaction_journal.transaction_journal_id', '=', 'transaction_journals.id')
-                  ->leftJoin('budget_limits', 'budget_limits.budget_id', '=', 'budget_transaction_journal.budget_id')
-                  ->leftJoin('limit_repetitions', 'limit_repetitions.budget_limit_id', '=', 'budget_limits.id')
-                  ->where('transaction_journals.date', '>=', $this->startdate->format('Y-m-d'))
-                  ->where('transaction_journals.date', '<=', $this->enddate->format('Y-m-d'))
-                  ->where('transactions.amount', '>', 0)
-                  ->where('limit_repetitions.id', '=', $this->id)
-                  ->sum('transactions.amount');
+        $sum = DB::table('transactions')
+                 ->leftJoin('transaction_journals', 'transaction_journals.id', '=', 'transactions.transaction_journal_id')
+                 ->leftJoin('budget_transaction_journal', 'budget_transaction_journal.transaction_journal_id', '=', 'transaction_journals.id')
+                 ->leftJoin('budget_limits', 'budget_limits.budget_id', '=', 'budget_transaction_journal.budget_id')
+                 ->leftJoin('limit_repetitions', 'limit_repetitions.budget_limit_id', '=', 'budget_limits.id')
+                 ->where('transaction_journals.date', '>=', $this->startdate->format('Y-m-d'))
+                 ->where('transaction_journals.date', '<=', $this->enddate->format('Y-m-d'))
+                 ->where('transaction_journals.user_id', Auth::user()->id)
+                 ->whereNull('transactions.deleted_at')
+                 ->where('transactions.amount', '>', 0)
+                 ->where('limit_repetitions.id', '=', $this->id)
+                 ->sum('transactions.amount');
 
         return floatval($sum);
     }
