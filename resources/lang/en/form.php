5b19263720ff256c369cf1f1e6a49ae79b273999
James Cole <thegrumpydictator@gmail.com>
2015-05-14 16:43:12 +0200

    Some more [skip ci]

--- resources/lang/en/form.php
+++ resources/lang/en/form.php
@@ -29,5 +29,35 @@ return [
     'amount'                      => 'Amount',
     'date'                        => 'Date',
     'category'                    => 'Category',
-    'tags'                        => 'Tags'
+    'tags'                        => 'Tags',
+    'deletePermanently'           => 'Delete permanently',
+    'cancel'                      => 'Cancel',
+
+    'delete_account'              => 'Delete account ":name"',
+    'delete_bill'                 => 'Delete bill ":name"',
+    'delete_budget'               => 'Delete budget ":name"',
+    'delete_category'             => 'Delete category ":name"',
+    'delete_currency'             => 'Delete currency ":name"',
+    'delete_piggyBank'            => 'Delete piggy banl ":name"',
+    'delete_journal'              => 'Delete transaction with description ":description"',
+
+    'account_areYouSure'          => 'Are you sure you want to delete the account named ":name"?',
+    'bill_areYouSure'             => 'Are you sure you want to delete the bill named ":name"?',
+    'budget_areYouSure'           => 'Are you sure you want to delete the budget named ":name"?',
+    'category_areYouSure'         => 'Are you sure you want to delete the category named ":name"?',
+    'currency_areYouSure'         => 'Are you sure you want to delete the currency named ":name"?',
+    'piggyBank_areYouSure'        => 'Are you sure you want to delete the piggy bank named ":name"?',
+    'journal_areYouSure'          => 'Are you sure you want to delete the transaction described ":description"?',
+
+    'permDeleteWarning'           => 'Deleting stuff from Firely is permanent and cannot be undone.',
+    'also_delete_transactions'    => 'The only transaction connected to this account will be deleted as well.' .
+                                     '|All :count transactions connected to this account will be deleted as well.',
+    'also_delete_piggyBanks'      => 'The only piggy bank connected to this account will be deleted as well.' .
+                                     '|All :count piggy bank connected to this account will be deleted as well.',
+    'bill_keep_transactions'      => 'The only transaction connected to this bill will not be deleted.' .
+                                     '|All :count transactions connected to this bill will spared deletion.',
+    'budget_keep_transactions'    => 'The only transaction connected to this budget will not be deleted.' .
+                                     '|All :count transactions connected to this budget will spared deletion.',
+    'category_keep_transactions'  => 'The only transaction connected to this category will not be deleted.' .
+                                     '|All :count transactions connected to this category will spared deletion.',
 ];
