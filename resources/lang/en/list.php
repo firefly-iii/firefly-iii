3895ae63c7cc9c273736e7becbdd33f565eb2ca8
James Cole <thegrumpydictator@gmail.com>
2015-05-14 13:00:43 +0200

    Some translations for lists [skip ci]

---
+++ resources/lang/en/list.php
@@ -0,0 +1,29 @@
+<?php
+
+// all table headers.
+
+return [
+    'name'           => 'Name',
+    'role'           => 'Role',
+    'currentBalance' => 'Current balance',
+    'active'         => 'Is active?',
+    'lastActivity'   => 'Last activity',
+    'balanceDiff'    => 'Balance difference between :start and :end',
+    'matchedOn'      => 'Matched on',
+    'matchingAmount' => 'Amount',
+    'lastMatch'      => 'Last match',
+    'expectedMatch'  => 'Expected match',
+    'automatch'      => 'Automatch?',
+    'repeat_freq'    => 'Repeats',
+    'description'    => 'Description',
+    'amount'         => 'Amount',
+    'date'           => 'Date',
+    'from'           => 'From',
+    'to'             => 'To',
+    'budget'         => 'Budget',
+    'category'       => 'Category',
+    'bill'           => 'Bill',
+    'withdrawal'     => 'Withdrawal',
+    'deposit'        => 'Deposit',
+    'transfer'       => 'Transfer'
+];
---
+++ resources/lang/nl/list.php
@@ -0,0 +1,29 @@
+<?php
+
+// all table headers.
+
+return [
+    'name'           => 'Naam',
+    'role'           => 'Rol',
+    'currentBalance' => 'Huidig saldo',
+    'active'         => 'Actief?',
+    'lastActivity'   => 'Laatste activiteit',
+    'balanceDiff'    => 'Saldoverschil tussen :start en :end',
+    'matchedOn'      => 'Wordt herkend',
+    'matchingAmount' => 'Bedrag',
+    'lastMatch'      => 'Laatste keer gezien',
+    'expectedMatch'  => 'Wordt verwacht',
+    'automatch'      => 'Automatisch herkennen?',
+    'repeat_freq'    => 'Herhaling',
+    'description'    => 'Omschrijving',
+    'amount'         => 'Bedrag',
+    'date'           => 'Datum',
+    'from'           => 'Van',
+    'to'             => 'Naar',
+    'budget'         => 'Budget',
+    'category'       => 'Categorie',
+    'bill'           => 'Rekening',
+    'withdrawal'     => 'Uitgave',
+    'deposit'        => 'Inkomsten',
+    'transfer'       => 'Overschrijving'
+];
