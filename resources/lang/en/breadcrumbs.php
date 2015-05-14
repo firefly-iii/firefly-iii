d1a34e7a6fd91b1052c522a7462180bc42ae26d5
James Cole <thegrumpydictator@gmail.com>
2015-05-14 11:22:20 +0200

    Breadcrumbs translated to English. [skip ci]

---
+++ resources/lang/en/breadcrumbs.php
@@ -0,0 +1,70 @@
+<?php
+return [
+    'home'                => 'Home',
+
+    'asset_accounts'      => 'Asset accounts',
+    'expense_accounts'    => 'Expense accounts',
+    'revenue_accounts'    => 'Revenue accounts',
+
+    'new_asset_account'   => 'New asset accounts',
+    'new_expense_account' => 'New expense account',
+    'new_revenue_account' => 'New revenue account',
+
+    'delete_account'      => 'Delete account ":name"',
+    'edit_account'        => 'Edit account ":name"',
+
+    'budgets'             => 'Budgets',
+    'newBudget'           => 'Create a new budget',
+    'delete_budget'       => 'Delete budget ":name"',
+    'edit_budget'         => 'Edit budget ":name"',
+
+    'categories'          => 'Categories',
+    'newCategory'         => 'Create a new categori',
+    'delete_category'     => 'Delete category ":name"',
+    'edit_category'       => 'Edit category ":name"',
+
+    'currencies'          => 'Currencies',
+    'edit_currency'       => 'Edit currencies ":name"',
+    'delete_currency'     => 'Delete currencies ":name"',
+
+    'piggyBanks'          => 'Piggy banks',
+    'newPiggyBank'        => 'Create a new piggy bank',
+    'edit_piggyBank'      => 'Edit piggy bank ":name"',
+    'delete_piggyBank'    => 'Delete piggy bank ":name"',
+
+    'preferences'         => 'Preferences',
+    'profile'             => 'Profile',
+    'changePassword'      => 'Change your password',
+
+    'bills'               => 'Bills',
+    'newBill'             => 'New bill',
+    'edit_bill'           => 'Edit bill ":name"',
+    'delete_bill'         => 'Delete bill ":name"',
+
+    'reminders'           => 'Reminders',
+    'reminder'            => 'Reminder #:id',
+
+    'reports'             => 'Reports',
+    'monthly_report'      => 'Montly report for :date',
+    'yearly_report'       => 'Yearly report for :date',
+    'budget_report'       => 'Budget report for :date',
+
+    'searchResult'        => 'Search for ":query"',
+
+    'withdrawal_list'     => 'Expenses',
+    'deposit_list'        => 'Revenue, income and deposits',
+    'transfer_list'       => 'Transfers',
+
+    'create_withdrawal'   => 'Create new withdrawal',
+    'create_deposit'      => 'Create new deposit',
+    'create_transfer'     => 'Create new transfer',
+
+    'edit_journal'        => 'Edit transaction ":description"',
+    'delete_journal'      => 'Delete transaction ":description"',
+
+    'tags'                => 'Tags',
+    'createTag'           => 'Create new tag',
+    'edit_tag'            => 'Edit tag ":tag"',
+    'delete_tag'          => 'Delete tag ":tag"',
+
+];
