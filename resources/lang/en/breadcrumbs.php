42970aea809e6e37f3add518af949217a0ff0cc3
James Cole <thegrumpydictator@gmail.com>
2015-05-21 17:45:01 +0200

    Fix some translations.

--- resources/lang/en/breadcrumbs.php
+++ resources/lang/en/breadcrumbs.php
@@ -2,75 +2,85 @@
 return [
     'home'                  => 'Home',
 
+    // accounts
     'asset_accounts'        => 'Asset accounts',
     'expense_accounts'      => 'Expense accounts',
     'revenue_accounts'      => 'Revenue accounts',
     'cash_accounts'         => 'Cash accounts',
-
     'new_asset_account'     => 'New asset accounts',
     'new_expense_account'   => 'New expense account',
     'new_revenue_account'   => 'New revenue account',
-
     'delete_account'        => 'Delete account ":name"',
     'edit_account'          => 'Edit account ":name"',
-
     'edit_asset_account'    => 'Edit asset account ":name"',
     'edit_expense_account'  => 'Edit expense account ":name"',
     'edit_revenue_account'  => 'Edit revenue account ":name"',
 
+    // budgets
     'budgets'               => 'Budgets',
     'newBudget'             => 'Create a new budget',
     'delete_budget'         => 'Delete budget ":name"',
     'edit_budget'           => 'Edit budget ":name"',
 
+    // categories
     'categories'            => 'Categories',
     'newCategory'           => 'Create a new categori',
     'delete_category'       => 'Delete category ":name"',
     'edit_category'         => 'Edit category ":name"',
 
+    // currencies
     'currencies'            => 'Currencies',
     'edit_currency'         => 'Edit currencies ":name"',
     'delete_currency'       => 'Delete currencies ":name"',
 
+    // piggy banks
     'piggyBanks'            => 'Piggy banks',
     'newPiggyBank'          => 'Create a new piggy bank',
     'edit_piggyBank'        => 'Edit piggy bank ":name"',
     'delete_piggyBank'      => 'Delete piggy bank ":name"',
 
+    // top menu
     'preferences'           => 'Preferences',
     'profile'               => 'Profile',
     'changePassword'        => 'Change your password',
 
+    // bills
     'bills'                 => 'Bills',
     'newBill'               => 'New bill',
     'edit_bill'             => 'Edit bill ":name"',
     'delete_bill'           => 'Delete bill ":name"',
 
+    // reminders
     'reminders'             => 'Reminders',
     'reminder'              => 'Reminder #:id',
 
+    // reports
     'reports'               => 'Reports',
     'monthly_report'        => 'Montly report for :date',
     'monthly_report_shared' => 'Montly report for :date (including shared accounts)',
     'yearly_report'         => 'Yearly report for :date',
     'yearly_report_shared'  => 'Yearly report for :date (including shared accounts)',
-
     'budget_report'         => 'Budget report for :date',
 
+    // search
     'searchResult'          => 'Search for ":query"',
 
+    // transaction lists.
     'withdrawal_list'       => 'Expenses',
     'deposit_list'          => 'Revenue, income and deposits',
     'transfer_list'         => 'Transfers',
     'transfers_list'        => 'Transfers',
 
+    // create transactions
     'create_withdrawal'     => 'Create new withdrawal',
     'create_deposit'        => 'Create new deposit',
     'create_transfer'       => 'Create new transfer',
 
+    // edit transactions
     'edit_journal'          => 'Edit transaction ":description"',
     'delete_journal'        => 'Delete transaction ":description"',
 
+    // tags
     'tags'                  => 'Tags',
     'createTag'             => 'Create new tag',
     'edit_tag'              => 'Edit tag ":tag"',
