94be5244fe7568dca94bf9a54f70334ea524ae33
James Cole <thegrumpydictator@gmail.com>
2015-06-27 20:52:06 +0200

    Final chart. First version.

---
+++ app/Generator/Chart/PiggyBank/ChartJsPiggyBankChartGenerator.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace FireflyIII\Generator\Chart\PiggyBank;
+
+use Carbon\Carbon;
+use Config;
+use Illuminate\Support\Collection;
+use Preferences;
+
+
+/**
+ * Class GooglePiggyBankChartGenerator
+ *
+ * @package FireflyIII\Generator\Chart\PiggyBank
+ */
+class ChartJsPiggyBankChartGenerator implements PiggyBankChartGenerator
+{
+
+    /**
+     * @param Collection $set
+     *
+     * @return array
+     */
+    public function history(Collection $set)
+    {
+
+        // language:
+        $language = Preferences::get('language', 'en')->data;
+        $format   = Config::get('firefly.month.' . $language);
+
+        $data = [
+            'count'    => 1,
+            'labels'   => [],
+            'datasets' => [
+                [
+                    'label' => 'Diff',
+                    'data'  => []
+                ]
+            ],
+        ];
+        $sum  = '0';
+        bcscale(2);
+        foreach ($set as $entry) {
+            $date                          = new Carbon($entry->date);
+            $sum                           = bcadd($sum, $entry->sum);
+            $data['labels'][]              = $date->formatLocalized($format);
+            $data['datasets'][0]['data'][] = round($sum, 2);
+        }
+
+        return $data;
+    }
+}
