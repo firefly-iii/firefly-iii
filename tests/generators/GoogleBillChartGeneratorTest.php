0258982e60031d8be7e997a9601d04d5cc1025c7
James Cole <thegrumpydictator@gmail.com>
2015-06-28 20:56:04 +0200

    Implemented two google chart tests.

--- tests/generators/GoogleBillChartGeneratorTest.php
+++ tests/generators/GoogleBillChartGeneratorTest.php
@@ -1,10 +1,19 @@
 <?php
 
+use Carbon\Carbon;
+use FireflyIII\Generator\Chart\Bill\GoogleBillChartGenerator;
+use Illuminate\Support\Collection;
+use League\FactoryMuffin\Facade as FactoryMuffin;
+
+
 /**
  * Class GoogleBillChartGeneratorTest
  */
 class GoogleBillChartGeneratorTest extends TestCase
 {
+
+    /** @var GoogleBillChartGenerator */
+    protected $object;
 
     /**
      * Sets up the fixture, for example, opens a network connection.
@@ -13,6 +22,8 @@ class GoogleBillChartGeneratorTest extends TestCase
     public function setUp()
     {
         parent::setUp();
+
+        $this->object = new GoogleBillChartGenerator;
 
     }
 
@@ -31,7 +42,30 @@ class GoogleBillChartGeneratorTest extends TestCase
      */
     public function testFrontpage()
     {
-        $this->markTestIncomplete();
+        // to test frontpage, we generate the exact fake entries
+        // needed:
+        $paid = new Collection;
+        for ($i = 0; $i < 5; $i++) {
+            $obj              = new stdClass();
+            $obj->description = 'Something';
+            $obj->amount      = 100;
+            $paid->push($obj);
+        }
+
+        $unpaid = new Collection;
+        $sum    = 0;
+        for ($i = 0; $i < 5; $i++) {
+            $bill = FactoryMuffin::create('FireflyIII\Models\Bill');
+            $date = new Carbon;
+            $sum += (($bill->amount_max + $bill->amount_min) / 2);
+            $unpaid->push([$bill, $date]);
+        }
+
+
+        $data = $this->object->frontpage($paid, $unpaid);
+
+        $this->assertCount(2, $data['cols']);
+        $this->assertCount(2, $data['rows']); // two rows, two columns.
     }
 
     /**
@@ -39,6 +73,18 @@ class GoogleBillChartGeneratorTest extends TestCase
      */
     public function testSingle()
     {
-        $this->markTestIncomplete();
+        $bill    = FactoryMuffin::create('FireflyIII\Models\Bill');
+        $entries = new Collection;
+        for ($i = 0; $i < 5; $i++) {
+            $obj         = new stdClass;
+            $obj->amount = 100;
+            $obj->date   = new Carbon;
+            $entries->push($obj);
+        }
+        $data = $this->object->single($bill, $entries);
+
+        $this->assertCount(5, $data['rows']);
+        $this->assertCount(4, $data['cols']);
+        $this->assertEquals(100, $data['rows'][0]['c'][3]['v']);
     }
 }
