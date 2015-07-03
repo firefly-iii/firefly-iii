24e7c68243ace3a89013ad5c79421e4de93d5f95
James Cole <thegrumpydictator@gmail.com>
2015-07-03 08:15:30 +0200

    Initial codeception installation.

---
+++ tests/_support/Helper/Acceptance.php
@@ -0,0 +1,9 @@
+<?php
+namespace Helper;
+// here you can define custom actions
+// all public methods declared in helper class will be available in $I
+
+class Acceptance extends \Codeception\Module
+{
+
+}
