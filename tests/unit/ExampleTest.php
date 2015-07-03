ad6700c1145e61fa6b42dd7b6a4beecea7a522c9
James Cole <thegrumpydictator@gmail.com>
2015-07-03 10:28:40 +0200

    An example test. Will continue later.

---
+++ tests/unit/ExampleTest.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Class ExampleTest
+ */
+class ExampleTest extends \PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+    }
+
+    protected function tearDown()
+    {
+    }
+
+    // tests
+    public function testMe()
+    {
+    }
+}
