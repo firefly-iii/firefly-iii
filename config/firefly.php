5153591c8f810d0463c7bc3c19dbd68ff2dcdc96
James Cole <thegrumpydictator@gmail.com>
2015-07-07 10:38:53 +0200

    New version.

--- config/firefly.php
+++ config/firefly.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'chart'                    => 'chartjs', // or 'chartjs'
-    'version'                  => '3.4.6',
+    'chart'                    => 'chartjs',
+    'version'                  => '3.4.7',
     'index_periods'            => ['1D', '1W', '1M', '3M', '6M', '1Y', 'custom'],
     'budget_periods'           => ['daily', 'weekly', 'monthly', 'quarterly', 'half-year', 'yearly'],
     'csv_import_enabled'       => true,
