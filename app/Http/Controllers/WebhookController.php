868fe469327171e30dc46487447bebca97d30e35
James Cole <thegrumpydictator@gmail.com>
2015-07-22 18:44:51 +0200

    Some more debug stuff.

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -18,7 +18,9 @@ class WebhookController extends Controller
     public function sendgrid()
     {
         var_dump($_POST);
-        Log::debug(print_r($_POST,true));
+        Log::debug(print_r($_POST, true));
+        Log::debug(print_r($_GET, true));
+        Log::debug(print_r($_SERVER, true));
 
     }
 
