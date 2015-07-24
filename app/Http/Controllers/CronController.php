d1d4a5293453f7f4c844175878893e294c07980c
James Cole <thegrumpydictator@gmail.com>
2015-07-24 13:23:02 +0200

    Catch empty send grid credentials.

--- app/Http/Controllers/CronController.php
+++ app/Http/Controllers/CronController.php
@@ -23,27 +23,30 @@ class CronController extends Controller
     public function sendgrid()
     {
 
-        $URL        = 'https://api.sendgrid.com/api/bounces.get.json';
-        $parameters = [
-            'api_user' => env('SENDGRID_USERNAME'),
-            'api_key'  => env('SENDGRID_PASSWORD'),
-            'date'     => 1,
-            'days'     => 7
-        ];
-        $fullURL    = $URL . '?' . http_build_query($parameters);
-        $data       = json_decode(file_get_contents($fullURL));
+        if (strlen(env('SENDGRID_USERNAME')) > 0 && strlen(env('SENDGRID_PASSWORD')) > 0) {
 
-        /*
-         * Loop the result, if any.
-         */
-        if (is_array($data)) {
-            foreach ($data as $entry) {
-                $address = $entry->email;
-                $user    = User::where('email', $address)->where('blocked', 0)->first();
-                if (!is_null($user)) {
-                    $user->blocked  = 1;
-                    $user->password = 'bounced';
-                    $user->save();
+            $URL        = 'https://api.sendgrid.com/api/bounces.get.json';
+            $parameters = [
+                'api_user' => env('SENDGRID_USERNAME'),
+                'api_key'  => env('SENDGRID_PASSWORD'),
+                'date'     => 1,
+                'days'     => 7
+            ];
+            $fullURL    = $URL . '?' . http_build_query($parameters);
+            $data       = json_decode(file_get_contents($fullURL));
+
+            /*
+             * Loop the result, if any.
+             */
+            if (is_array($data)) {
+                foreach ($data as $entry) {
+                    $address = $entry->email;
+                    $user    = User::where('email', $address)->where('blocked', 0)->first();
+                    if (!is_null($user)) {
+                        $user->blocked  = 1;
+                        $user->password = 'bounced';
+                        $user->save();
+                    }
                 }
             }
         }
