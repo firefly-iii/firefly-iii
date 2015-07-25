f4ade470dff30a49b4430f1935d21d6445b41df6
James Cole <thegrumpydictator@gmail.com>
2015-07-25 18:48:48 +0200

    Remove if statements.

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,13 +13,6 @@ class VerifyCsrfToken extends BaseVerifier
 {
 
     /**
-     * Routes we want to exclude from CSRF.
-     *
-     * @var array
-     */
-    protected $routes = [];
-
-    /**
      * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request $request
@@ -29,29 +22,6 @@ class VerifyCsrfToken extends BaseVerifier
      */
     public function handle($request, Closure $next)
     {
-        if ($this->excludedRoutes($request)) {
-            return $next($request);
-        }
-
         return parent::handle($request, $next);
     }
-
-    /**
-     * This will return a bool value based on route checking.
-     *
-     * @param  Request $request
-     *
-     * @return boolean
-     */
-    protected function excludedRoutes($request)
-    {
-        foreach ($this->routes as $route) {
-            if ($request->is($route)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
 }
