4b4c1c7f8f529936103453a129b41564e0fc72dd
James Cole <thegrumpydictator@gmail.com>
2015-12-11 08:40:45 +0100

    Moved some translations to see if they will still be picked up by Laravel.

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -29,12 +29,10 @@ return [
     "before"                  => "The :attribute must be a date before :date.",
     'unique_object_for_user'  => 'This name is already in use',
     'unique_account_for_user' => 'This account name is already in use',
-    "between"                 => [
-        "numeric" => "The :attribute must be between :min and :max.",
-        "file"    => "The :attribute must be between :min and :max kilobytes.",
-        "string"  => "The :attribute must be between :min and :max characters.",
-        "array"   => "The :attribute must have between :min and :max items.",
-    ],
+    "between.numeric"         => "The :attribute must be between :min and :max.",
+    "between.file"            => "The :attribute must be between :min and :max kilobytes.",
+    "between.string"          => "The :attribute must be between :min and :max characters.",
+    "between.array"           => "The :attribute must have between :min and :max items.",
     "boolean"                 => "The :attribute field must be true or false.",
     "confirmed"               => "The :attribute confirmation does not match.",
     "date"                    => "The :attribute is not a valid date.",
@@ -49,19 +47,15 @@ return [
     "in"                      => "The selected :attribute is invalid.",
     "integer"                 => "The :attribute must be an integer.",
     "ip"                      => "The :attribute must be a valid IP address.",
-    "max"                     => [
-        "numeric" => "The :attribute may not be greater than :max.",
-        "file"    => "The :attribute may not be greater than :max kilobytes.",
-        "string"  => "The :attribute may not be greater than :max characters.",
-        "array"   => "The :attribute may not have more than :max items.",
-    ],
+    "max.numeric"             => "The :attribute may not be greater than :max.",
+    "max.file"                => "The :attribute may not be greater than :max kilobytes.",
+    "max.string"              => "The :attribute may not be greater than :max characters.",
+    "max.array"               => "The :attribute may not have more than :max items.",
     "mimes"                   => "The :attribute must be a file of type: :values.",
-    "min"                     => [
-        "numeric" => "The :attribute must be at least :min.",
-        "file"    => "The :attribute must be at least :min kilobytes.",
-        "string"  => "The :attribute must be at least :min characters.",
-        "array"   => "The :attribute must have at least :min items.",
-    ],
+    "min.numeric"             => "The :attribute must be at least :min.",
+    "min.file"                => "The :attribute must be at least :min kilobytes.",
+    "min.string"              => "The :attribute must be at least :min characters.",
+    "min.array"               => "The :attribute must have at least :min items.",
     "not_in"                  => "The selected :attribute is invalid.",
     "numeric"                 => "The :attribute must be a number.",
     "regex"                   => "The :attribute format is invalid.",
@@ -72,44 +66,13 @@ return [
     "required_without"        => "The :attribute field is required when :values is not present.",
     "required_without_all"    => "The :attribute field is required when none of :values are present.",
     "same"                    => "The :attribute and :other must match.",
-    "size"                    => [
-        "numeric" => "The :attribute must be :size.",
-        "file"    => "The :attribute must be :size kilobytes.",
-        "string"  => "The :attribute must be :size characters.",
-        "array"   => "The :attribute must contain :size items.",
-    ],
+    "size.numeric"            => "The :attribute must be :size.",
+    "size.file"               => "The :attribute must be :size kilobytes.",
+    "size.string"             => "The :attribute must be :size characters.",
+    "size.array"              => "The :attribute must contain :size items.",
     "unique"                  => "The :attribute has already been taken.",
     "url"                     => "The :attribute format is invalid.",
     "timezone"                => "The :attribute must be a valid zone.",
-
-    /*
-    |--------------------------------------------------------------------------
-    | Custom Validation Language Lines
-    |--------------------------------------------------------------------------
-    |
-    | Here you may specify custom validation messages for attributes using the
-    | convention "attribute.rule" to name the lines. This makes it quick to
-    | specify a specific custom language line for a given attribute rule.
-    |
-    */
-
-    'custom' => [
-        'attribute-name' => [
-            'rule-name' => 'custom-message',
-        ],
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Custom Validation Attributes
-    |--------------------------------------------------------------------------
-    |
-    | The following language lines are used to swap attribute place-holders
-    | with something more reader friendly such as E-Mail Address instead
-    | of "email". This simply helps us make messages a little cleaner.
-    |
-    */
-
     'attributes' => [],
 
 ];
