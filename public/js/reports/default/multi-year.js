af994e4daec8589835b8255aa122ec9a96244d0d
James Cole <thegrumpydictator@gmail.com>
2015-12-14 21:12:10 +0100

    Included first multi-year chart.

--- public/js/reports/default/multi-year.js
+++ public/js/reports/default/multi-year.js
@@ -12,4 +12,15 @@ $(function () {
 function drawChart() {
     "use strict";
 
+    // income and expense over multi year:
+    columnChart('chart/report/in-out/' + reportType + '/' + startDate + '/' + endDate + '/' + accountIds, 'income-expenses-chart');
+    columnChart('chart/report/in-out-sum/' + reportType + '/' + startDate + '/' + endDate + '/' + accountIds, 'income-expenses-sum-chart');
+
+
+    $.each($('.account-chart'), function(i,v) {
+        var holder = $(v);
+        console.log(holder.data('id'));
+    });
+
+
 }
