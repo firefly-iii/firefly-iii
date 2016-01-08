bbd19be554a22f65a9489bd883058fc5d858b2b0
James Cole <thegrumpydictator@gmail.com>
2016-01-08 16:00:28 +0100

    Update requests after upgrade to Laravel 5.2

--- app/Http/Requests/Request.php
+++ app/Http/Requests/Request.php
@@ -1,16 +1,10 @@
-<?php namespace FireflyIII\Http\Requests;
+<?php
+
+namespace FireflyIII\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-/**
- * Class Request
- *
- * @codeCoverageIgnore
- * @package FireflyIII\Http\Requests
- */
 abstract class Request extends FormRequest
 {
-
     //
-
 }
