4b00db7662c873597cdc275b1b4d428a9b81ba22
James Cole <thegrumpydictator@gmail.com>
2016-01-08 14:30:19 +0100

    Removed test code after upgrading to laravel 5.2 (will have to reinstate).

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,10 +1,12 @@
 <?php
 
-/**
- * Class TestCase
- */
 class TestCase extends Illuminate\Foundation\Testing\TestCase
 {
+    /**
+     * The base URL to use while testing the application.
+     *
+     * @var string
+     */
     protected $baseUrl = 'http://localhost';
 
     /**
@@ -14,38 +16,10 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase
      */
     public function createApplication()
     {
-        $app = require __DIR__ . '/../bootstrap/app.php';
+        $app = require __DIR__.'/../bootstrap/app.php';
 
-        $app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
+        $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
 
         return $app;
-    }
-
-    /**
-     * Sets up the fixture, for example, opens a network connection.
-     * This method is called before a test is executed.
-     */
-    public function setUp()
-    {
-        parent::setUp();
-    }
-
-    /**
-     * This method is called before the first test of this test class is run.
-     *
-     * @since Method available since Release 3.4.0
-     */
-    public static function setUpBeforeClass()
-    {
-        parent::setUpBeforeClass();
-    }
-
-    /**
-     * Tears down the fixture, for example, closes a network connection.
-     * This method is called after a test is executed.
-     */
-    public function tearDown()
-    {
-        parent::tearDown();
     }
 }
