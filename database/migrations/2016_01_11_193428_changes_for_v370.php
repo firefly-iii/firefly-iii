38895afea6361d1665c148b7995a226119a023b4
James Cole <thegrumpydictator@gmail.com>
2016-01-11 20:42:08 +0100

    New migration (empty)

---
+++ database/migrations/2016_01_11_193428_changes_for_v370.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * 2016_01_11_193428_changes_for_v370.php
+ * Copyright (C) 2016 Sander Dorigo
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * Class ChangesForV370
+ */
+class ChangesForV370 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
