70ed4188b6f7e9d283998ad4239641cf26f04a67
James Cole <thegrumpydictator@gmail.com>
2016-01-11 20:41:57 +0100

    New controller for rules.

---
+++ app/Http/Controllers/RuleController.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * RuleController.php
+ * Copyright (C) 2016 Sander Dorigo
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
+namespace FireflyIII\Http\Controllers;
+
+use FireflyIII\Http\Requests;
+use View;
+
+/**
+ * Class RuleController
+ *
+ * @package FireflyIII\Http\Controllers
+ */
+class RuleController extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        View::share('title', trans('firefly.rules'));
+        View::share('mainTitleIcon', 'fa-random');
+    }
+
+
+    public function index()
+    {
+        return view('rules.index');
+    }
+}
