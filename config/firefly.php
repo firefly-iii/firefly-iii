f9703eca4c107c36d8082313a98d206a8da12e6e
James Cole <thegrumpydictator@gmail.com>
2016-01-12 20:56:36 +0100

    Removed double configuration.

--- config/firefly.php
+++ config/firefly.php
@@ -168,28 +168,6 @@ return [
         'end_date'        => 'FireflyIII\Support\Binder\Date'
     ],
 
-
-    'bindables'     => [
-        // models
-        'account'         => 'FireflyIII\Models\Account',
-        'attachment'      => 'FireflyIII\Models\Attachment',
-        'bill'            => 'FireflyIII\Models\Bill',
-        'budget'          => 'FireflyIII\Models\Budget',
-        'category'        => 'FireflyIII\Models\Category',
-        'currency'        => 'FireflyIII\Models\TransactionCurrency',
-        'limitrepetition' => 'FireflyIII\Models\LimitRepetition',
-        'piggyBank'       => 'FireflyIII\Models\PiggyBank',
-        'tj'              => 'FireflyIII\Models\TransactionJournal',
-        'tag'             => 'FireflyIII\Models\Tag',
-        // lists
-        'accountList'     => 'FireflyIII\Support\Binder\AccountList',
-        'budgetList'      => 'FireflyIII\Support\Binder\BudgetList',
-        'categoryList'    => 'FireflyIII\Support\Binder\CategoryList',
-
-        // others
-        'start_date'      => 'FireflyIII\Support\Binder\Date',
-        'end_date'        => 'FireflyIII\Support\Binder\Date'
-    ],
     'rule-triggers' => [
         'from_account_starts'   => 'FireflyIII\Rules\Triggers',
         'from_account_ends'     => 'FireflyIII\Rules\Triggers',
