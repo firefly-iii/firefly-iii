35f493beff2ed61689c4385452a7f4ea29f36a70
James Cole <thegrumpydictator@gmail.com>
2016-01-12 20:37:47 +0100

    Move domain stuff to configuration.

--- app/Support/Domain.php
+++ app/Support/Domain.php
@@ -9,6 +9,8 @@
 
 namespace FireflyIII\Support;
 
+use Config;
+
 /**
  * Class Domain
  *
@@ -21,29 +23,7 @@ class Domain
      */
     public static function getBindables()
     {
-        return [
-            // models
-            'account'         => 'FireflyIII\Models\Account',
-            'attachment'      => 'FireflyIII\Models\Attachment',
-            'bill'            => 'FireflyIII\Models\Bill',
-            'budget'          => 'FireflyIII\Models\Budget',
-            'category'        => 'FireflyIII\Models\Category',
-            'currency'        => 'FireflyIII\Models\TransactionCurrency',
-            'limitrepetition' => 'FireflyIII\Models\LimitRepetition',
-            'piggyBank'       => 'FireflyIII\Models\PiggyBank',
-            'tj'              => 'FireflyIII\Models\TransactionJournal',
-            'tag'             => 'FireflyIII\Models\Tag',
-            // lists
-            'accountList'     => 'FireflyIII\Support\Binder\AccountList',
-            'budgetList'      => 'FireflyIII\Support\Binder\BudgetList',
-            'categoryList'    => 'FireflyIII\Support\Binder\CategoryList',
-
-            // others
-            'start_date'      => 'FireflyIII\Support\Binder\Date',
-            'end_date'        => 'FireflyIII\Support\Binder\Date'
-        ];
-
+        return Config::get('firefly.bindables');
 
     }
-
 }
