3354d53a4804394e28567ac0847182b3abe9d7ff
James Cole <thegrumpydictator@gmail.com>
2016-01-19 18:09:53 +0100

    Reset phpunit xml.

--- pu.sh
+++ pu.sh
@@ -3,6 +3,9 @@
 # set testing environment
 cp .env.testing .env
 
+# set default phpunit.
+cp phpunit.default.xml phpunit.xml
+
 # delete test databases:
 if [ -f storage/database/testing.db ]
 then
