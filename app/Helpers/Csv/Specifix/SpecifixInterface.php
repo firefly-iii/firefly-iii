ea6c54cad08d108acafcca657bde38966f973e65
James Cole <thegrumpydictator@gmail.com>
2016-01-20 21:23:39 +0100

    Expand specifix to have a property that determines whether they fire before anything happens, or after. Could be expanded to allow for even more fine-grained control. See issue #145

--- app/Helpers/Csv/Specifix/SpecifixInterface.php
+++ app/Helpers/Csv/Specifix/SpecifixInterface.php
@@ -8,15 +8,29 @@ namespace FireflyIII\Helpers\Csv\Specifix;
  */
 interface SpecifixInterface
 {
+    const PRE_PROCESSOR  = 1;
+    const POST_PROCESSOR = 2;
     /**
      * Implement bank and locale related fixes.
      */
     public function fix();
 
     /**
+     * @return int
+     */
+    public function getProcessorType();
+
+    /**
      * @param array $data
      */
     public function setData($data);
+
+    /**
+     * @param int $processorType
+     *
+     * @return $this
+     */
+    public function setProcessorType($processorType);
 
     /**
      * @param array $row
