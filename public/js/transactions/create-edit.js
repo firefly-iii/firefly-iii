3a12ad192fac1e2ddcd42e4709ce92cc9036feb7
James Cole <thegrumpydictator@gmail.com>
2016-01-29 18:39:50 +0100

    Move more javascript around.

---
+++ public/js/transactions/create-edit.js
@@ -0,0 +1,47 @@
+/*
+ * create-edit.js
+ * Copyright (C) 2016 Sander Dorigo
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+/* globals what:true, $, doSwitch, txt, middleCrumbName, title,button, middleCrumbUrl, piggiesLength, breadcrumbs */
+$(document).ready(function () {
+    "use strict";
+    if ($('input[name="expense_account"]').length > 0) {
+        $.getJSON('json/expense-accounts').success(function (data) {
+            $('input[name="expense_account"]').typeahead({source: data});
+        });
+    }
+
+    if ($('input[name="tags"]').length > 0) {
+        $.getJSON('json/tags').success(function (data) {
+            var opt = {
+                typeahead: {
+                    source: data
+                }
+            };
+            $('input[name="tags"]').tagsinput(
+                opt
+            );
+        });
+    }
+
+    if ($('input[name="revenue_account"]').length > 0) {
+        $.getJSON('json/revenue-accounts').success(function (data) {
+            $('input[name="revenue_account"]').typeahead({source: data});
+        });
+    }
+
+    if ($('input[name="description"]').length > 0 && what !== undefined) {
+        $.getJSON('json/transaction-journals/' + what).success(function (data) {
+            $('input[name="description"]').typeahead({source: data});
+        });
+    }
+
+    if ($('input[name="category"]').length > 0) {
+        $.getJSON('json/categories').success(function (data) {
+            $('input[name="category"]').typeahead({source: data});
+        });
+    }
+});
