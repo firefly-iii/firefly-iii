706b095f95fb5acc27adf140cc232d5db57595cb
James Cole <thegrumpydictator@gmail.com>
2016-02-12 17:50:12 +0100

    Forgot to actually return the created array.

--- app/Http/Requests/TagFormRequest.php
+++ app/Http/Requests/TagFormRequest.php
@@ -27,30 +27,6 @@ class TagFormRequest extends Request
     /**
      * @return array
      */
-    public function rules()
-    {
-        $idRule  = '';
-        $tagRule = 'required|min:1|uniqueObjectForUser:tags,tag';
-        if (Tag::find(Input::get('id'))) {
-            $idRule  = 'belongsToUser:tags';
-            $tagRule = 'required|min:1|uniqueObjectForUser:tags,tag,' . Input::get('id');
-        }
-
-        return [
-            'tag'         => $tagRule,
-            'id'          => $idRule,
-            'description' => 'min:1',
-            'date'        => 'date',
-            'latitude'    => 'numeric|min:-90|max:90',
-            'longitude'   => 'numeric|min:-90|max:90',
-            'zoomLevel'   => 'numeric|min:0|max:80',
-            'tagMode'     => 'required|in:nothing,balancingAct,advancePayment',
-        ];
-    }
-
-    /**
-     * @return array
-     */
     public function collectTagData() :array
     {
         if (Input::get('setTag') == 'true') {
@@ -74,7 +50,32 @@ class TagFormRequest extends Request
             'tagMode'     => $this->get('tagMode'),
         ];
 
+        return $data;
 
 
+    }
+
+    /**
+     * @return array
+     */
+    public function rules()
+    {
+        $idRule  = '';
+        $tagRule = 'required|min:1|uniqueObjectForUser:tags,tag';
+        if (Tag::find(Input::get('id'))) {
+            $idRule  = 'belongsToUser:tags';
+            $tagRule = 'required|min:1|uniqueObjectForUser:tags,tag,' . Input::get('id');
+        }
+
+        return [
+            'tag'         => $tagRule,
+            'id'          => $idRule,
+            'description' => 'min:1',
+            'date'        => 'date',
+            'latitude'    => 'numeric|min:-90|max:90',
+            'longitude'   => 'numeric|min:-90|max:90',
+            'zoomLevel'   => 'numeric|min:0|max:80',
+            'tagMode'     => 'required|in:nothing,balancingAct,advancePayment',
+        ];
     }
 }
