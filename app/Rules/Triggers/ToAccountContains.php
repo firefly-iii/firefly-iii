a6c318983350ebcb101cdf04919153c5bc9c2b54
James Cole <thegrumpydictator@gmail.com>
2016-02-17 18:00:13 +0100

    Add willMatchEveryThing as a static replacement for matchesAnything

--- app/Rules/Triggers/ToAccountContains.php
+++ app/Rules/Triggers/ToAccountContains.php
@@ -73,4 +73,29 @@ class ToAccountContains implements TriggerInterface
 
     }
 
+
+    /**
+     * A trigger is said to "match anything", or match any given transaction,
+     * when the trigger value is very vague or has no restrictions. Easy examples
+     * are the "AmountMore"-trigger combined with an amount of 0: any given transaction
+     * has an amount of more than zero! Other examples are all the "Description"-triggers
+     * which have hard time handling empty trigger values such as "" or "*" (wild cards).
+     *
+     * If the user tries to create such a trigger, this method MUST return true so Firefly III
+     * can stop the storing / updating the trigger. If the trigger is in any way restrictive
+     * (even if it will still include 99.9% of the users transactions), this method MUST return
+     * false.
+     *
+     * @param null $value
+     *
+     * @return bool
+     */
+    public static function willMatchEverything($value = null)
+    {
+        if (!is_null($value)) {
+            return strval($value) === "";
+        }
+
+        return true;
+    }
 }
