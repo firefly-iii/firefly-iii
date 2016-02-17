ddfbd69e8b9c913759e28ec2d101d952511ee0f2
James Cole <thegrumpydictator@gmail.com>
2016-02-17 18:23:12 +0100

    Minimum of one trigger.

--- app/Http/Requests/TestRuleFormRequest.php
+++ app/Http/Requests/TestRuleFormRequest.php
@@ -46,7 +46,7 @@ class TestRuleFormRequest extends Request
 
         $validTriggers = array_keys(Config::get('firefly.rule-triggers'));
         $rules         = [
-            'rule-trigger.*'       => 'required|in:' . join(',', $validTriggers),
+            'rule-trigger.*'       => 'required|min:1|in:' . join(',', $validTriggers),
             'rule-trigger-value.*' => 'required|min:1|ruleTriggerValue',
         ];
 
