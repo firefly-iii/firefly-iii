3277858c5a38399478ecb25b67a16508d1f06c07
James Cole <thegrumpydictator@gmail.com>
2016-04-01 16:44:46 +0200

    Code cleanup. [skip ci]

--- app/Support/Migration/TestData.php
+++ app/Support/Migration/TestData.php
@@ -4,7 +4,7 @@ namespace FireflyIII\Support\Migration;
 
 /**
  * TestData.php
- * Copyright (C) 2016 Sander Dorigo
+ * Copyright (C) 2016 thegrumpydictator@gmail.com
  *
  * This software may be modified and distributed under the terms
  * of the MIT license.  See the LICENSE file for details.
