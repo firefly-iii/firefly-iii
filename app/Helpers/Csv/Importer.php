cce4ef19e55b9da50f089760da62eb7aa5cdd4c2
James Cole <thegrumpydictator@gmail.com>
2016-04-18 19:34:15 +0200

    Fix #244

--- app/Helpers/Csv/Importer.php
+++ app/Helpers/Csv/Importer.php
@@ -28,7 +28,7 @@ class Importer
     /** @var Data */
     protected $data;
     /** @var array */
-    protected $errors;
+    protected $errors = [];
     /** @var  array */
     protected $importData;
     /** @var  array */
