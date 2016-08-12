9850220aacc2d5e4df54490bdcf59625fc8ff85e
James Cole <thegrumpydictator@gmail.com>
2016-08-12 10:46:22 +0200

    Add new languages.

--- config/firefly.php
+++ config/firefly.php
@@ -94,6 +94,8 @@ return [
         'nl_NL' => ['name_locale' => 'Nederlands', 'name_english' => 'Dutch', 'complete' => true],
         'pt_BR' => ['name_locale' => 'Português do Brasil', 'name_english' => 'Portuguese (Brazil)', 'complete' => true],
         'fr_FR' => ['name_locale' => 'Français', 'name_english' => 'French', 'complete' => false],
+        'zh-HK' => ['name_locale' => 'Chinese Traditional, Hong Kong', 'name_english' => 'Chinese Traditional, Hong Kong', 'complete' => false],
+        'zh-TW' => ['name_locale' => 'Chinese Traditional', 'name_english' => 'Chinese Traditional', 'complete' => false],
     ],
     'transactionTypesByWhat'   => [
         'expenses'   => ['Withdrawal'],
