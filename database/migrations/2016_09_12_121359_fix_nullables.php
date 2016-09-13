3fe831c7d804ed32c017bea509ad636a7ae174c8
James Cole <thegrumpydictator@gmail.com>
2016-09-13 19:20:09 +0200

    Bit of code cleanup courtesy of PHPStorm.

--- database/migrations/2016_09_12_121359_fix_nullables.php
+++ database/migrations/2016_09_12_121359_fix_nullables.php
@@ -1,31 +1,13 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 
-class FixNullables extends Migration {
-
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::table(
-            'rule_groups', function (Blueprint $table)
-        {
-            $table->text('description')->nullable()->change();
-        }
-        );
-
-        Schema::table(
-            'rules', function (Blueprint $table)
-        {
-            $table->text('description')->nullable()->change();
-        }
-        );
-    }
+/**
+ * Class FixNullables
+ */
+class FixNullables extends Migration
+{
 
     /**
      * Reverse the migrations.
@@ -35,5 +17,25 @@ class FixNullables extends Migration {
     public function down()
     {
 
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table(
+            'rule_groups', function (Blueprint $table) {
+            $table->text('description')->nullable()->change();
+        }
+        );
+
+        Schema::table(
+            'rules', function (Blueprint $table) {
+            $table->text('description')->nullable()->change();
+        }
+        );
     }
 }
