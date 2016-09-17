e4fd97ae779fbc34d2316371b00fde3ebe4e75c0
James Cole <thegrumpydictator@gmail.com>
2016-09-17 09:52:28 +0200

    Some code fixes. [skip ci]

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -77,7 +77,5 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
     }
 }
