394ef23eda36760122dd698753c1fd7ef15ea098
James Cole <thegrumpydictator@gmail.com>
2016-10-10 08:27:08 +0200

    New local names [skip ci]

--- config/firefly.php
+++ config/firefly.php
@@ -110,8 +110,8 @@ return [
         'hr_HR' => ['name_locale' => 'Croatian', 'name_english' => 'Croatian', 'complete' => false],
         'nl_NL' => ['name_locale' => 'Nederlands', 'name_english' => 'Dutch', 'complete' => true],
         'pt_BR' => ['name_locale' => 'Português do Brasil', 'name_english' => 'Portuguese (Brazil)', 'complete' => true],
-        'zh-HK' => ['name_locale' => 'Chinese Traditional, Hong Kong', 'name_english' => 'Chinese Traditional, Hong Kong', 'complete' => false],
-        'zh-TW' => ['name_locale' => 'Chinese Traditional', 'name_english' => 'Chinese Traditional', 'complete' => false],
+        'zh-HK' => ['name_locale' => '繁體中文（香港）', 'name_english' => 'Chinese Traditional, Hong Kong', 'complete' => false],
+        'zh-TW' => ['name_locale' => '正體中文', 'name_english' => 'Chinese Traditional', 'complete' => false],
     ],
     'transactionTypesByWhat'   => [
         'expenses'   => ['Withdrawal'],
