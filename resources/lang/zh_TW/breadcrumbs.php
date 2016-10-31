2cbe4a013e4a938fe5d57d632bf76964cf74541c
James Cole <JC5@users.noreply.github.com>
2016-10-31 18:41:04 +0100

    Translated

---
+++ resources/lang/zh_TW/breadcrumbs.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * breadcrumbs.php
+ * Copyright (C) 2016 thegrumpydictator@gmail.com
+ *
+ * This software may be modified and distributed under the terms of the
+ * Creative Commons Attribution-ShareAlike 4.0 International License.
+ *
+ * See the LICENSE file for details.
+ */
+
+return [
+    'home'              => '首頁',
+    'edit_currency'     => '編輯貨幣 ":name"',
+    'delete_currency'   => '刪除貨幣 ":name"',
+    'newPiggyBank'      => '創建一個新的存錢筒',
+    'edit_piggyBank'    => '編輯存錢筒 ":name"',
+    'preferences'       => '設定',
+    'profile'           => '個人設定',
+    'changePassword'    => '更改密碼',
+    'bills'             => '賬單',
+    'newBill'           => '新增賬單',
+    'edit_bill'         => '編輯賬單 ":name"',
+    'delete_bill'       => '刪除賬單 ":name"',
+    'reports'           => '報表',
+    'searchResult'      => '搜尋 ":query"',
+    'withdrawal_list'   => '支出',
+    'deposit_list'      => '收入、薪金與存款',
+    'transfer_list'     => '轉帳',
+    'transfers_list'    => '轉帳',
+    'create_withdrawal' => '新增提款',
+    'create_deposit'    => '新增存款',
+    'create_transfer'   => '新增轉帳',
+    'edit_journal'      => '編緝交易 ":description"',
+    'delete_journal'    => '刪除交易 ":description"',
+    'tags'              => '標籤',
+    'createTag'         => '建立新標籤',
+    'edit_tag'          => '編輯標籤 ":tag"',
+    'delete_tag'        => '刪除標籤 ":tag"',
+];
