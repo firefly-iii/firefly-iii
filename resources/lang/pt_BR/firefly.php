c3584ad20cada972c885da699c97e03f5bee951b
James Cole <JC5@users.noreply.github.com>
2016-11-25 17:01:25 +0100

    New translations

--- resources/lang/pt_BR/firefly.php
+++ resources/lang/pt_BR/firefly.php
@@ -521,6 +521,9 @@ return [
     'stored_new_account'                         => 'Nova conta ":name" armazenado!',
     'updated_account'                            => 'Conta ":name" atualizada',
     'credit_card_options'                        => 'Opções de cartão de crédito',
+    'no_transactions_account'                    => 'There are no transactions (in this period) for asset account ":name".',
+    'no_data_for_chart'                          => 'There is not enough information (yet) to generate this chart.',
+    'select_more_than_one_account'               => 'Please select more than one account',
 
     // categories:
     'new_category'                               => 'Nova categoria',
