d4d812c1955c712ec80132580f8b9e34017d0cbb
James Cole <JC5@users.noreply.github.com>
2016-11-28 19:02:23 +0100

    New translations

--- resources/lang/zh_TW/validation.php
+++ resources/lang/zh_TW/validation.php
@@ -21,6 +21,7 @@ return [
     'file_too_large'                 => '檔案 ":name" 過大。',
     'belongs_to_user'                => ':attribute 的值是未知的。',
     'accepted'                       => ':attribute 必須被接受。',
+    'bic'                            => 'This is not a valid BIC.',
     'active_url'                     => ':attribute 不是有效的URL。',
     'after'                          => ':attribute 必須是一個在 :date 之後的日期。',
     'alpha'                          => ':attribute 只允許包含字母。',
