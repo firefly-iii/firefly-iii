cd80d82ad495c5ee9c4628e52389b1c8b9a2edac
James Cole <JC5@users.noreply.github.com>
2016-11-28 19:02:37 +0100

    New translations

--- resources/lang/fr_FR/validation.php
+++ resources/lang/fr_FR/validation.php
@@ -21,6 +21,7 @@ return [
     'file_too_large'                 => 'Le fichier ":name" est trop grand.',
     'belongs_to_user'                => 'La valeur de :attribute est inconnue',
     'accepted'                       => 'Le champ :attribute doit être accepté.',
+    'bic'                            => 'This is not a valid BIC.',
     'active_url'                     => 'Le champ :attribute n\'est pas une URL valide.',
     'after'                          => 'Le champ :attribute doit être une date postérieure à :date.',
     'alpha'                          => 'Le champ :attribute doit seulement contenir des lettres.',
