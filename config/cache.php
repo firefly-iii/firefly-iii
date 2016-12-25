8f6a271cc0d443d51ed6a15a435ebdd1cffa9c82
James Cole <thegrumpydictator@gmail.com>
2016-12-25 13:38:30 +0100

    Add the ability to prefix cache differently.

--- config/cache.php
+++ config/cache.php
@@ -48,36 +48,36 @@ return [
         ],
 
         'database' => [
-            'driver' => 'database',
-            'table' => 'cache',
+            'driver'     => 'database',
+            'table'      => 'cache',
             'connection' => null,
         ],
 
         'file' => [
             'driver' => 'file',
-            'path' => storage_path('framework/cache'),
+            'path'   => storage_path('framework/cache'),
         ],
 
         'memcached' => [
-            'driver' => 'memcached',
+            'driver'        => 'memcached',
             'persistent_id' => env('MEMCACHED_PERSISTENT_ID'),
-            'sasl'       => [
+            'sasl'          => [
                 env('MEMCACHED_USERNAME'),
                 env('MEMCACHED_PASSWORD'),
             ],
-            'options'    => [
+            'options'       => [
             ],
-            'servers' => [
+            'servers'       => [
                 [
-                    'host' => env('MEMCACHED_HOST', '127.0.0.1'),
-                    'port' => env('MEMCACHED_PORT', 11211),
+                    'host'   => env('MEMCACHED_HOST', '127.0.0.1'),
+                    'port'   => env('MEMCACHED_PORT', 11211),
                     'weight' => 100,
                 ],
             ],
         ],
 
         'redis' => [
-            'driver' => 'redis',
+            'driver'     => 'redis',
             'connection' => 'default',
         ],
 
@@ -94,6 +94,6 @@ return [
     |
     */
 
-    'prefix' => 'firefly',
+    'prefix' => env('CACHE_PREFIX', 'firefly'),
 
 ];
