a6ccbcb79558030eeccacbdf9071ca220e8ea848
James Cole <JC5@users.noreply.github.com>
2016-12-26 10:31:47 +0100

    New translations

--- resources/lang/es_ES/demo.php
+++ resources/lang/es_ES/demo.php
@@ -8,7 +8,9 @@
  */
 
 return [
-    'no_demo_text'  => 'Sorry, there is no extra demo-explanation text for <abbr title="page: {{ Route.getCurrentRoute.getName }}">this page</abbr>.',
+    'no_demo_text'  => 'Sorry, there is no extra demo-explanation text for <abbr title=":route">this page</abbr>.',
     'see_help_icon' => 'However, the <i class="fa fa-question-circle"></i>-icon in the top right corner may tell you more.',
+    'index'         => 'Welcome to <strong>Firefly III</strong>! On this page you get a quick overview of your finances. For more information, check out Accounts &rarr; <a href=":asset">Asset Accounts</a> and of course the <a href=":budgets">Budgets</a> and <a href=":reports">Reports</a> pages. Or just take a look around and see where you end up.',
+
 
 ];
