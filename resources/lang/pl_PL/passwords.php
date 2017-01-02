cf6ea64aba8f8e2e3895e312d684b97a3c313380
James Cole <JC5@users.noreply.github.com>
2017-01-02 14:22:06 +0100

    New translations

--- resources/lang/pl_PL/passwords.php
+++ resources/lang/pl_PL/passwords.php
@@ -10,10 +10,10 @@
  */
 
 return [
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'user'     => 'We can\'t find a user with that e-mail address.',
-    'token'    => 'This password reset token is invalid.',
-    'sent'     => 'We have e-mailed your password reset link!',
-    'reset'    => 'Your password has been reset!',
+    'password' => 'Hasło musi zawierać przynajmniej 6 znaków i musi się zgadzać z potwierdzeniem.',
+    'user'     => 'Nie możemy znaleźć użytkownika o podanym adresie email.',
+    'token'    => 'Ten token do resetowania hasła jest nieprawidłowy.',
+    'sent'     => 'Wysłaliśmy link do resetowania hasła na twój adres email!',
+    'reset'    => 'Twoje hasło zostało zresetowane!',
     'blocked'  => 'Nice try though.',
 ];
