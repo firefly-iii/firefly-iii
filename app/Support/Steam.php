4f64f1d754f262eccb60c92f5261df175671e430
James Cole <thegrumpydictator@gmail.com>
2017-01-13 21:14:46 +0100

    Force order by. [skip ci]

--- app/Support/Steam.php
+++ app/Support/Steam.php
@@ -124,7 +124,7 @@ class Steam
                                   ->where('transaction_journals.date', '>=', $start->format('Y-m-d'))
                                   ->where('transaction_journals.date', '<=', $end->format('Y-m-d'))
                                   ->groupBy('transaction_journals.date')
-                                  ->orderBy('transaction_journals.date')
+                                  ->orderBy('transaction_journals.date','ASC')
                                   ->whereNull('transaction_journals.deleted_at')
                                   ->get(['transaction_journals.date', DB::raw('SUM(transactions.amount) AS modified')]);
         $currentBalance = $startBalance;
