de3e8edd6df5119820e7f4beb926d07faf318ef3
James Cole <JC5@users.noreply.github.com>
2017-01-24 15:41:44 +0100

    New translations

--- resources/lang/pt_BR/firefly.php
+++ resources/lang/pt_BR/firefly.php
@@ -262,6 +262,7 @@ return [
     'rule_trigger_description_is_choice'         => 'Descrição é..',
     'rule_trigger_category_is_choice'            => 'Category is..',
     'rule_trigger_budget_is_choice'              => 'Budget is..',
+    'rule_trigger_tag_is_choice'                 => '(A) tag is..',
     'rule_trigger_store_journal'                 => 'When a transaction is created',
     'rule_trigger_update_journal'                => 'When a transaction is updated',
     'rule_action_set_category'                   => 'Definir categoria para ":action_value"',
