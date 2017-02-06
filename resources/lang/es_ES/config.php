9bb8a9ccd1e43c3cb9599a6c9889ba1675fef50b
James Cole <JC5@users.noreply.github.com>
2017-02-06 13:20:54 +0100

    New translations

--- resources/lang/es_ES/config.php
+++ resources/lang/es_ES/config.php
@@ -10,7 +10,7 @@
  */
 
 return [
-    'locale'          => 'en, English, en_US, en_US.utf8, en_US.UTF-8',
+    'locale'          => 'es, Spanish, es_ES, es_ES.utf8, es_ES.UTF-8',
     'month'           => '%B %Y',
     'month_and_day'   => '%B %e, %Y',
     'date_time'       => '%B %e, %Y, @ %T',
