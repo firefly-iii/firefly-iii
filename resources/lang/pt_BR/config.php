915afa4534612cca9c33368b5691020ed38c59bd
James Cole <JC5@users.noreply.github.com>
2017-02-06 13:20:47 +0100

    Translated

--- resources/lang/pt_BR/config.php
+++ resources/lang/pt_BR/config.php
@@ -10,7 +10,7 @@
  */
 
 return [
-    'locale'          => 'en, English, en_US, en_US.utf8, en_US.UTF-8',
+    'locale'          => 'pt-br, pt_BR, pt_BR.utf8, pt_BR.UTF-8',
     'month'           => '%B %Y',
     'month_and_day'   => '%e de %B de %Y',
     'date_time'       => '%B %e, %Y, @ %T',
