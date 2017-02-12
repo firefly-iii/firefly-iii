3399b133ae07931c38f51dc5707994090bfcc5a9
James Cole <thegrumpydictator@gmail.com>
2017-02-12 13:23:08 +0100

    New user controller tests.

---
+++ tests/Feature/Controllers/Admin/ConfigurationControllerTest.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * ConfigurationControllerTest.php
+ * Copyright (c) 2017 thegrumpydictator@gmail.com
+ * This software may be modified and distributed under the terms of the Creative Commons Attribution-ShareAlike 4.0 International License.
+ *
+ * See the LICENSE file for details.
+ */
+
+declare(strict_types = 1);
+
+namespace Tests\Feature\Controllers\Admin;
+
+
+use FireflyConfig;
+use FireflyIII\Models\Configuration;
+use Tests\TestCase;
+
+class ConfigurationControllerTest extends TestCase
+{
+
+    /**
+     * @covers \FireflyIII\Http\Controllers\Admin\ConfigurationController::index
+     */
+    public function testIndex()
+    {
+        $this->be($this->user());
+
+        $falseConfig       = new Configuration;
+        $falseConfig->data = false;
+
+        $trueConfig       = new Configuration;
+        $trueConfig->data = true;
+
+        FireflyConfig::shouldReceive('get')->withArgs(['single_user_mode', true])->once()->andReturn($trueConfig);
+        FireflyConfig::shouldReceive('get')->withArgs(['is_demo_site', false])->times(2)->andReturn($falseConfig);
+
+        $response = $this->get(route('admin.configuration.index'));
+        $response->assertStatus(200);
+
+        // has bread crumb
+        $response->assertSee('<ol class="breadcrumb">');
+    }
+
+    /**
+     * @covers \FireflyIII\Http\Controllers\Admin\ConfigurationController::postIndex
+     */
+    public function testPostIndex()
+    {
+        $falseConfig       = new Configuration;
+        $falseConfig->data = false;
+
+        FireflyConfig::shouldReceive('get')->withArgs(['is_demo_site', false])->once()->andReturn($falseConfig);
+        FireflyConfig::shouldReceive('set')->withArgs(['single_user_mode', false])->once();
+        FireflyConfig::shouldReceive('set')->withArgs(['is_demo_site', false])->once();
+
+        $this->be($this->user());
+        $response = $this->post(route('admin.configuration.index.post'));
+        $response->assertSessionHas('success');
+        $response->assertStatus(302);
+    }
+
+}
