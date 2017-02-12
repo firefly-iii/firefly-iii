5528663727d80cd6f47e9e270ffeadf02c00460c
James Cole <thegrumpydictator@gmail.com>
2017-02-12 11:25:17 +0100

    Re-implemented basic account controller tests.

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * ExampleTest.php
+ * Copyright (c) 2017 thegrumpydictator@gmail.com
+ * This software may be modified and distributed under the terms of the Creative Commons Attribution-ShareAlike 4.0 International License.
+ *
+ * See the LICENSE file for details.
+ */
 
 namespace Tests\Feature;
 
@@ -16,8 +23,20 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $response = $this->get('/');
+        $response = $this->get('/login');
 
         $response->assertStatus(200);
+    }
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testAnotherBasicTest()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(302);
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,8 +2,12 @@
 
 namespace Tests;
 
+use FireflyIII\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-
+use Log;
+use Mockery;
+use FireflyIII\Models\Preference;
+use Carbon\Carbon;
 /**
  * Class TestCase
  *
@@ -12,4 +16,75 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * @return User
+     */
+    public function user()
+    {
+        $user = User::find(1);
+
+        return $user;
+    }
+
+    /**
+     * @param User   $user
+     * @param string $range
+     */
+    public function changeDateRange(User $user, $range)
+    {
+        $valid = ['1D', '1W', '1M', '3M', '6M', '1Y', 'custom'];
+        if (in_array($range, $valid)) {
+            Preference::where('user_id', $user->id)->where('name', 'viewRange')->delete();
+            Preference::create(
+                [
+                    'user_id' => $user->id,
+                    'name'    => 'viewRange',
+                    'data'    => $range,
+                ]
+            );
+            // set period to match?
+
+        }
+        if ($range === 'custom') {
+            $this->session(
+                [
+                    'start' => Carbon::now()->subDays(20),
+                    'end'   => Carbon::now(),
+                ]
+            );
+        }
+    }
+
+
+    /**
+     * @param string $class
+     *
+     * @return \Mockery\MockInterface
+     */
+    protected function mock($class)
+    {
+        Log::debug(sprintf('Will now mock %s', $class));
+        $object = Mockery::mock($class);
+        $this->app->instance($class, $object);
+
+        return $object;
+    }
+
+    /**
+     * @return array
+     */
+    public function dateRangeProvider()
+    {
+        return [
+            'one day'      => ['1D'],
+            'one week'     => ['1W'],
+            'one month'    => ['1M'],
+            'three months' => ['3M'],
+            'six months'   => ['6M'],
+            'one year'     => ['1Y'],
+            'custom range' => ['custom'],
+        ];
+    }
+
 }
