33b099456f0845577a7438ad9e24b2fc28cb67fe
James Cole <JC5@users.noreply.github.com>
2017-03-05 13:30:18 +0100

    New translations

--- resources/lang/pt_BR/firefly.php
+++ resources/lang/pt_BR/firefly.php
@@ -239,6 +239,7 @@ return [
     'rule_trigger_to_account_is'                 => 'Conta de destino é ":trigger_value"',
     'rule_trigger_to_account_contains'           => 'Conta de destino contém ":trigger_value"',
     'rule_trigger_transaction_type'              => 'Transação é do tipo ":trigger_value"',
+    'rule_trigger_category_is'                   => 'Category is ":trigger_value"',
     'rule_trigger_amount_less'                   => 'Quantia é inferior :trigger_value',
     'rule_trigger_amount_exactly'                => 'Quantia é: trigger_value',
     'rule_trigger_amount_more'                   => 'Quantia é mais de :trigger_value',
