8dc2faa5e5ad2ee8e98c0b56e0b49817c03ad050
James Cole <JC5@users.noreply.github.com>
2017-03-09 21:10:34 +0100

    New translations firefly.php (German)

--- resources/lang/de_DE/firefly.php
+++ resources/lang/de_DE/firefly.php
@@ -115,6 +115,7 @@ return [
     'multi_select_no_selection'                  => 'Nichts ausgewählt',
     'multi_select_all_selected'                  => 'Alle ausgewählt',
     'multi_select_filter_placeholder'            => 'Suche..',
+    'all_journals_without_budget'                => 'All transactions without a budget',
 
 
     // repeat frequencies:
