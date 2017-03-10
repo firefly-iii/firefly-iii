84a3e68eb5e1a5ce02e982382aa857b2a5438316
James Cole <JC5@users.noreply.github.com>
2017-03-10 16:11:18 +0100

    New translations firefly.php (Dutch)

--- resources/lang/nl_NL/firefly.php
+++ resources/lang/nl_NL/firefly.php
@@ -116,6 +116,9 @@ return [
     'multi_select_all_selected'                  => 'Alles geselecteerd',
     'multi_select_filter_placeholder'            => 'Vinden..',
     'all_journals_without_budget'                => 'All transactions without a budget',
+    'journals_without_budget'                    => 'Transactions without a budget',
+    'all_journals_for_account' => 'All transactions for account :name',
+    'journals_in_period_for_account' => 'All transactions for account :name between :start and :end',
 
 
     // repeat frequencies:
