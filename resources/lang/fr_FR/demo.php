b70c5ae41ee192b62dc3374ff4a90c1d30249b69
James Cole <JC5@users.noreply.github.com>
2017-03-21 10:11:17 +0100

    New translations demo.php (French)

--- resources/lang/fr_FR/demo.php
+++ resources/lang/fr_FR/demo.php
@@ -9,14 +9,14 @@
 
 return [
     'no_demo_text'                   => 'Désolé, il n’y a aucun texte supplémentaire de démonstration ou d\'explication pour <abbr title=":route"> cette page</abbr>.',
-    'see_help_icon'                  => 'Cependant, l\'icône <i class="fa fa-question-circle"></i>- dans le coin supérieur droit peut vous en dire plus.',
+    'see_help_icon'                  => 'Cependant, l\'icône <i class="fa fa-question-circle"></i> située dans le coin supérieur droit peut vous en dire plus.',
     'index'                          => 'Bienvenue chez <strong>Firefly III</strong> ! Sur cette page, vous obtenez un aperçu rapide de vos finances. Pour plus d’informations, consultez comptes &rarr; <a href=":asset"> Comptes d’actif</a> et, bien sûr, les pages des <a href=":budgets"> Budgets</a> et des <a href=":reports"> rapports</a>. Ou juste jetez un coup d’œil et voyez où vous vous retrouvez.',
     'accounts-index'                 => 'Asset accounts are your personal bank accounts. Expense accounts are the accounts you spend money at, such as stores and friends. Revenue accounts are accounts you receive money from, such as your job, the government or other sources of income. On this page you can edit or remove them.',
     'budgets-index'                  => 'This page shows you an overview of your budgets. The top bar shows the amount that is available to be budgeted. This can be customized for any period by clicking the amount on the right. The amount you\'ve actually spent is shown in the bar below. Below that are the expenses per budget and what you\'ve budgeted for them.',
     'reports-index-start'            => 'Firefly III supports four types of reports. Read about them by clicking on the <i class="fa fa-question-circle"></i>-icon in the top right corner.',
     'reports-index-examples'         => 'Be sure to check out these examples: <a href=":one">a monthly financial overview</a>, <a href=":two">a yearly financial overview</a> and <a href=":three">a budget overview</a>.',
     'currencies-index'               => 'Firefly III supports multiple currencies. Although it defaults to the Euro it can be set to the US Dollar and many other currencies. As you can see a small selection of currencies has been included but you can add your own if you wish to. Changing the default currency will not change the currency of existing transactions however: Firefly III supports the use of multiple currencies at the same time.',
-    'transactions-index'             => 'These expenses, deposits and transfers are not particularly imaginative. They have been generated automatically.',
+    'transactions-index'             => 'Ces dépenses, dépôts et transferts ne sont pas particulièrement imaginatifs. Ils ont été générés automatiquement.',
     'piggy-banks-index'              => 'Comme vous pouvez le voir, il y a trois tirelires. Utilisez les boutons plus et moins pour influer sur le montant d’argent dans chaque tirelire. Cliquez sur le nom de la tirelire pour voir l’administration pour chaque tirelire.',
     'import-index'                   => 'Bien sûr, n’importe quel fichier CSV peut être importé dans Firefly III ',
     'import-configure-security'      => 'Pour des raisons de sécurité, votre téléchargement a été remplacé par un fichier local.',
