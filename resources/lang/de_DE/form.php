10481895e6c15002d6bc6b0a8ad71c976a517075
James Cole <JC5@users.noreply.github.com>
2017-04-14 23:00:29 +0200

    New translations form.php (German)

--- resources/lang/de_DE/form.php
+++ resources/lang/de_DE/form.php
@@ -66,6 +66,8 @@ return [
     'decimal_places'                 => 'Decimal places',
     'exchange_rate_instruction'      => 'Foreign currencies',
     'exchanged_amount'               => 'Exchanged amount',
+    'source_amount'                  => 'Amount (source)',
+    'destination_amount'             => 'Amount (destination)',
 
     'revenue_account_source'      => 'Ertragskonto (Quelle)',
     'source_account_asset'        => 'Quellkonto (Bestandskonto)',
