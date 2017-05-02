df5bb14758add6a05f00ca42601cd5a9f10dd36d
James Cole <JC5@users.noreply.github.com>
2017-05-03 01:00:08 +0200

    New translations config.php (Slovenian)

--- resources/lang/sl_SI/config.php
+++ resources/lang/sl_SI/config.php
@@ -10,12 +10,12 @@
  */
 
 return [
-    'locale'          => 'en, English, en_US, en_US.utf8, en_US.UTF-8',
+    'locale'          => 'sl, Slovenian, sl_SI, sl_SI.utf8, sl_SI.UTF-8',
     'month'           => '%B %Y',
     'month_and_day'   => '%B %e, %Y',
     'date_time'       => '%B %e, %Y, @ %T',
     'specific_day'    => '%e %B %Y',
-    'week_in_year'    => 'Week %W, %Y',
+    'week_in_year'    => 'teden %W, %Y',
     'quarter_of_year' => '%B %Y',
     'year'            => '%Y',
     'half_year'       => '%B %Y',
