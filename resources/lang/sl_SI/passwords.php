2ed0ea02435b2eb7ab76b4b1af2b1654e6d9ab40
James Cole <JC5@users.noreply.github.com>
2017-05-03 09:00:11 +0200

    New translations passwords.php (Slovenian)

--- resources/lang/sl_SI/passwords.php
+++ resources/lang/sl_SI/passwords.php
@@ -10,10 +10,10 @@
  */
 
 return [
-    'password' => 'Passwords must be at least six characters and match the confirmation.',
-    'user'     => 'We can\'t find a user with that e-mail address.',
-    'token'    => 'This password reset token is invalid.',
-    'sent'     => 'We have e-mailed your password reset link!',
-    'reset'    => 'Your password has been reset!',
-    'blocked'  => 'Nice try though.',
+    'password' => 'Geslo mora biti dolgo vsaj šest znakov in mora biti enako v obeh poljih.',
+    'user'     => 'Uporabnika s takim e-poštnim naslovom v naši bazi nismo našli.',
+    'token'    => 'Žeton za ponastavitev gesla ni veljaven.',
+    'sent'     => 'Poslali smo vam povezavo za ponastavitev gesla!',
+    'reset'    => 'Vaše geslo je bilo ponastavljeno!',
+    'blocked'  => 'Dober poskus...',
 ];
