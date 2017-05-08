0c44fe6ce0d5c3f29e413b059a5a3df083416890
Enrico Lamperti <elamperti@users.noreply.github.com>
2017-05-07 21:24:07 -0300

    Add translations for Spanish language

---
+++ resources/lang/es_ES/breadcrumbs.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * breadcrumbs.php
+ * Copyright (C) 2016 thegrumpydictator@gmail.com
+ *
+ * This software may be modified and distributed under the terms of the
+ * Creative Commons Attribution-ShareAlike 4.0 International License.
+ *
+ * See the LICENSE file for details.
+ */
+
+return [
+    'home'              => 'Inicio',
+    'edit_currency'     => 'Editar moneda ":name"',
+    'delete_currency'   => 'Eliminar moneda ":name"',
+    'newPiggyBank'      => 'Crear nueva alcancía',
+    'edit_piggyBank'    => 'Editar alcancía ":name"',
+    'preferences'       => 'Preferencias',
+    'profile'           => 'Perfil',
+    'changePassword'    => 'Cambiar contraseña',
+    'bills'             => 'Facturas',
+    'newBill'           => 'Nueva factura',
+    'edit_bill'         => 'Editar factura ":name"',
+    'delete_bill'       => 'Eliminar factura ":name"',
+    'reports'           => 'Reportes',
+    'searchResult'      => 'Buscar ":query"',
+    'withdrawal_list'   => 'Expensas',
+    'deposit_list'      => 'Ganancia, ingresos y depósitos',
+    'transfer_list'     => 'Transferencias',
+    'transfers_list'    => 'Transferencias',
+    'create_withdrawal' => 'Crear nueva extracción',
+    'create_deposit'    => 'Crear nuevo depósito',
+    'create_transfer'   => 'Crear nueva transferencia',
+    'edit_journal'      => 'Editar transacción ":description"',
+    'delete_journal'    => 'Eliminar transacción ":description"',
+    'tags'              => 'Etiquetas',
+    'createTag'         => 'Crear nueva etiqueta',
+    'edit_tag'          => 'Editar etiqueta ":tag"',
+    'delete_tag'        => 'Eliminar etiqueta ":tag"',
+];
