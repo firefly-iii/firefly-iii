5e1bd8e1eb70e502148df39920c8ed180370254f
James Cole <JC5@users.noreply.github.com>
2017-06-10 15:20:48 +0200

    New translations csv.php (Dutch)

--- resources/lang/nl_NL/csv.php
+++ resources/lang/nl_NL/csv.php
@@ -13,28 +13,32 @@ declare(strict_types=1);
 
 return [
 
-    'import_configure_title' => 'Import configureren',
-    'import_configure_intro' => 'Hier zie je enkele opties voor jouw CSV bestand. Geef aan of je CSV bestand kolomtitels bevat, en hoe het datumveld is opgebouwd. Hier moet je wellicht wat experimenteren. Het scheidingsteken is meestal een ",", maar dat kan ook een ";" zijn. Controleer dit zorgvuldig.',
-    'import_configure_form'  => 'Standaard CSV import opties',
-    'header_help'            => 'Vink hier als de eerste rij kolomtitels bevat',
-    'date_help'              => 'Datum/tijd formaat in jouw CSV bestand. Volg het formaat zoals ze het <a href="https://secure.php.net/manual/en/datetime.createfromformat.php#refsect1-datetime.createfromformat-parameters">op deze pagina</a> uitleggen. Het standaardformaat ziet er zo uit: :dateExample.',
-    'delimiter_help'         => 'Kies het veldscheidingsteken dat in jouw bestand wordt gebruikt. Als je het niet zeker weet, is de komma de beste optie.',
-    'import_account_help'    => 'Als jouw CSV bestand geen referenties bevat naar jouw rekening(en), geef dan hier aan om welke rekening het gaat.',
-    'upload_not_writeable'   => 'Het grijze vlak bevat een bestandspad. Dit pad moet schrijfbaar zijn.',
+    // initial config
+    'initial_config_title'        => 'Import configuration (1/3)',
+    'initial_config_text'         => 'To be able to import your file correctly, please validate the options below.',
+    'initial_config_box'          => 'Basic CSV import configuration',
+    'initial_header_help'         => 'Check this box if the first row of your CSV file are the column titles.',
+    'initial_date_help'           => 'Date time format in your CSV. Follow the format like <a href="https://secure.php.net/manual/en/datetime.createfromformat.php#refsect1-datetime.createfromformat-parameters">this page</a> indicates. The default value will parse dates that look like this: :dateExample.',
+    'initial_delimiter_help'      => 'Choose the field delimiter that is used in your input file. If not sure, comma is the safest option.',
+    'initial_import_account_help' => 'If your CSV file does NOT contain information about your asset account(s), use this dropdown to select to which account the transactions in the CSV belong to.',
 
-    // roles
-    'column_roles_title'     => 'Bepaal de inhoud van elke kolom',
-    'column_roles_table'     => 'Tabel',
-    'column_name'            => 'Kolomnaam',
-    'column_example'         => 'Voorbeeldgegevens',
-    'column_role'            => 'Kolomrol',
-    'do_map_value'           => 'Maak een mapping',
-    'column'                 => 'Kolom',
-    'no_example_data'        => 'Geen voorbeeldgegevens',
-    'store_column_roles'     => 'Ga verder met import',
-    'do_not_map'             => '(niet mappen)',
-    'map_title'              => 'Verbind importdata met Firefly III data',
-    'map_text'               => 'In deze tabellen is de linkerwaarde een waarde uit je CSV bestand. Jij moet de link leggen, als mogelijk, met een waarde uit jouw database. Firefly houdt zich hier aan. Als er geen waarde is, selecteer dan ook niets.',
+    // roles config
+    'roles_title'                 => 'Define each column\'s role',
+    'roles_text'                  => 'Each column in your CSV file contains certain data. Please indicate what kind of data the importer should expect. The option to "map" data means that you will link each entry found in the column to a value in your database. An often mapped column is the column that contains the IBAN of the opposing account. That can be easily matched to IBAN\'s present in your database already.',
+    'roles_table'                 => 'Table',
+    'roles_column_name'           => 'Name of column',
+    'roles_column_example'        => 'Column example data',
+    'roles_column_role'           => 'Column data meaning',
+    'roles_do_map_value'          => 'Map these values',
+    'roles_column'                => 'Column',
+    'roles_no_example_data'       => 'No example data available',
+
+    'roles_store' => 'Continue import',
+    'roles_do_not_map'         => '(do not map)',
+
+    // map data
+    'map_title'                => 'Verbind importdata met Firefly III data',
+    'map_text'                 => 'In deze tabellen is de linkerwaarde een waarde uit je CSV bestand. Jij moet de link leggen, als mogelijk, met een waarde uit jouw database. Firefly houdt zich hier aan. Als er geen waarde is, selecteer dan ook niets.',
 
     'field_value'          => 'Veldwaarde',
     'field_mapped_to'      => 'Gelinkt aan',
