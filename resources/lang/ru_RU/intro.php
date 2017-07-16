eb15ca8ebd64f44b4bac76c661c9165a69fc31db
James Cole <JC5@users.noreply.github.com>
2017-07-16 07:50:20 +0200

    New translations intro.php (Russian)

---
+++ resources/lang/ru_RU/intro.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * intro.php
+ * Copyright (c) 2017 thegrumpydictator@gmail.com
+ * This software may be modified and distributed under the terms of the Creative Commons Attribution-ShareAlike 4.0 International License.
+ *
+ * See the LICENSE file for details.
+ */
+
+return [
+    'index_intro'            => 'Welcome to the index page of Firefly III. Please take the time to walk through this intro to get a feeling of how Firefly III works.',
+    'index_accounts-chart'   => 'This chart shows the current balance of your asset accounts. You can select the accounts visible here in your preferences.',
+    'index_box_out_holder'   => 'This little box and the boxes next to this one will give you a quick overview of your financial situation',
+    'index_all_transactions' => 'These boxes will hold your most recent transactions.',
+    'index_help'             => 'If you ever need help with a page or a form, press this button.',
+    'index_outro'            => 'Most pages of Firefly III will start with a little tour like this one. Please contact me when you have questions or comments. Enjoy!',
+
+];
