bc83bd6ec8b684513caaf3324e6f71f92c728e74
James Cole <JC5@users.noreply.github.com>
2017-07-16 18:40:15 +0200

    New translations intro.php (Dutch)

--- resources/lang/nl_NL/intro.php
+++ resources/lang/nl_NL/intro.php
@@ -8,11 +8,18 @@
  */
 
 return [
-    'index_intro'            => 'Welcome to the index page of Firefly III. Please take the time to walk through this intro to get a feeling of how Firefly III works.',
-    'index_accounts-chart'   => 'This chart shows the current balance of your asset accounts. You can select the accounts visible here in your preferences.',
-    'index_box_out_holder'   => 'This little box and the boxes next to this one will give you a quick overview of your financial situation',
-    'index_all_transactions' => 'These boxes will hold your most recent transactions.',
-    'index_help'             => 'If you ever need help with a page or a form, press this button.',
-    'index_outro'            => 'Most pages of Firefly III will start with a little tour like this one. Please contact me when you have questions or comments. Enjoy!',
+    'index_intro'                => 'Welcome to the index page of Firefly III. Please take the time to walk through this intro to get a feeling of how Firefly III works.',
+    'index_accounts-chart'       => 'This chart shows the current balance of your asset accounts. You can select the accounts visible here in your preferences.',
+    'index_box_out_holder'       => 'This little box and the boxes next to this one will give you a quick overview of your financial situation',
+    'index_help'                 => 'If you ever need help with a page or a form, press this button.',
+    'index_outro'                => 'Most pages of Firefly III will start with a little tour like this one. Please contact me when you have questions or comments. Enjoy!',
+    'index_sidebar-toggle'       => 'To create new transactions, accounts or other things, use the menu under this icon.',
+    'rules_index_intro'          => 'Firefly III allows you to manage rules, that will automagically be applied to any transaction you create or edit.',
+    'rules_index_new_rule_group' => 'You can combine rules in groups for easier management.',
+    'rules_index_new_rule'       => 'Create as many rules as you like.',
+    'rules_index_prio_buttons'   => 'Order them any way you see fit.',
+    'rules_index_test_buttons'   => 'You can test your rules or apply them to existing transactions.',
+    'rules_index_rule-triggers'  => 'Rules have "triggers" and "actions" that you can order by drag-and-drop.',
+    'rules_index_outro'          => 'Be sure to check out the help pages using the (?) icon in the top right!',
 
 ];
