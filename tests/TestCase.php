bc0ef11a8cacdfe5a07508cfd6dd8756b3fa33e3
James Cole <thegrumpydictator@gmail.com>
2017-08-11 05:36:05 +0200

    Add copyright markers.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,5 +1,15 @@
 <?php
 
+
+/**
+ * TestCase.php
+ * Copyright (c) 2017 thegrumpydictator@gmail.com
+ * This software may be modified and distributed under the terms of the
+ * Creative Commons Attribution-ShareAlike 4.0 International License.
+ *
+ * See the LICENSE file for details.
+ */
+
 namespace Tests;
 
 use Carbon\Carbon;
