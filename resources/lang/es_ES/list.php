c0673b10b5b337f8aa8e6a29ba1d6ea79825f40a
James Cole <JC5@users.noreply.github.com>
2017-08-25 07:06:56 +0200

    New translations list.php (Spanish)

--- resources/lang/es_ES/list.php
+++ resources/lang/es_ES/list.php
@@ -88,7 +88,7 @@ return [
     'budget_count'           => 'Número de presupuestos',
     'rule_and_groups_count'  => 'Número de reglas y grupos de reglas',
     'tags_count'             => 'Número de etiquetas',
-    'inward'                 => 'Inward description',
-    'outward'                => 'Outward description',
-    'number_of_transactions' => 'Number of transactions',
+    'inward'                 => '',
+    'outward'                => '',
+    'number_of_transactions' => '',
 ];
