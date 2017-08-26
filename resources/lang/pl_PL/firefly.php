ef4bc0ca0cb02fb809a48ba092becd1d8757d0ad
James Cole <JC5@users.noreply.github.com>
2017-08-26 15:02:44 +0200

    New translations firefly.php (Polish)

--- resources/lang/pl_PL/firefly.php
+++ resources/lang/pl_PL/firefly.php
@@ -311,6 +311,7 @@ return [
     'meta_data'                                  => 'Metadane',
     'location'                                   => 'Lokalizacja',
     'without_date'                               => 'Without date',
+    'result'                                     => 'Result',
 
     // preferences
     'pref_home_screen_accounts'                  => 'Konta na stronie domowej',
