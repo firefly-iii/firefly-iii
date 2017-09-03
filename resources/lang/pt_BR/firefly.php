f031b6fa3f4d1bd2c64b867ed90b40540c5e88fa
James Cole <JC5@users.noreply.github.com>
2017-09-03 11:10:47 +0200

    New translations firefly.php (Portuguese, Brazilian)

--- resources/lang/pt_BR/firefly.php
+++ resources/lang/pt_BR/firefly.php
@@ -782,6 +782,9 @@ return [
     'account_role_sharedAsset'                   => 'Shared asset account',
     'account_role_savingAsset'                   => 'Savings account',
     'account_role_ccAsset'                       => 'Credit card',
+    'budget_chart_click'                         => 'Please click on a budget name in the table above to see a chart.',
+    'category_chart_click'                       => 'Please click on a category name in the table above to see a chart.',
+
 
     // charts:
     'chart'                                      => 'Gráfico',
