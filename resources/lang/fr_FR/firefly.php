c143d54c97416e5245a427b8dad5a1d0d6bf7c4c
James Cole <JC5@users.noreply.github.com>
2017-09-03 11:00:41 +0200

    New translations firefly.php (French)

--- resources/lang/fr_FR/firefly.php
+++ resources/lang/fr_FR/firefly.php
@@ -624,6 +624,11 @@ return [
     'perm-delete-many'                           => 'Supprimer de nombreux éléments en une seule fois peut être très problématique. Soyez prudent.',
     'mass_deleted_transactions_success'          => 'Montant des opérations supprimées : :amount.',
     'mass_edited_transactions_success'           => 'Montant des opérations mises à jour : :amount',
+    'opt_group_no_account_type'                  => '(no account type)',
+    'opt_group_defaultAsset'                     => 'Default asset accounts',
+    'opt_group_savingAsset'                      => 'Savings accounts',
+    'opt_group_sharedAsset'                      => 'Shared asset accounts',
+    'opt_group_ccAsset'                          => 'Credit cards',
 
 
     // new user:
