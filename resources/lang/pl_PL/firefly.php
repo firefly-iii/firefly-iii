26f71400e9344fcf11d54510afbb21640df47060
James Cole <JC5@users.noreply.github.com>
2017-09-03 11:10:39 +0200

    New translations firefly.php (Polish)

--- resources/lang/pl_PL/firefly.php
+++ resources/lang/pl_PL/firefly.php
@@ -782,6 +782,9 @@ return [
     'account_role_sharedAsset'                   => 'Współdzielone konto aktywów',
     'account_role_savingAsset'                   => 'Konto oszczędnościowe',
     'account_role_ccAsset'                       => 'Karta kredytowa',
+    'budget_chart_click'                         => 'Please click on a budget name in the table above to see a chart.',
+    'category_chart_click'                       => 'Please click on a category name in the table above to see a chart.',
+
 
     // charts:
     'chart'                                      => 'Wykres',
