5ee071fdc73468ea8445665bea6d7a1e6dcab50b
James Cole <JC5@users.noreply.github.com>
2017-09-03 18:30:11 +0200

    New translations validation.php (French)

--- resources/lang/fr_FR/validation.php
+++ resources/lang/fr_FR/validation.php
@@ -17,7 +17,6 @@ return [
     'deleted_user'                   => 'Compte tenu des contraintes de sécurité, vous ne pouvez pas vous inscrire en utilisant cette adresse e-mail.',
     'rule_trigger_value'             => 'Cette valeur n’est pas valide pour le déclencheur sélectionné.',
     'rule_action_value'              => 'Cette valeur n’est pas valide pour l’action sélectionnée.',
-    'invalid_domain'                 => 'Compte tenu des contraintes de sécurité, vous ne pouvez pas vous enregistrer depuis ce domaine.',
     'file_already_attached'          => 'Le fichier téléchargé ":name" est déjà attaché à cet objet.',
     'file_attached'                  => 'Envoi du fichier ":name" avec succès.',
     'file_invalid_mime'              => 'Le fichier ":name" est du type ":mime" ce qui n\'est pas accepté pour un nouvel envoi.',
