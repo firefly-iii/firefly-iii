3b9b74b5ea33707ad4c06839233d8145e40c87f0
James Cole <thegrumpydictator@gmail.com>
2017-09-09 22:11:33 +0200

    Example test should point to /login

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,25 +1,10 @@
 <?php
 
-/**
- * ExampleTest.php
- * Copyright (c) 2017 thegrumpydictator@gmail.com
- * This software may be modified and distributed under the terms of the
- * Creative Commons Attribution-ShareAlike 4.0 International License.
- *
- * See the LICENSE file for details.
- */
-
-declare(strict_types=1);
-
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
-/**
- * Class ExampleTest
- *
- * @package Tests\Feature
- */
 class ExampleTest extends TestCase
 {
     /**
@@ -27,21 +12,9 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function testAnotherBasicTest()
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(302);
-    }
-
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
     public function testBasicTest()
     {
-        $response = $this->get('/login');
+        $response = $this->get('/');
 
         $response->assertStatus(200);
     }
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,38 +1,10 @@
 <?php
 
-
-/**
- * ExampleTest.php
- * Copyright (c) 2017 thegrumpydictator@gmail.com
- * This software may be modified and distributed under the terms of the
- * Creative Commons Attribution-ShareAlike 4.0 International License.
- *
- * See the LICENSE file for details.
- */
-
-declare(strict_types=1);
-
-/**
- * ExampleTest.php
- * Copyright (c) 2017 thegrumpydictator@gmail.com
- * This software may be modified and distributed under the terms of the
- * Creative Commons Attribution-ShareAlike 4.0 International License.
- *
- * See the LICENSE file for details.
- */
-
 namespace Tests\Unit;
 
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
-/**
- * Class ExampleTest
- *
- * @package Tests\Unit
- * @SuppressWarnings(PHPMD.TooManyPublicMethods)
- * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
- * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
- */
 class ExampleTest extends TestCase
 {
     /**
