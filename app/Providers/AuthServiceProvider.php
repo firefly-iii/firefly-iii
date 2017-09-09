f077adaefde8b7c9daff2eab04440c0b1ea39294
James Cole <thegrumpydictator@gmail.com>
2017-09-09 22:06:13 +0200

    Separated providers.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -1,25 +1,10 @@
 <?php
-/**
- * AuthServiceProvider.php
- * Copyright (C) 2016 thegrumpydictator@gmail.com
- *
- * This software may be modified and distributed under the terms of the
- * Creative Commons Attribution-ShareAlike 4.0 International License.
- *
- * See the LICENSE file for details.
- */
-
-declare(strict_types=1);
 
 namespace FireflyIII\Providers;
 
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
-/**
- * Class AuthServiceProvider
- *
- * @package FireflyIII\Providers
- */
 class AuthServiceProvider extends ServiceProvider
 {
     /**
@@ -27,10 +12,9 @@ class AuthServiceProvider extends ServiceProvider
      *
      * @var array
      */
-    protected $policies
-        = [
-            'FireflyIII\Model' => 'FireflyIII\Policies\ModelPolicy',
-        ];
+    protected $policies = [
+        'FireflyIII\Model' => 'FireflyIII\Policies\ModelPolicy',
+    ];
 
     /**
      * Register any authentication / authorization services.
