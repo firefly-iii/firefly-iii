ce205ab413911ecdc767cdbec4ab513c78fb2386
James Cole <thegrumpydictator@gmail.com>
2017-09-12 18:24:42 +0200

    Various small fixes.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -34,6 +34,7 @@ class LoginController extends Controller
      */
     public function __construct()
     {
+        parent::__construct();
         $this->middleware('guest')->except('logout');
     }
 }
