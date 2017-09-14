a20d9fa8f11d7147eaf60378290ba7c7c042b36f
James Cole <thegrumpydictator@gmail.com>
2017-09-14 16:11:55 +0200

    Add missing translations #843

--- resources/lang/en_US/list.php
+++ resources/lang/en_US/list.php
@@ -82,4 +82,6 @@ return [
     'inward'                 => 'Inward description',
     'outward'                => 'Outward description',
     'number_of_transactions' => 'Number of transactions',
+    'total_amount'           => 'Total amount',
+
 ];
