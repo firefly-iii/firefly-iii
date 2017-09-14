45a05111e6baa7b43349387c923328dda6a2993c
James Cole <JC5@users.noreply.github.com>
2017-09-14 16:40:12 +0200

    New translations firefly.php (Dutch)

--- resources/lang/nl_NL/firefly.php
+++ resources/lang/nl_NL/firefly.php
@@ -130,8 +130,8 @@ return [
     'exchange_rate_instructions'                 => 'Betaalrekening "@naam" accepteert alleen boekingen in @native_currency. Als je @foreign_currency wilt gebruiken moet je ook het bedrag in @native_currency opgeven:',
     'transfer_exchange_rate_instructions'        => 'Bronbetaalrekening "@source_name" accepteert alleen overschrijvingen in @source_currency. Doelbetaalrekening "@dest_name: accepteert alleen overschrijvingen in @dest_currency. Je moet het juiste bedrag in beide valuta opgeven.',
     'transaction_data'                           => 'Transactiegegevens',
-    'invalid_server_configuration'               => 'Invalid server configuration',
-    'invalid_locale_settings'                    => 'Firefly III is unable to format monetary amounts because your server is missing the required packages. There are <a href="https://github.com/firefly-iii/help/wiki/Missing-locale-packages">instructions how to do this</a>.',
+    'invalid_server_configuration'               => 'Ongeldige serverconfiguratie',
+    'invalid_locale_settings'                    => 'Firefly III kan geldbedragen niet goed weergeven omdat je server de vereiste software mist. Er zijn <a href="https://github.com/firefly-iii/help/wiki/Missing-locale-packages">instructies hoe dit te doen</a>.',
 
     // search
     'search'                                     => 'Zoeken',
@@ -912,7 +912,7 @@ return [
     'comments'                              => 'Opmerkingen',
     'to_link_not_found'                     => 'Als de transactie die je wilt koppelen niet gevonden wordt, voer dan het ID in.',
     'invalid_link_data'                     => 'Ongeldige informatie, de transactie kan niet gekoppeld worden.',
-    'invalid_link_selection'                => 'Cannot link these transactions',
+    'invalid_link_selection'                => 'Deze transacties kunnen niet worden gekoppeld',
     'journals_linked'                       => 'De transacties zijn gekoppeld.',
     'journals_error_linked'                 => 'Deze transacties zijn al gekoppeld.',
     'journal_links'                         => 'Transactiekoppelingen',
@@ -944,7 +944,7 @@ return [
     'cannot_edit_multiple_dest'             => 'Je kan transactie #:id met omschrijving ":description" niet wijzigen, want deze bevat meerdere doelrekeningen.',
     'cannot_edit_opening_balance'           => 'Je kan het startsaldo van een rekening niet wijzigen via dit scherm.',
     'no_edit_multiple_left'                 => 'Je hebt geen geldige transacties geselecteerd.',
-    'cannot_convert_split_journal'          => 'Cannot convert a split transaction',
+    'cannot_convert_split_journal'          => 'Kan geen gesplitste transactie omzetten',
 
     // import bread crumbs and titles:
     'import'                                => 'Import',
@@ -988,7 +988,7 @@ return [
     'import_status_finished_title'          => 'Importeren is klaar',
     'import_status_finished_text'           => 'Je gegevensbestand is geïmporteerd.',
     'import_status_finished_job'            => 'De geimporteerde transacties kan je vinden onder tag <a href=":link" class="label label-success" style="font-size:100%;font-weight:normal;">:tag</a>.',
-    'import_status_job_running'             => 'The import is running...',
+    'import_status_job_running'             => 'De import is bezig...',
     'import_with_key'                       => 'Import met code \':key\'',
 
     // sandstorm.io errors and messages:
