dd8142e26aea609396abec3b55d6d2b004757338
James Cole <JC5@users.noreply.github.com>
2017-09-14 21:21:08 +0200

    New translations firefly.php (French)

--- resources/lang/fr_FR/firefly.php
+++ resources/lang/fr_FR/firefly.php
@@ -416,7 +416,10 @@ return [
     'secure_pw_should'                           => 'Dois-je cocher la case ?',
     'secure_pw_long_password'                    => 'Si vous venez de générer un long mot de passe unique pour Firefly III à l\'aide d\'un type de générateur de mot de passe : <strong>no</strong>.',
     'secure_pw_short'                            => 'Si vous venez d\'entrer le mot de passe que vous utilisez toujours : <em>S\'il vous plaît, oui</em>.',
-
+    'personal_access_token'                      => 'Personal access token',
+    'explain_access_token'                       => 'You need this token to perform command line options, such as importing or exporting data. Without it, such sensitive commands will not work. Do not share your access token. Nobody will ask you for this token, not even me. If you fear you lost this, or when you\'re paranoid, regenerate this token using the button.',
+    'regenerate_access_token'                    => 'Regenerate access token',
+    'token_regenerated'                          => 'A new token was generated',
 
     // attachments
     'nr_of_attachments'                          => 'Une pièce jointe|:count pièces jointes',
