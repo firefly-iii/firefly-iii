08bc3e7596c5f81bc3d08311612792f6a5d55092
James Cole <JC5@users.noreply.github.com>
2017-09-26 09:00:55 +0200

    New translations form.php (Spanish)

--- resources/lang/es_ES/form.php
+++ resources/lang/es_ES/form.php
@@ -67,6 +67,8 @@ return [
     'source_amount'                  => 'Importe (origen)',
     'destination_amount'             => 'Importe (destino)',
     'native_amount'                  => 'Native amount',
+    'new_email_address'              => 'New email address',
+    'verification'                   => 'Verification',
 
     'source_account_asset'        => 'Source account (asset account)',
     'destination_account_expense' => 'Destination account (expense account)',
