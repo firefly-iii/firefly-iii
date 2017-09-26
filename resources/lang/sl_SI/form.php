acea869a98d2cacf2aa75d8d46f67d6684d10bf4
James Cole <JC5@users.noreply.github.com>
2017-09-26 09:00:49 +0200

    New translations form.php (Slovenian)

--- resources/lang/sl_SI/form.php
+++ resources/lang/sl_SI/form.php
@@ -67,6 +67,8 @@ return [
     'source_amount'                  => 'Amount (source)',
     'destination_amount'             => 'Amount (destination)',
     'native_amount'                  => 'Native amount',
+    'new_email_address'              => 'New email address',
+    'verification'                   => 'Verification',
 
     'source_account_asset'        => 'vir (premoženjski račun)',
     'destination_account_expense' => 'ciljni konto (stroški)',
