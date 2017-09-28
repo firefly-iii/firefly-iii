8b36c17e30557f427305f58a978512ebcc080ccc
James Cole <JC5@users.noreply.github.com>
2017-09-28 09:01:00 +0200

    New translations firefly.php (French)

--- resources/lang/fr_FR/firefly.php
+++ resources/lang/fr_FR/firefly.php
@@ -947,6 +947,17 @@ return [
     'delete_journal_link'                   => 'Supprimez le lien entre <a href=":source_link">:source</a> et <a href=":destination_link">:destination</a>',
     'deleted_link'                          => 'Lien supprimé',
 
+    // link translations:
+    'relates to_inward'                     => 'relates to',
+    'is (partially) refunded by_inward'     => 'is (partially) refunded by',
+    'is (partially) paid for by_inward'     => 'is (partially) paid for by',
+    'is (partially) reimbursed by_inward'   => 'is (partially) reimbursed by',
+    'relates to_outward'                    => 'relates to',
+    '(partially) refunds_outward'           => '(partially) refunds',
+    '(partially) pays for_outward'          => '(partially) pays for',
+    '(partially) reimburses_outward'        => '(partially) reimburses',
+
+
     // split a transaction:
     'splits'                                => 'Scinder',
     'add_another_split'                     => 'Ajouter une autre fraction',
