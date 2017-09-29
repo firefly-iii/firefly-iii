aa7e6166c1e61bf4802b32b95ad1e5557eb9ac0d
James Cole <JC5@users.noreply.github.com>
2017-09-29 09:01:51 +0200

    New translations firefly.php (Dutch)

--- resources/lang/nl_NL/firefly.php
+++ resources/lang/nl_NL/firefly.php
@@ -532,6 +532,13 @@ return [
     'update_budget'                              => 'Budget bijwerken',
     'update_budget_amount_range'                 => 'Update het verwacht beschikbare bedrag tussen :start en :end',
     'budget_period_navigator'                    => 'Periodenavigator',
+    'info_on_available_amount'                   => 'What do I have available?',
+    'available_amount_indication'                => 'Use these amounts to get an indication of what your total budget could be.',
+    'amount_budgeted'                            => 'Amount budgeted',
+    'amount_earned'                              => 'Amount earned',
+    'suggested'                                  => 'Suggested',
+    'average_between'                            => 'Average between :start and :end',
+
 
     // bills:
     'matching_on'                                => 'Wordt herkend',
