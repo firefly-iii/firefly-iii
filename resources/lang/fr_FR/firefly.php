6ee40342dc2d81e39966cedad831eb23816c4583
James Cole <JC5@users.noreply.github.com>
2017-09-30 18:00:48 +0200

    New translations firefly.php (French)

--- resources/lang/fr_FR/firefly.php
+++ resources/lang/fr_FR/firefly.php
@@ -328,6 +328,8 @@ return [
     'without_date'                               => 'Sans date',
     'result'                                     => 'Résultat',
     'sums_apply_to_range'                        => 'All sums apply to the selected range',
+    'press_tag_location'                         => 'Right click or long press to set the tag\'s location.',
+    'clear_location'                             => 'Clear location',
 
     // preferences
     'pref_home_screen_accounts'                  => 'Comptes de l’écran d’accueil',
