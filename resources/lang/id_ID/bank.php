cbe123c25d549e3c09278a683cf6356139224532
James Cole <JC5@users.noreply.github.com>
2017-10-03 12:30:21 +0200

    New translations bank.php (Indonesian)

---
+++ resources/lang/id_ID/bank.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'bunq_prerequisites_title' => 'Prerequisites for an import from bunq',
+    'bunq_prerequisites_text'  => 'In order to import from bunq, you need to obtain an API key. You can do this through the app.',
+];
