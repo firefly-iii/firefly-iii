9f622285ca6f6577d5b855b342949614267b338a
James Cole <JC5@users.noreply.github.com>
2017-10-03 12:30:22 +0200

    New translations pagination.php (Indonesian)

---
+++ resources/lang/id_ID/pagination.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * pagination.php
+ * Copyright (C) 2016 thegrumpydictator@gmail.com
+ *
+ * This software may be modified and distributed under the terms of the
+ * Creative Commons Attribution-ShareAlike 4.0 International License.
+ *
+ * See the LICENSE file for details.
+ */
+
+return [
+
+    'previous' => '&laquo; Previous',
+    'next'     => 'Next &raquo;',
+
+];
