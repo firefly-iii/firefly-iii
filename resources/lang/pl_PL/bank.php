f18aeee1fbc77fc6a57ace76b1640de6282d9cdf
James Cole <JC5@users.noreply.github.com>
2017-10-04 20:50:09 +0200

    New translations bank.php (Polish)

--- resources/lang/pl_PL/bank.php
+++ resources/lang/pl_PL/bank.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'bunq_prerequisites_title' => 'Prerequisites for an import from bunq',
-    'bunq_prerequisites_text'  => 'In order to import from bunq, you need to obtain an API key. You can do this through the app.',
+    'bunq_prerequisites_title' => 'Wymagania wstępne dotyczące importowania z bunq',
+    'bunq_prerequisites_text'  => 'Aby zaimportować dane z bunq, musisz uzyskać klucz API. Możesz to zrobić za pośrednictwem aplikacji.',
 ];
