4cdf2184c3e3029ea52293c8ac8ed80cb87e45e5
James Cole <JC5@users.noreply.github.com>
2017-10-09 20:40:09 +0200

    New translations firefly.php (French)

--- resources/lang/fr_FR/firefly.php
+++ resources/lang/fr_FR/firefly.php
@@ -971,6 +971,9 @@ return [
     'this_deposit'                          => 'Ce dépôt',
     'this_transfer'                         => 'Ce transfert',
     'overview_for_link'                     => 'Aperçu pour le type de lien ":name"',
+    'source_transaction'                    => 'Source transaction',
+    'link_description'                      => 'Link description',
+    'destination_transaction'               => 'Destination transaction',
     'delete_journal_link'                   => 'Supprimez le lien entre <a href=":source_link">:source</a> et <a href=":destination_link">:destination</a>',
     'deleted_link'                          => 'Lien supprimé',
 
