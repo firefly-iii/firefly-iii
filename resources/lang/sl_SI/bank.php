64b957fda66d9cf7f56427e187ce90de21ea0bbc
James Cole <JC5@users.noreply.github.com>
2017-10-15 10:40:22 +0200

    New translations bank.php (Slovenian)

--- resources/lang/sl_SI/bank.php
+++ resources/lang/sl_SI/bank.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'bunq_prerequisites_title' => 'Predpogoji za uvoz iz bunq',
+    'bunq_prerequisites_title' => 'Prerequisites for an import from bunq',
     'bunq_prerequisites_text'  => 'Če želite uvoziti iz bunq-a, morate pridobiti ključ za API. To lahko storite s pomočjo aplikacije.',
 ];
