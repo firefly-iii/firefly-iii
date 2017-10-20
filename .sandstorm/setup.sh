a67037d586f903559b6beadebd9eb0b397c2db7b
James Cole <thegrumpydictator@gmail.com>
2017-10-20 14:21:47 +0200

    Update sandstorm files.

--- .sandstorm/setup.sh
+++ .sandstorm/setup.sh
@@ -15,35 +15,40 @@ apt-get install -y python-software-properties software-properties-common
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E9C74FEEA2098A6E
 add-apt-repository "deb http://packages.dotdeb.org jessie all"
 
+# add another repos
+apt-get install apt-transport-https lsb-release ca-certificates
+wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list
+
 # install packages.
 apt-get update
-apt-get install -y nginx php7.0-fpm php7.0-mysql php7.0-cli php7.0-curl git php7.0-dev php7.0-intl php7.0-dom php7.0-mbstring php7.0-bcmath mysql-server
+apt-get install -y nginx php7.1-fpm php7.1-mysql php7.1-cli php7.1-curl git php7.1-dev php7.1-zip php7.1-intl php7.1-dom php7.1-mbstring php7.1-bcmath mysql-server
 service nginx stop
-service php7.0-fpm stop
+service php7.1-fpm stop
 service mysql stop
 systemctl disable nginx
-systemctl disable php7.0-fpm
+systemctl disable php7.1-fpm
 systemctl disable mysql
-# patch /etc/php/7.0/fpm/pool.d/www.conf to not change uid/gid to www-data
+# patch /etc/php/7.1/fpm/pool.d/www.conf to not change uid/gid to www-data
 sed --in-place='' \
        --expression='s/^listen.owner = www-data/;listen.owner = www-data/' \
        --expression='s/^listen.group = www-data/;listen.group = www-data/' \
-       /etc/php/7.0/fpm/pool.d/www.conf
-# patch /etc/php/7.0/fpm/php-fpm.conf to not have a pidfile
+       /etc/php/7.1/fpm/pool.d/www.conf
+# patch /etc/php/7.1/fpm/php-fpm.conf to not have a pidfile
 sed --in-place='' \
        --expression='s/^pid =/;pid =/' \
-       /etc/php/7.0/fpm/php-fpm.conf
+       /etc/php/7.1/fpm/php-fpm.conf
 
 # move sock file to better dir:
 sed --in-place='' \
-       --expression='s/^listen = \/run\/php\/php7.0-fpm.sock/listen = \/var\/run\/php7.0-fpm.sock/' \
-       /etc/php/7.0/fpm/pool.d/www.conf
+       --expression='s/^listen = \/run\/php\/php7.1-fpm.sock/listen = \/var\/run\/php7.1-fpm.sock/' \
+       /etc/php/7.1/fpm/pool.d/www.conf
 
-# patch /etc/php/7.0/fpm/pool.d/www.conf to no clear environment variables
+# patch /etc/php/7.1/fpm/pool.d/www.conf to no clear environment variables
 # so we can pass in SANDSTORM=1 to apps
 sed --in-place='' \
        --expression='s/^;clear_env = no/clear_env=no/' \
-       /etc/php/7.0/fpm/pool.d/www.conf
+       /etc/php/7.1/fpm/pool.d/www.conf
 # patch mysql conf to not change uid, and to use /var/tmp over /tmp
 # for secure-file-priv see https://github.com/sandstorm-io/vagrant-spk/issues/195
 sed --in-place='' \
