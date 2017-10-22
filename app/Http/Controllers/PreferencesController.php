3f681bf71b905d3587ed8d3dec2346d03906ab74
James Cole <thegrumpydictator@gmail.com>
2017-10-22 20:22:02 +0200

    Key without lengt. [skip ci]

--- app/Http/Controllers/PreferencesController.php
+++ app/Http/Controllers/PreferencesController.php
@@ -67,7 +67,7 @@ class PreferencesController extends Controller
     public function code(Google2FA $google2fa)
     {
         $domain = $this->getDomain();
-        $secret = $google2fa->generateSecretKey(16);
+        $secret = $google2fa->generateSecretKey();
         Session::flash('two-factor-secret', $secret);
         $image = $google2fa->getQRCodeInline('Firefly III at ' . $domain, auth()->user()->email, $secret, 150);
 
