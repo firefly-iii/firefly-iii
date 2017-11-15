845d7d701f14bc86004f7340275b61ab4bc366d4
James Cole <thegrumpydictator@gmail.com>
2017-11-15 10:36:50 +0100

    Update language files.

--- resources/lang/ru_RU/firefly.php
+++ resources/lang/ru_RU/firefly.php
@@ -682,7 +682,7 @@ return [
     'mass_delete_journals'                     => 'Delete a number of transactions',
     'mass_edit_journals'                       => 'Edit a number of transactions',
     'cannot_edit_other_fields'                 => 'You cannot mass-edit other fields than the ones here, because there is no room to show them. Please follow the link and edit them by one-by-one, if you need to edit these fields.',
-    'no_budget'                                => 'none',
+    'no_budget'                                => '(no budget)',
     'perm-delete-many'                         => 'Deleting many items in one go can be very disruptive. Please be cautious.',
     'mass_deleted_transactions_success'        => 'Deleted :amount transaction(s).',
     'mass_edited_transactions_success'         => 'Updated :amount transaction(s)',
