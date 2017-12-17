6c3204a8cee6dafe2dc516e02d0015177515da6c
James Cole <thegrumpydictator@gmail.com>
2017-12-17 14:43:13 +0100

    Update copyright [skip ci]

--- public/js/ff/transactions/single/create.js
+++ public/js/ff/transactions/single/create.js
@@ -15,7 +15,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Firefly III.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Firefly III. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /** global: currencyInfo, overruleCurrency,useAccountCurrency, accountInfo, what,Modernizr, title, breadcrumbs, middleCrumbName, button, piggiesLength, txt, middleCrumbUrl,exchangeRateInstructions, convertForeignToNative, convertSourceToDestination, selectsForeignCurrency, accountInfo */
