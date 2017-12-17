1f6e42afd92d83d031cd6e8bdd4b7b8501bda31f
James Cole <thegrumpydictator@gmail.com>
2017-12-17 14:44:05 +0100

    Update copyright [skip ci]

--- app/Services/Spectre/Request/SpectreRequest.php
+++ app/Services/Spectre/Request/SpectreRequest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * BunqRequest.php
+ * SpectreRequest.php
  * Copyright (c) 2017 thegrumpydictator@gmail.com
  *
  * This file is part of Firefly III.
@@ -16,7 +16,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Firefly III.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Firefly III. If not, see <http://www.gnu.org/licenses/>.
  */
 declare(strict_types=1);
 
