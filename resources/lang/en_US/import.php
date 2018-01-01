06fa2ab7a10a1b2f0787327232481f87acde49ef
James Cole <thegrumpydictator@gmail.com>
2018-01-01 15:37:15 +0100

    Update string in Spectre import, as suggested by @pkoziol

--- resources/lang/en_US/import.php
+++ resources/lang/en_US/import.php
@@ -153,6 +153,6 @@ return [
     // Spectre
     'spectre_title'                   => 'Import using Spectre',
     'spectre_prerequisites_title'     => 'Prerequisites for an import using Spectre',
-    'spectre_prerequisites_text'      => 'In order to import data using the Spectre API, you need to prove some secrets. They can be found on the <a href="https://www.saltedge.com/clients/profile/secrets">secrets page</a>.',
+    'spectre_prerequisites_text'      => 'In order to import data using the Spectre API, you must provide Firefly III with two secret values. They can be found on the <a href="https://www.saltedge.com/clients/profile/secrets">secrets page</a>.',
     'spectre_enter_pub_key'           => 'The import will only work when you enter this public key on your <a href="https://www.saltedge.com/clients/security/edit">security page</a>.',
 ];
