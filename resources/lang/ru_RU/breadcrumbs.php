2fc760780edd295bdae602176706218614c538b8
James Cole <thegrumpydictator@gmail.com>
2018-01-08 19:19:03 +0100

    Add support for Russian.

---
+++ resources/lang/ru_RU/breadcrumbs.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * breadcrumbs.php
+ * Copyright (c) 2017 thegrumpydictator@gmail.com
+ *
+ * This file is part of Firefly III.
+ *
+ * Firefly III is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Firefly III is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Firefly III. If not, see <http://www.gnu.org/licenses/>.
+ */
+declare(strict_types=1);
+
+return [
+    'home'                => 'Главная',
+    'edit_currency'       => 'Редактирование валюты ":name"',
+    'delete_currency'     => 'Удаление валюты ":name"',
+    'newPiggyBank'        => 'Создание новой копилки',
+    'edit_piggyBank'      => 'Редактирование копилки ":name"',
+    'preferences'         => 'Настройки',
+    'profile'             => 'Профиль',
+    'changePassword'      => 'Изменение вашего пароля',
+    'change_email'        => 'Изменить адрес электронной почты',
+    'bills'               => 'Счета к оплате',
+    'newBill'             => 'Новый счёт к оплате',
+    'edit_bill'           => 'Редактирование счёта к оплате ":name"',
+    'delete_bill'         => 'Удаление счёта к оплате ":name"',
+    'reports'             => 'Отчёты',
+    'search_result'       => 'Результаты поиска для ":query"',
+    'withdrawal_list'     => 'Мои расходы',
+    'deposit_list'        => 'Мои доходы',
+    'transfer_list'       => 'Переводы',
+    'transfers_list'      => 'Переводы',
+    'reconciliation_list' => 'Сверка',
+    'create_withdrawal'   => 'Создать новый расход',
+    'create_deposit'      => 'Создать новый доход',
+    'create_transfer'     => 'Создать новый перевод',
+    'edit_journal'        => 'Редактирование транзакции ":description"',
+    'edit_reconciliation' => 'Редактировать ":description"',
+    'delete_journal'      => 'Удаление транзакции ":description"',
+    'tags'                => 'Метки',
+    'createTag'           => 'Создать новую метку',
+    'edit_tag'            => 'Редактирование метки ":tag"',
+    'delete_tag'          => 'Удаление метки ":tag"',
+    'delete_journal_link' => 'Удалить связь между транзакциями',
+];
---
+++ resources/lang/ru_RU/demo.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * demo.php
+ * Copyright (c) 2017 thegrumpydictator@gmail.com
+ *
+ * This file is part of Firefly III.
+ *
+ * Firefly III is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Firefly III is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Firefly III. If not, see <http://www.gnu.org/licenses/>.
+ */
+declare(strict_types=1);
+
+return [
+    'no_demo_text'                   => 'Извините, но для <abbr title=":route">этой страницы</abbr> нет дополнительного пояснения.',
+    'see_help_icon'                  => 'Воспользуйтесь значком <i class="fa fa-question-circle"></i> в правом верхнем углу, чтобы узнать больше.',
+    'index'                          => 'Добро пожаловать в <strong>Firefly III</strong>! На этой странице вы видите вашу финансовую ситуацию в общих чертах. Более подробная информация доступна на страницах &rarr; <a href=":asset">Активные счета</a> <a href=":budgets">Бюджет</a> и <a href=":reports">Отчёты</a>. Или просто внимательно оглядитесь и изучите всё вокруг.',
+    'accounts-index'                 => 'Основные счета - это ваши личные банковские счета или другие денежные активы. Счёта расходов - это счета, на которые вы тратите деньги, например, магазины и друзья. Счета доходов - это счета, с которых вы получаете деньги, например, ваша работа, правительство или другие источники дохода. На этой странице вы можете редактировать или удалять их.',
+    'budgets-index'                  => 'На этой странице вы видите все свои бюджеты. На верхней панели показана сумма, доступная в рамках бюджета. Бюджет можно настроить на любой период, щёлкнув сумму справа. Сумма, которую вы фактически потратили, показана на диаграмме внизу. Ещё ниже показаны в сравнении ваши фактически расходы и запланированный бюджет.',
+    'reports-index-start'            => 'Firefly III поддерживает несколько типов отчетов. Вы можете узнать про них, нажав на значок <i class="fa fa-question-circle"></i> в правом верхнем углу.',
+    'reports-index-examples'         => 'Обязательно ознакомьтесь с этими примерами: <a href=":one">a ежемесячный финансовый обзор</a>, <a href=":two">a годовой финансовый обзор</a> и <a href=":three">a обзор бюджета</a>.',
+    'currencies-index'               => 'Firefly III поддерживает несколько валют. Хотя по умолчанию используется Евро, вы можете сделать основной валютой доллары США или любую другую валюту. Как вы видите, несколько валют уже есть в списке, но вы можете добавить любую другую, если это требуется. Обратите внимание, что выбор новой валюты по умолчанию не повлияет на уже существующие транзакции: Firefly III поддерживает одновременное использование нескольких валют.',
+    'transactions-index'             => 'Эти расходы, доходы и переводы не очень интересны. Они были созданы автоматически.',
+    'piggy-banks-index'              => 'Как вы можете видеть, здесь есть три копилки. Используйте кнопки «плюс» и «минус», чтобы влиять на количество денег в каждой копилке. Нажмите название копилки, чтобы увидеть её настройки.',
+    'import-index'                   => 'Конечно, любой CSV-файл может быть импортирован в Firefly III',
+    'import-configure-security'      => 'По соображениям безопасности, загружаемый вами файл был заменен локальным.',
+    'import-configure-configuration' => 'Конфигурация, которую вы видите ниже, корректна для локального файла.',
+];
