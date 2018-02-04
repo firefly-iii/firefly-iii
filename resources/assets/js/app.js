83510209136357afbd0ac6ddd727e5a2a535efe6
James Cole <thegrumpydictator@gmail.com>
2018-02-04 08:15:20 +0100

    Add passport components to app.js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -17,6 +17,25 @@ window.Vue = require('vue');
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
+Vue.component(
+    'passport-clients',
+    require('./components/passport/Clients.vue')
+);
+
+Vue.component(
+    'passport-authorized-clients',
+    require('./components/passport/AuthorizedClients.vue')
+);
+
+Vue.component(
+    'passport-personal-access-tokens',
+    require('./components/passport/PersonalAccessTokens.vue')
+);
+
+
+
+
+
 const app = new Vue({
     el: '#app'
 });
