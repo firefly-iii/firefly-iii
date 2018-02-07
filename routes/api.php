809e40c5ce3e8f18d5a02f3632590ee95cd24d83
James Cole <thegrumpydictator@gmail.com>
2018-02-07 11:20:37 +0100

    Remove double middleware from routes.

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@
  */
 
 Route::group(
-    ['middleware' => 'auth:api', 'namespace' => 'FireflyIII\Api\V1\Controllers', 'prefix' => 'bill', 'as' => 'api.v1.bills.'], function () {
+    ['namespace' => 'FireflyIII\Api\V1\Controllers', 'prefix' => 'bill', 'as' => 'api.v1.bills.'], function () {
 
     // Bills API routes:
     Route::get('', ['uses' => 'BillController@index', 'as' => 'index']);
