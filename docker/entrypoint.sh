55c4d5fe432783fc9800011d7600515e7bafb05a
UnrealKazu <9115757+UnrealKazu@users.noreply.github.com>
2018-02-20 22:06:02 +0100

    Remove DB_ variables when using sqlite
    When the user has selected sqlite using environment variables, we need to remove all DB_* variables other than DB_CONNECTION.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,7 +4,14 @@
 chown -R www-data:www-data $FIREFLY_PATH/storage/export $FIREFLY_PATH/storage/upload
 chmod -R 775 $FIREFLY_PATH/storage/export $FIREFLY_PATH/storage/upload
 
-cat .env.docker | envsubst > .env && cat .env
+cat .env.docker | envsubst > .env
+
+# if the database is an sqlite database, we need to remove all non-connection variables
+if [ "$FF_DB_CONNECTION" == "sqlite" ]; then
+    sed -i '/DB_/{/DB_CONNECTION/!d;}' .env
+fi
+
+cat .env
 composer dump-autoload
 php artisan optimize
 php artisan package:discover
