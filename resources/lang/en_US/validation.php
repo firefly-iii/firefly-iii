e658d447ca794d100e0f8f99461eccb3cfbb0a40
James Cole <thegrumpydictator@gmail.com>
2018-02-26 21:09:33 +0100

    Update validation.

--- resources/lang/en_US/validation.php
+++ resources/lang/en_US/validation.php
@@ -23,6 +23,7 @@ declare(strict_types=1);
 return [
     'iban'                           => 'This is not a valid IBAN.',
     'unique_account_number_for_user' => 'It looks like this account number is already in use.',
+    'unique_iban_for_user'           => 'It looks like this IBAN is already in use.',
     'deleted_user'                   => 'Due to security constraints, you cannot register using this email address.',
     'rule_trigger_value'             => 'This value is invalid for the selected trigger.',
     'rule_action_value'              => 'This value is invalid for the selected action.',
@@ -30,6 +31,7 @@ return [
     'file_attached'                  => 'Succesfully uploaded file ":name".',
     'must_exist'                     => 'The ID in field :attribute does not exist in the database.',
     'all_accounts_equal'             => 'All accounts in this field must be equal.',
+    'invalid_selection'              => 'Your selection is invalid',
     'belongs_user'                   => 'This value is invalid for this field.',
     'at_least_one_transaction'       => 'Need at least one transaction.',
     'require_currency_info'          => 'The content of this field is invalid without currency information.',
