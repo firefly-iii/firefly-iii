7812a1bb51ac3c1c51c25175fbdbce5773586759
James Cole <thegrumpydictator@gmail.com>
2018-04-06 18:14:48 +0200

    Fix for #1320

--- .deploy/docker/entrypoint.sh
+++ .deploy/docker/entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # make sure we own the volumes:
-chown -R www-data:www-data $FIREFLY_PATH/storage/export $FIREFLY_PATH/storage/upload
-chmod -R 775 $FIREFLY_PATH/storage/export $FIREFLY_PATH/storage/upload
+chown -R www-data:www-data -R $FIREFLY_PATH/storage/export $FIREFLY_PATH/storage/upload $FIREFLY_PATH/storage/logs
+chmod -R 775 $FIREFLY_PATH/storage/export $FIREFLY_PATH/storage/upload $FIREFLY_PATH/storage/upload $FIREFLY_PATH/storage/logs
+
+# remove any lingering files that may break upgrades:
+rm -f $FIREFLY_PATH/storage/logs/laravel.log
 
 cat .env.docker | envsubst > .env && cat .env
 composer dump-autoload
