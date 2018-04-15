dbe17debb4611d9346a024793a732fc0d30de6ff
Brenden Conte <brenden@thecontes.info>
2018-04-15 02:35:47 -0400

    Remove the 'php artisan optimize' line from .deploy/docker/entroypoint.sh because the command was removed in artisan 5.6

--- .deploy/docker/entrypoint.sh
+++ .deploy/docker/entrypoint.sh
@@ -9,7 +9,6 @@ rm -f $FIREFLY_PATH/storage/logs/laravel.log
 
 cat .env.docker | envsubst > .env && cat .env
 composer dump-autoload
-php artisan optimize
 php artisan package:discover
 php artisan firefly:instructions install
 exec apache2-foreground
