9b376039e2d66b8cad5c2e1ec4a8780979ee69f3
user <user@thecontes.info>
2018-04-15 02:01:34 -0400

    Remove the 'php artisan optimize' command from the entrypoint.sh file since the optimize command was removed from artisan in 5.6

--- .deploy/docker/entrypoint.sh
+++ .deploy/docker/entrypoint.sh
@@ -6,7 +6,6 @@ chmod -R 775 $FIREFLY_PATH/storage/export $FIREFLY_PATH/storage/upload
 
 cat .env.docker | envsubst > .env && cat .env
 composer dump-autoload
-php artisan optimize
 php artisan package:discover
 php artisan firefly:instructions install
 exec apache2-foreground
