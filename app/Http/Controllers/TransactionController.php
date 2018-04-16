49e48725a563c3fb2a132bbbcc8d6087cd2c0269
James Cole <thegrumpydictator@gmail.com>
2018-04-16 18:03:56 +0200

    Debug code for transaction view. [skip ci]

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -228,6 +228,9 @@ class TransactionController extends Controller
         $what     = strtolower($transactionType);
         $subTitle = trans('firefly.' . $what) . ' "' . $journal->description . '"';
 
+        var_dump($transactions);
+        exit;
+
         return view('transactions.show', compact('journal', 'events', 'subTitle', 'what', 'transactions', 'linkTypes', 'links'));
     }
 
