c6b60ff6b4e7ce708a72ac4537d2831c61e01d12
James Cole <thegrumpydictator@gmail.com>
2018-07-01 21:32:48 +0200

    Different order for recurrences [skip ci]

--- app/Repositories/Recurring/RecurringRepository.php
+++ app/Repositories/Recurring/RecurringRepository.php
@@ -71,7 +71,7 @@ class RecurringRepository implements RecurringRepositoryInterface
         return $this->user->recurrences()
                           ->with(['TransactionCurrency', 'TransactionType', 'RecurrenceRepetitions', 'RecurrenceTransactions'])
                           ->orderBy('active', 'DESC')
-                          ->orderBy('transaction_type_id', 'DESC')
+                          ->orderBy('transaction_type_id', 'ASC')
                           ->orderBy('title', 'ASC')
                           ->get();
     }
