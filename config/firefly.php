10f195d334b8d7552bd6117b4d880134aa1f0e88
James Cole <thegrumpydictator@gmail.com>
2018-07-02 20:18:00 +0200

    Update versions.

--- config/firefly.php
+++ config/firefly.php
@@ -88,8 +88,8 @@ return [
         'is_demo_site'     => false,
     ],
     'encryption'               => null === env('USE_ENCRYPTION') || env('USE_ENCRYPTION') === true,
-    'version'                  => '4.7.4',
-    'api_version'              => '0.3',
+    'version'                  => '4.7.5',
+    'api_version'              => '0.4',
     'db_version'               => 4,
     'maxUploadSize'            => 15242880,
     'allowedMimes'             => [
