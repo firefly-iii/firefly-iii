df0e2dd2a281c3078dfaf73f0262f2dedc167951
James Cole <thegrumpydictator@gmail.com>
2018-08-17 06:39:48 +0200

    Bump version and warn about PHP 7.2

--- config/firefly.php
+++ config/firefly.php
@@ -88,8 +88,8 @@ return [
         'is_demo_site'     => false,
     ],
     'encryption'               => null === env('USE_ENCRYPTION') || env('USE_ENCRYPTION') === true,
-    'version'                  => '4.7.5.3',
-    'api_version'              => '0.6',
+    'version'                  => '4.7.6',
+    'api_version'              => '0.7',
     'db_version'               => 4,
     'maxUploadSize'            => 15242880,
     'allowedMimes'             => [
