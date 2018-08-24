20490fcd8016e14985113272a2134ac20abea0a1
James Cole <thegrumpydictator@gmail.com>
2018-08-24 21:14:04 +0200

    Update JS

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -37,21 +37,3 @@ if (token) {
    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
 
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
-//
-// import Echo from 'laravel-echo'
-//
-// window.Pusher = require('pusher-js');
-//
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: 'your-pusher-key',
-//     cluster: 'mt1',
-//     encrypted: true
-// });
-
-/** Include font awesome */
