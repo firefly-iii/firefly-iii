d7bf9e234cfe8476215ac69f0e5ab96d69dcb8c8
James Cole <thegrumpydictator@gmail.com>
2019-01-01 18:03:24 +0100

    Fix test coverage.

--- tests/Feature/Controllers/Chart/AccountControllerTest.php
+++ tests/Feature/Controllers/Chart/AccountControllerTest.php
@@ -239,6 +239,8 @@ class AccountControllerTest extends TestCase
         $accountRepos  = $this->mock(AccountRepositoryInterface::class);
         $currencyRepos = $this->mock(CurrencyRepositoryInterface::class);
         $fiscalHelper  = $this->mock(FiscalHelperInterface::class);
+        $fiscalHelper->shouldReceive('endOfFiscalYear')->andReturn(new Carbon);
+        $fiscalHelper->shouldReceive('startOfFiscalYear')->andReturn(new Carbon);
         // change the preference:
         Preferences::setForUser($this->user(), 'frontPageAccounts', []);
 
@@ -381,6 +383,8 @@ class AccountControllerTest extends TestCase
         $accountRepos  = $this->mock(AccountRepositoryInterface::class);
         $currencyRepos = $this->mock(CurrencyRepositoryInterface::class);
         $fiscalHelper  = $this->mock(FiscalHelperInterface::class);
+        $fiscalHelper->shouldReceive('endOfFiscalYear')->andReturn(new Carbon);
+        $fiscalHelper->shouldReceive('startOfFiscalYear')->andReturn(new Carbon);
         // grab two expense accounts from the current user.
         $accounts = $this->user()->accounts()->where('account_type_id', 5)->take(2)->get();
 
