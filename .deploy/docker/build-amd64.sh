33ccf7c7070c1e80ac04d977c5dcbd42edd623ac
James Cole <thegrumpydictator@gmail.com>
2019-02-02 17:59:18 +0100

    Improve build scripts.

--- .deploy/docker/build-amd64.sh
+++ .deploy/docker/build-amd64.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
 # build image
-docker build -t jc5x/ff-test-builds:develop -f Dockerfile .
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 if [ "$TRAVIS_BRANCH" == "develop" ]; then
+    echo "Build develop amd64"
+    docker build -t jc5x/firefly-iii:develop -f Dockerfile .
     docker push jc5x/firefly-iii:develop
 fi
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
-    docker login -u="$DOCKER_USER" -p="$DOCKER_PASS"
+    echo "Build master amd64"
+    docker build -t jc5x/firefly-iii:latest -f Dockerfile .
+    docker tag jc5x/firefly-iii:latest jc5x/firefly-iii:release-$VERSION
     docker push jc5x/firefly-iii:latest
     docker push jc5x/firefly-iii:release-$VERSION
 fi
--- .deploy/docker/build-arm.sh
+++ .deploy/docker/build-arm.sh
@@ -9,15 +9,18 @@ tar xzf qemu-arm-static.tar.gz && \
 popd
 
 # build image
-docker build -t jc5x/ff-test-builds:develop -f Dockerfile-ARM .
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 if [ "$TRAVIS_BRANCH" == "develop" ]; then
+    echo "Build develop arm"
+    docker build -t jc5x/firefly-iii:develop-arm -f Dockerfile-ARM .
     docker push jc5x/firefly-iii:develop-arm
 fi
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
-    docker login -u="$DOCKER_USER" -p="$DOCKER_PASS"
+    echo "Build master arm"
+    docker build -t jc5x/firefly-iii:latest-arm -f Dockerfile-ARM .
+    docker tag jc5x/firefly-iii:latest-arm jc5x/firefly-iii:release-$VERSION-arm
     docker push jc5x/firefly-iii:latest-arm
     docker push jc5x/firefly-iii:release-$VERSION-arm
 fi
