88d65b4784e20430309109f3698242a4d1d3098e
James Cole <thegrumpydictator@gmail.com>
2019-02-02 19:22:03 +0100

    Switch args around.

--- .deploy/docker/build-arm.sh
+++ .deploy/docker/build-arm.sh
@@ -15,13 +15,13 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 if [ "$TRAVIS_BRANCH" == "develop" ]; then
     echo "Build develop arm"
-    docker build --platform arm -t jc5x/firefly-iii:develop-arm -f Dockerfile-ARM .
-    docker push jc5x/firefly-iii:develop-arm
+    docker build --tag jc5x/firefly-iii:develop-arm --file Dockerfile-ARM .
+    docker push jc5x/firefly-iii:develop-arm --platform arm
 fi
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
     echo "Build master arm"
-    docker build --platform arm -t jc5x/firefly-iii:latest-arm -f Dockerfile-ARM .
+    docker build --platform arm --tag jc5x/firefly-iii:latest-arm --file Dockerfile-ARM .
     docker tag jc5x/firefly-iii:latest-arm jc5x/firefly-iii:release-$VERSION-arm
     docker push jc5x/firefly-iii:latest-arm
     docker push jc5x/firefly-iii:release-$VERSION-arm
