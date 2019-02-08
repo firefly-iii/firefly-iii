21ddbd220ab6101263341150bdb912aed5be04a6
James Cole <thegrumpydictator@gmail.com>
2019-02-08 17:40:29 +0100

    Include lib for arm

--- .deploy/docker/build-generic.sh
+++ .deploy/docker/build-generic.sh
@@ -3,6 +3,15 @@
 # build image
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
+# get qemu-arm-static binary
+if [ "$TRAVIS_BRANCH" != "amd64" ]; then
+    mkdir tmp
+    pushd tmp && \
+    curl -L -o qemu-arm-static.tar.gz https://github.com/multiarch/qemu-user-static/releases/download/v2.6.0/qemu-arm-static.tar.gz && \
+    tar xzf qemu-arm-static.tar.gz && \
+    popd
+fi
+
 # build develop
 if [ "$TRAVIS_BRANCH" == "develop" ]; then
     echo "Build develop $ARCH"
