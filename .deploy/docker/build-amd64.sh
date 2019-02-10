ccf60f4cdcdc290e9367864c318348f4239399b4
James Cole <thegrumpydictator@gmail.com>
2019-02-10 08:07:25 +0100

    Back to classic build.

--- .deploy/docker/build-amd64.sh
+++ .deploy/docker/build-amd64.sh
@@ -5,13 +5,13 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 if [ "$TRAVIS_BRANCH" == "develop" ]; then
     echo "Build develop amd64"
-    docker build -t jc5x/firefly-iii:develop-amd -f Dockerfile .
+    docker build -t jc5x/firefly-iii:develop-amd -f Dockerfile.amd64 .
     docker push jc5x/firefly-iii:develop-amd
 fi
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
     echo "Build master amd64"
-    docker build -t jc5x/firefly-iii:latest-amd -f Dockerfile .
+    docker build -t jc5x/firefly-iii:latest-amd -f Dockerfile.amd64 .
     docker tag jc5x/firefly-iii:latest-amd jc5x/firefly-iii:release-$VERSION-amd
     docker push jc5x/firefly-iii:latest-amd
     docker push jc5x/firefly-iii:release-$VERSION-amd
