7526f13ca9b70547df758580f847a1eb21ede64d
James Cole <thegrumpydictator@gmail.com>
2019-02-11 17:07:34 +0100

    Update build script.

--- .deploy/docker/build-amd64.sh
+++ .deploy/docker/build-amd64.sh
@@ -3,16 +3,18 @@
 # build image
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
+
+
 if [ "$TRAVIS_BRANCH" == "develop" ]; then
     echo "Build develop amd64"
-    docker build -t jc5x/firefly-iii:develop-amd -f Dockerfile.amd64 .
-    docker push jc5x/firefly-iii:develop-amd
+    docker build -t jc5x/firefly-iii:develop-amd64 -f Dockerfile.amd64 .
+    docker push jc5x/firefly-iii:develop-amd64
 fi
 
 if [ "$TRAVIS_BRANCH" == "master" ]; then
     echo "Build master amd64"
-    docker build -t jc5x/firefly-iii:latest-amd -f Dockerfile.amd64 .
-    docker tag jc5x/firefly-iii:latest-amd jc5x/firefly-iii:release-$VERSION-amd
-    docker push jc5x/firefly-iii:latest-amd
-    docker push jc5x/firefly-iii:release-$VERSION-amd
+    docker build -t jc5x/firefly-iii:latest-amd64 -f Dockerfile.amd64 .
+    docker tag jc5x/firefly-iii:latest-amd64 jc5x/firefly-iii:release-$VERSION-amd64
+    docker push jc5x/firefly-iii:latest-amd64
+    docker push jc5x/firefly-iii:release-$VERSION-amd64
 fi
