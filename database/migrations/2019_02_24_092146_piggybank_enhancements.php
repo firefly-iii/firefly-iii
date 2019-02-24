fc8ad1ab6a5198cabe2beccd680c5984182cd369
Mr. Funk <funk@Funks-Mac-Pro-4.local>
2019-02-24 11:40:23 -0500

    init migration

---
+++ database/migrations/2019_02_24_092146_piggybank_enhancements.php
@@ -0,0 +1,94 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class PiggybankEnhancements extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('piggy_bank_accounts')) {
+            Schema::create(
+                'piggy_bank_accounts',
+                function (Blueprint $table) {
+                    $table->integer('piggy_bank_id', false, true);
+                    $table->integer('account_id', false, true);
+                    $table->foreign('piggy_bank_id')->references('id')->onUpdate('cascade')->on('piggy_banks')->onDelete('cascade');
+                    $table->foreign('account_id')->references('id')->onUpdate('cascade')->on('accounts')->onDelete('cascade');
+                    $table->primary(['piggy_bank_id', 'account_id']);
+                }
+            );
+        }
+
+        Schema::table(
+            'piggy_bank_events',
+            function (Blueprint $table) {
+                $table->integer('account_id', false, true)->after('piggy_bank_id')->nullable();;
+            }
+        );
+        DB::statement('
+        UPDATE
+                piggy_bank_events t1,
+                piggy_banks t2
+        SET
+                t1.account_id = t2.account_id
+        WHERE
+                t1.piggy_bank_id = t2.id;'
+        );
+        // DB::statement('ALTER TABLE `piggy_bank_events` MODIFY `age` DATETIME');
+
+        // DB::statement('UPDATE `piggy_bank_events`
+        // SET
+        // `id` = <{id: }>,
+        // `created_at` = <{created_at: }>,
+        // `updated_at` = <{updated_at: }>,
+        // `piggy_bank_id` = <{piggy_bank_id: }>,
+        // `account_id` = <{account_id: }>,
+        // `transaction_journal_id` = <{transaction_journal_id: }>,
+        // `date` = <{date: }>,
+        // `amount` = <{amount: }>
+        // WHERE `id` = <{expr}>;
+        // SELECT * FROM fireflydev.piggy_bank_events;');
+
+        // setup composite keys
+        // $piggy_banks = DB::Select(DB::Raw('SELECT * FROM piggy_banks'));
+
+        //     DB::Statement('
+        //     INSERT INTO account (created_at, updated_at, name, project_id)
+        //     VALUES (NOW(), NOW(), "Migrated account", ' . $piggy_bank->account_id . ')
+        // ');
+        //     foreach ($piggy_banks as $piggy_bank) {
+        //         DB::Statement('
+        //     INSERT INTO account (created_at, updated_at, name, project_id)
+        //     VALUES (NOW(), NOW(), "Migrated account", ' . $project->project_id . ')
+        // ');
+        //     }
+        // update with id
+        // add foreign key
+        // Schema::table(
+        //     'piggy_bank_events',
+        //     function (Blueprint $table) {
+        //         $table->foreign('account_id')->references('id')->onUpdate('cascade')->on('accounts')->onDelete('cascade');
+        //     }
+        // );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('piggy_bank_accounts');
+        Schema::table('piggy_bank_events', function (Blueprint $table) {
+            $table->dropColumn(['account_id']);
+        });
+    }
+}
