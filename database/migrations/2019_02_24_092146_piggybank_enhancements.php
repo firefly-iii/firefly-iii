781aee1760c043b2f9d5d3681bc092d06b0e0b1e
Mr. Funk <funk@Funks-Mac-Pro-4.local>
2019-02-24 15:44:10 -0500

    migrate checkpoinbt

--- database/migrations/2019_02_24_092146_piggybank_enhancements.php
+++ database/migrations/2019_02_24_092146_piggybank_enhancements.php
@@ -13,25 +13,30 @@ class PiggybankEnhancements extends Migration
      */
     public function up()
     {
-        if (!Schema::hasTable('piggy_bank_accounts')) {
-            Schema::create(
-                'piggy_bank_accounts',
-                function (Blueprint $table) {
-                    $table->integer('piggy_bank_id', false, true);
-                    $table->integer('account_id', false, true);
-                    $table->foreign('piggy_bank_id')->references('id')->onUpdate('cascade')->on('piggy_banks')->onDelete('cascade');
-                    $table->foreign('account_id')->references('id')->onUpdate('cascade')->on('accounts')->onDelete('cascade');
-                    $table->primary(['piggy_bank_id', 'account_id']);
-                }
-            );
-        }
+        // not really needed can track changes through new columns to events
+        // if (!Schema::hasTable('piggy_bank_accounts')) {
+        //     Schema::create(
+        //         'piggy_bank_accounts',
+        //         function (Blueprint $table) {
+        //             $table->integer('piggy_bank_id', false, true);
+        //             $table->integer('account_id', false, true);
+        //             $table->foreign('piggy_bank_id')->references('id')->onUpdate('cascade')->on('piggy_banks')->onDelete('cascade');
+        //             $table->foreign('account_id')->references('id')->onUpdate('cascade')->on('accounts')->onDelete('cascade');
+        //             $table->primary(['piggy_bank_id', 'account_id']);
+        //         }
+        //     );
+        // }
 
-        Schema::table(
-            'piggy_bank_events',
+        Schema::table('piggy_bank_events',
             function (Blueprint $table) {
-                $table->integer('account_id', false, true)->after('piggy_bank_id')->nullable();;
+                // $table->decimal('transfer', 22, 12)->after('amount')->nullable();
+                $table->integer('account_id', false, true)->after('piggy_bank_id')->nullable();
+                $table->integer('from_account_id', false, true)->after('account_id')->nullable();
+                // $table->foreign('from_account_id')->references('id')->onUpdate('cascade')->on('accounts')->onDelete('cascade');
             }
         );
+
+        // may need to do this statment per db type, e.g. mysql, sqlite, pgres
         DB::statement('
         UPDATE
                 piggy_bank_events t1,
@@ -39,8 +44,14 @@ class PiggybankEnhancements extends Migration
         SET
                 t1.account_id = t2.account_id
         WHERE
-                t1.piggy_bank_id = t2.id;'
-        );
+                t1.piggy_bank_id = t2.id;
+        ');
+
+        Schema::table('piggy_bank_events', function (Blueprint $table) {
+            $table->integer('account_id')->nullable(false)->change();
+            // $table->foreign('account_id')->references('id')->onUpdate('cascade')->on('accounts')->onDelete('cascade');
+        });
+
         // DB::statement('ALTER TABLE `piggy_bank_events` MODIFY `age` DATETIME');
 
         // DB::statement('UPDATE `piggy_bank_events`
@@ -86,9 +97,11 @@ class PiggybankEnhancements extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('piggy_bank_accounts');
+        // Schema::dropIfExists('piggy_bank_accounts');
         Schema::table('piggy_bank_events', function (Blueprint $table) {
+            // $table->dropColumn(['transfer']);
             $table->dropColumn(['account_id']);
+            $table->dropColumn(['from_account_id']);
         });
     }
 }
