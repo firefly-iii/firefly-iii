26bfe620b305bd8772d36703c1b9fcb1aa1399d4
Mr. Funk <funk@Funks-Mac-Pro-4.local>
2019-02-24 22:56:34 -0500

    fix sub aggregate query

--- app/Models/PiggyBank.php
+++ app/Models/PiggyBank.php
@@ -128,7 +128,7 @@ class PiggyBank extends Model
      */
     public function accounts(): HasMany
     {
-        $events = $this->hasMany(PiggyBankEvent::class)->selectRaw('piggy_bank_events.account_id as id,accounts.name,(SUM(piggy_bank_events.amount) + (select SUM(p1.transfer) FROM `piggy_bank_events` as p1 where p1.account_id=piggy_bank_events.account_id) - (select SUM(p2.transfer) FROM `piggy_bank_events` as p2 where p2.from_account_id=piggy_bank_events.account_id)) as total')->join('accounts', 'piggy_bank_events.account_id', '=', 'accounts.id')->groupBy('piggy_bank_events.account_id');
+        $events = $this->hasMany(PiggyBankEvent::class)->selectRaw('piggy_bank_events.account_id as id,accounts.name, SUM(piggy_bank_events.amount) as sum, (select SUM(p1.transfer) FROM `piggy_bank_events` as p1 where p1.account_id=piggy_bank_events.account_id and p1.piggy_bank_id=piggy_bank_events.piggy_bank_id) as transfers, (select SUM(p2.transfer) FROM `piggy_bank_events` as p2 where p2.from_account_id=piggy_bank_events.account_id and p2.piggy_bank_id=piggy_bank_events.piggy_bank_id) as withdrawals')->join('accounts', 'piggy_bank_events.account_id', '=', 'accounts.id')->groupBy('piggy_bank_events.account_id')->groupBy('piggy_bank_events.piggy_bank_id');
         return $events;
     }
 
