a11d2df6bba1425455f711c88731b7734c88a0e1
James Cole <thegrumpydictator@gmail.com>
2019-03-17 09:13:23 +0100

    Norwegian strings.

---
+++ resources/lang/nb_NO/config.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * config.php
+ * Copyright (c) 2018 thegrumpydictator@gmail.com
+ *
+ * This file is part of Firefly III.
+ *
+ * Firefly III is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Firefly III is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Firefly III. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+return [
+    'html_language'         => 'no',
+    'locale'                => 'no, nb, Norsk, nb_NO, nb_NO.utf8, nb_NO.UTF-8',
+    'month'                 => '%B %Y',
+    'month_and_day'         => '%e %B, %Y',
+    'month_and_date_day'    => '%A %B. %e, %Y',
+    'month_and_day_no_year' => '%B. %e',
+    'date_time'             => '%e. %B, %Y @ %T',
+    'specific_day'          => '%e. %B %Y',
+    'week_in_year'          => 'Uke %W, %Y',
+    'year'                  => '%Y',
+    'half_year'             => '%B %Y',
+    'month_js'              => 'MMMM YYYY',
+    'month_and_day_js'      => 'MMMM Do, YYYY',
+    'date_time_js'          => 'MMMM Do, YYYY, @ HH:mm:ss',
+    'specific_day_js'       => 'D MMMM YYYY',
+    'week_in_year_js'       => '[Week] w, YYYY',
+    'year_js'               => 'YYYY',
+    'half_year_js'          => 'Q YYYY',
+    'dow_1'                 => 'Mandag',
+    'dow_2'                 => 'Tirsdag',
+    'dow_3'                 => 'Onsdag',
+    'dow_4'                 => 'Torsdag',
+    'dow_5'                 => 'Fredag',
+    'dow_6'                 => 'Lørdag',
+    'dow_7'                 => 'Søndag',
+];
---
+++ resources/lang/nb_NO/demo.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * demo.php
+ * Copyright (c) 2018 thegrumpydictator@gmail.com
+ *
+ * This file is part of Firefly III.
+ *
+ * Firefly III is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Firefly III is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Firefly III. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+return [
+    'no_demo_text'           => 'Beklager, det finnes ingen ekstra demoforklaring for <abbr title=":route">denne siden</abbr>.',
+    'see_help_icon'          => 'Imidlertid kan <i class="fa fa-question-circle"></i>-ikonet øverst i høyre hjørne fortelle deg mer.',
+    'index'                  => 'Velkommen til <strong>Firefly III</strong>! På denne siden får du en rask oversikt over økonomien din. For mer informasjon, sjekk ut Kontoer &rarr; <a href=":asset">Eiendelskontoer</a> og selvfølgelig <a href=":budgets">Budsjetter</a> og <a href=":reports">Rapporter</a>. Eller bare ta en titt rundt og se hvor du ender opp.',
+    'accounts-index'         => 'Brukskontoer er dine personlige bankkontoer. Kostnadskontoer er kontoene du bruker penger på, for eksempel butikker og venner. Inntektskontoer er kontoer du mottar penger fra, for eksempel jobben din, det offentlige eller andre inntektskilder. På denne siden kan du redigere eller fjerne dem.',
+    'budgets-index'          => 'Denne siden viser en oversikt over budsjettene dine. Den øverste linjen viser beløpet som er tilgjengelig for budsjettering. Dette kan tilpasses for enhver periode ved å klikke på beløpet til høyre. Beløpet du faktisk har brukt, vises i linjen under. Under det er kostnadene per budsjett og hva du har budsjettert for dem.',
+    'reports-index-start'    => 'Firefly III støtter en rekke typer rapporter. Les om dem ved å klikke på <i class="fa fa-question-circle"></i>-ikonet øverst i høyre hjørne.',
+    'reports-index-examples' => 'Husk å sjekke ut disse eksemplene: <a href=":one">månedlig finansiell oversikt</a>, <a href=":two">årlig finansiell oversikt</a> og <a href=":three">budsjettoversikt</a>.',
+    'currencies-index'       => 'Firefly III støtter flere valutaer. Selv om Euro er standard, kan det settes til norske kroner, eller en av mange andre valutaer. Som du kan se, så er et lite utvalg valutaer allerede lagt inn, men du kan legge til flere hvis du vil. Hvis du endrer standardvalutaen, endres ikke valutaen for eksisterende transaksjoner, men Firefly III støtter bruken av flere valutaer samtidig.',
+    'transactions-index'     => 'Disse utgiftene, innskuddene og overføringene er ikke særlig fantasifulle. De har blitt generert automatisk.',
+    'piggy-banks-index'      => 'Som du kan se, finnes det tre spargriser. Bruk pluss- og minus-knappene til å påvirke mengden penger i hver sparegris. Klikk på navnet til spargrisen for å administrere hver spargris.',
+    'import-index'           => 'Alle CSV-filer kan importeres til Firefly III. Den støtter også import av data fra bunq og Spectre. Andre banker og finansielle aggregatorer vil bli implementert i fremtiden. Men som en demo-bruker kan du bare se en "falsk"-leverandør i aksjon. Den vil generere noen tilfeldige transaksjoner for å vise hvordan prosessen fungerer.',
+    'profile-index'          => 'Vær oppmerksom at demoen tilbakestilles hver fjerde time. Din tilgang kan trekkes tilbake når som helst. Dette skjer automatisk og er ikke en feil.',
+];
