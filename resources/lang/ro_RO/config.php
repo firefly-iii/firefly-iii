4dbc5f1413d07fc09ad3247e61cdabf938161c61
James Cole <thegrumpydictator@gmail.com>
2019-03-22 18:26:46 +0100

    Romanian strings.

---
+++ resources/lang/ro_RO/config.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * config.php
+ * Copyright (c) 2018 thegrumpydictator@gmail.com
+ *
+ * This file is part of Firefly III.
+ *
+ * Firefly III is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Firefly III is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Firefly III. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+return [
+    'html_language'         => 'ro',
+    'locale'                => 'ro, Română, ro_RO.utf8, ro_RO.UTF-8',
+    'month'                 => '%B %Y',
+    'month_and_day'         => '%B %e, %Y',
+    'month_and_date_day'    => '%A %B %e %Y',
+    'month_and_day_no_year' => '%B %e',
+    'date_time'             => '%B %e, %Y, @ %T',
+    'specific_day'          => '%e %B %Y',
+    'week_in_year'          => 'Săptămâna %W, %Y',
+    'year'                  => '%Y',
+    'half_year'             => '%B %Y',
+    'month_js'              => 'MMMM YYYY',
+    'month_and_day_js'      => 'MMMM Do, YYYY',
+    'date_time_js'          => 'MMMM Do, YYYY, @ HH:mm:ss',
+    'specific_day_js'       => 'D MMMM YYYY',
+    'week_in_year_js'       => '[Week] w, YYYY',
+    'year_js'               => 'YYYY',
+    'half_year_js'          => 'Q YYYY',
+    'dow_1'                 => 'Luni',
+    'dow_2'                 => 'Marţi',
+    'dow_3'                 => 'Miercuri',
+    'dow_4'                 => 'Joi',
+    'dow_5'                 => 'Vineri',
+    'dow_6'                 => 'Sâmbătă',
+    'dow_7'                 => 'Duminică',
+];
