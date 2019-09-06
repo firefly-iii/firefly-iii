c6c66b9dcb1ed727fa5b6e26fc7f729e406ded7f
James Cole <thegrumpydictator@gmail.com>
2019-09-06 20:26:13 +0200

    Introduce Greek.

---
+++ resources/lang/el_GR/config.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * config.php
+ * Copyright (c) 2018 thegrumpydictator@gmail.com
+ *
+ * This file is part of Firefly III.
+ *
+ * Firefly III is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Firefly III is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Firefly III. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+return [
+    'html_language'           => 'en',
+    'locale'                  => 'en, English, en_US.utf8, en_US.UTF-8',
+    'month'                   => '%B %Y',
+    'month_and_day'           => '%B %e, %Y',
+    'month_and_day_moment_js' => 'MMM H, EEEE',
+    'month_and_date_day'      => '%A %B %e, %Y',
+    'month_and_day_no_year'   => '%B %e',
+    'date_time'               => '%B %e, %Y, @ %T',
+    'specific_day'            => '%e %B %Y',
+    'week_in_year'            => 'Εβδομάδα %W, %Y',
+    'year'                    => '%Y',
+    'half_year'               => '%B %Y',
+    'month_js'                => 'ΜΜΜΜ ΕΕΕΕ',
+    'month_and_day_js'        => 'ΜΜΜΜ Ημ, ΕΕΕΕ',
+    'date_time_js'            => 'ΜΜΜΜ Ημ, ΕΕΕΕ, @ ΩΩ:mm:ss',
+    'specific_day_js'         => 'Η ΜΜΜΜ ΕΕΕΕ',
+    'week_in_year_js'         => '[Week] ε, ΕΕΕΕ',
+    'year_js'                 => 'ΕΕΕΕ',
+    'half_year_js'            => 'Τ ΕΕΕΕ',
+    'dow_1'                   => 'Δευτέρα',
+    'dow_2'                   => 'Τρίτη',
+    'dow_3'                   => 'Τετάρτη',
+    'dow_4'                   => 'Πέμπτη',
+    'dow_5'                   => 'Παρασκευή',
+    'dow_6'                   => 'Σάββατο',
+    'dow_7'                   => 'Κυριακή',
+];
---
+++ resources/lang/el_GR/passwords.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * passwords.php
+ * Copyright (c) 2018 thegrumpydictator@gmail.com
+ *
+ * This file is part of Firefly III.
+ *
+ * Firefly III is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Firefly III is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Firefly III. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+return [
+    'password' => 'Οι κωδικοί πρόσβασης πρέπει να είναι τουλάχιστον έξι χαρακτήρες και να ταιριάζουν με την επιβεβαίωση.',
+    'user'     => 'Δεν βρέθηκε χρήστης με αυτήν τη διεύθυνση e-mail.',
+    'token'    => 'Αυτό το διακριτικό επαναφοράς κωδικού πρόσβασης δεν είναι έγκυρο.',
+    'sent'     => 'Σας έχουμε στείλει e-mail με τον σύνδεσμο επαναφοράς του κωδικού πρόσβασης!',
+    'reset'    => 'Έχει γίνει επαναφορά του κωδικού σας!',
+    'blocked'  => 'Καλή προσπάθεια ωστόσο.',
+];
