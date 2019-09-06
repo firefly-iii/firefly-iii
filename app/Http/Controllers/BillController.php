07dd44af2d0910df753a718cf1f464b063471d56
James Cole <thegrumpydictator@gmail.com>
2019-09-06 06:24:25 +0200

    Fix #2543

--- app/Http/Controllers/BillController.php
+++ app/Http/Controllers/BillController.php
@@ -434,17 +434,22 @@ class BillController extends Controller
             if (false === $bill['active']) {
                 continue;
             }
+            if (0 === count($bill['pay_dates'])) {
+                continue;
+            }
             /** @var TransactionCurrency $currency */
-            $currencyId                   = $bill['currency_id'];
-            $sums[$currencyId]            = $sums[$currencyId] ?? [
+            $currencyId        = $bill['currency_id'];
+            $sums[$currencyId] = $sums[$currencyId] ?? [
                     'currency_id'             => $currencyId,
                     'currency_code'           => $bill['currency_code'],
                     'currency_name'           => $bill['currency_name'],
                     'currency_symbol'         => $bill['currency_symbol'],
                     'currency_decimal_places' => $bill['currency_decimal_places'],
-                    'avg'                 => '0',
+                    'avg'                     => '0',
                 ];
-            $avg                          = bcdiv(bcadd((string)$bill['amount_min'], (string)$bill['amount_max']), '2');
+
+            $avg                      = bcdiv(bcadd((string)$bill['amount_min'], (string)$bill['amount_max']), '2');
+            $avg                      = bcmul($avg, (string)count($bill['pay_dates']));
             $sums[$currencyId]['avg'] = bcadd($sums[$currencyId]['avg'], $avg);
         }
 
