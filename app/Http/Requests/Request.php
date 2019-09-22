firefly-iii/firefly-iii

commit f8cb5ca21c9a38666134966c59626c82be4a4eaf

Additional fix for #2629 with API
API object's notes fields now correctly preserve newlines.

app/Http/Requests/Request.php
@@ -238,6 +238,21 @@ class Request extends FormRequest
 
 
     /**
+     * Return string value, but keep newlines, or NULL if empty.
+     *
+     * @param string $field
+     *
+     * @return string
+     */
+    public function nullableNlString(string $field): string
+    {
+        if (!$this->has($field)) {
+            return null;
+        }
+        return app('steam')->nlCleanString((string)($this->get($field) ?? ''));
+    }
+
+    /**
      * Parse and clean a string.
      *
      * @param string|null $string

app/Api/V1/Requests/Request.php
@@ -65,7 +65,7 @@ class Request extends FireflyIIIRequest
             'opening_balance_date'    => $this->date('opening_balance_date'),
             'cc_type'                 => $this->string('credit_card_type'),
             'cc_Monthly_payment_date' => $this->string('monthly_payment_date'),
-            'notes'                   => $this->string('notes'),
+            'notes'                   => $this->nlString('notes'),
             'interest'                => $this->string('interest'),
             'interest_period'         => $this->string('interest_period'),
         ];

app/Api/V1/Requests/AccountUpdateRequest.php
@@ -76,7 +76,7 @@ class AccountUpdateRequest extends Request
             'opening_balance_date'    => $this->date('opening_balance_date'),
             'cc_type'                 => $this->nullableString('credit_card_type'),
             'cc_Monthly_payment_date' => $this->nullableString('monthly_payment_date'),
-            'notes'                   => $this->nullableString('notes'),
+            'notes'                   => $this->nullableNlString('notes'),
             'interest'                => $this->nullableString('interest'),
             'interest_period'         => $this->nullableString('interest_period'),
         ];

app/Api/V1/Requests/AttachmentStoreRequest.php
@@ -56,7 +56,7 @@ class AttachmentStoreRequest extends Request
         return [
             'filename' => $this->string('filename'),
             'title'    => $this->string('title'),
-            'notes'    => $this->string('notes'),
+            'notes'    => $this->nlString('notes'),
             'model'    => $this->string('model'),
             'model_id' => $this->integer('model_id'),
         ];

app/Api/V1/Requests/AttachmentUpdateRequest.php
@@ -51,7 +51,7 @@ class AttachmentUpdateRequest extends Request
         return [
             'filename' => $this->string('filename'),
             'title'    => $this->string('title'),
-            'notes'    => $this->string('notes'),
+            'notes'    => $this->nlString('notes'),
             'model'    => $this->string('model'),
             'model_id' => $this->integer('model_id'),
         ];

app/Api/V1/Requests/BillRequest.php
@@ -70,7 +70,7 @@ class BillRequest extends Request
             'repeat_freq'   => $this->string('repeat_freq'),
             'skip'          => $this->integer('skip'),
             'active'        => $active,
-            'notes'         => $this->string('notes'),
+            'notes'         => $this->nlString('notes'),
         ];
 
         return $data;

app/Api/V1/Requests/PiggyBankRequest.php
@@ -62,7 +62,7 @@ class PiggyBankRequest extends Request
             'current_amount' => $this->string('current_amount'),
             'startdate'      => $this->date('start_date'),
             'targetdate'     => $this->date('target_date'),
-            'notes'          => $this->string('notes'),
+            'notes'          => $this->nlString('notes'),
         ];
     }
 

app/Api/V1/Requests/RecurrenceUpdateRequest.php
@@ -69,7 +69,7 @@ class RecurrenceUpdateRequest extends Request
                 'title'             => $this->nullableString('title'),
                 'description'       => $this->nullableString('description'),
                 'first_date'        => $this->date('first_date'),
-                'notes'             => $this->nullableString('notes'),
+                'notes'             => $this->nullableNlString('notes'),
                 'repeat_until'      => $this->date('repeat_until'),
                 'nr_of_repetitions' => $this->nullableInteger('nr_of_repetitions'),
                 'apply_rules'       => $applyRules,

commit 61796552b1bc75aab3ab8d3562ee6f4db2e2d27b

Fixed definition of Request->nullableNlString
Typo introduced in f8cb5ca21c9a38666134966c59626c82be4a4eaf

app/Http/Requests/Request.php
@@ -244,7 +244,7 @@ class Request extends FormRequest
      *
      * @return string
      */
-    public function nullableNlString(string $field): string
+    public function nullableNlString(string $field): ?string
     {
         if (!$this->has($field)) {
             return null;