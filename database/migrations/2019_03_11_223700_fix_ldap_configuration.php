78481b8aa979cdcf9d67c53029fcd760a5484c58
James Cole <thegrumpydictator@gmail.com>
2019-10-02 07:00:58 +0200

    Update copyright of Firefly III to the GNU Affero General Public License as suggested by @nxxxse in #2607. This applies to all code in this commit from this moment onwards.

--- database/migrations/2019_03_11_223700_fix_ldap_configuration.php
+++ database/migrations/2019_03_11_223700_fix_ldap_configuration.php
@@ -22,26 +22,6 @@
 declare(strict_types=1);
 
 
-/**
- * 2019_03_11_223700_fix_ldap_configuration.php
- * Copyright (c) 2019 https://github.com/wrouesnel
- *
- * This file is part of Firefly III.
- *
- * Firefly III is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Firefly III is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Firefly III. If not, see <http://www.gnu.org/licenses/>.
- */
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
