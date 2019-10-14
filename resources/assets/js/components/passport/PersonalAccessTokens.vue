4d67b4d118dc1fbef8f34a31e20582a483a7490f
James Cole <thegrumpydictator@gmail.com>
2019-10-14 19:22:54 +0200

    iOS compatible fix #2734

--- resources/assets/js/components/passport/PersonalAccessTokens.vue
+++ resources/assets/js/components/passport/PersonalAccessTokens.vue
@@ -224,6 +224,8 @@
         methods: {
 
             copyToken() {
+                this.copyToClipboard(this.accessToken);
+                return;
                 console.log('in token thing');
                 if (!navigator.clipboard) {
                     console.log('in fallback');
@@ -256,6 +258,52 @@
                 }
 
                 document.body.removeChild(textArea);
+            },
+
+            copyToClipboard(string) {
+                let textarea;
+                let result;
+
+                try {
+                    textarea = document.createElement('textarea');
+                    textarea.setAttribute('readonly', true);
+                    textarea.setAttribute('contenteditable', true);
+                    textarea.style.position = 'fixed'; // prevent scroll from jumping to the bottom when focus is set.
+                    textarea.value = string;
+
+                    document.body.appendChild(textarea);
+
+                    textarea.focus();
+                    textarea.select();
+
+                    const range = document.createRange();
+                    range.selectNodeContents(textarea);
+
+                    const sel = window.getSelection();
+                    sel.removeAllRanges();
+                    sel.addRange(range);
+
+                    textarea.setSelectionRange(0, textarea.value.length);
+                    result = document.execCommand('copy');
+                } catch (err) {
+                    console.error(err);
+                    result = null;
+                } finally {
+                    document.body.removeChild(textarea);
+                }
+
+                // manual copy fallback using prompt
+                if (!result) {
+                    const isMac = navigator.platform.toUpperCase().indexOf('MAC') >= 0;
+                    const copyHotkey = isMac ? '⌘C' : 'CTRL+C';
+                    result = prompt(`Press ${copyHotkey}`, string); // eslint-disable-line no-alert
+                    if (!result) {
+                        $('#copyError').show();
+                        return false;
+                    }
+                }
+                $('#copySuccess').show();
+                return true;
             },
 
             /**
