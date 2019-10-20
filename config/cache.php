0135ae425f31da666abdd770c1648bf714587c48
James Cole <thegrumpydictator@gmail.com>
2019-10-20 16:02:30 +0200

    Fix #2754

--- config/cache.php
+++ config/cache.php
@@ -84,7 +84,7 @@ return [
             'servers'       => [
                 [
                     'host'   => env('MEMCACHED_HOST', '127.0.0.1'),
-                    'port'   => env('MEMCACHED_PORT', 11211),
+                    'port'   => (int)env('MEMCACHED_PORT', 11211),
                     'weight' => 100,
                 ],
             ],
