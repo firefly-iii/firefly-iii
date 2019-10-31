724a1b8b08f527c70618822d1c08361c2d3d441f
James Cole <thegrumpydictator@gmail.com>
2019-10-31 20:56:36 +0100

    Script things.

---
+++ .deploy/docker/travis.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo $TRAVIS_DIST
+echo '{"experimental":true}' | sudo tee /etc/docker/daemon.json
+mkdir $HOME/.docker
+touch $HOME/.docker/config.json
+echo '{"experimental":"enabled"}' | sudo tee $HOME/.docker/config.json
+sudo service docker restart
+docker version -f '{{.Server.Experimental}}'
+docker version
+
+# build everything
+.deploy/docker/build-amd64.sh
+.deploy/docker/build-arm.sh
+.deploy/docker/manifest.sh
