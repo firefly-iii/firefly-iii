33ce7c07672df24bbb7fde9cb288f0ff144399de
James Cole <thegrumpydictator@gmail.com>
2019-11-03 11:43:07 +0100

    Release first alpha version to see how Travis responds.

--- .deploy/docker/travis.sh
+++ .deploy/docker/travis.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "travis.sh: I am building channel ${CHANNEL} for version ${VERSION} on architecture ${ARCH}."
+echo "travis.sh: I am building channel ${CHANNEL} for version ${VERSION} on architecture ${ARCH}, branch $TRAVIS_BRANCH."
 
 echo '{"experimental":true}' | sudo tee /etc/docker/daemon.json
 mkdir $HOME/.docker
@@ -8,21 +8,19 @@ touch $HOME/.docker/config.json
 echo '{"experimental":"enabled"}' | sudo tee $HOME/.docker/config.json
 sudo service docker restart
 
-# build everything
-echo "Now building $ARCH"
-
 # First build amd64 image:
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 if [$ARCH == "arm"]; then
-  docker run --rm --privileged multiarch/qemu-user-static:register --reset
-
-  # get qemu-arm-static binary
-  mkdir tmp
-  pushd tmp && \
-  curl -L -o qemu-arm-static.tar.gz https://github.com/multiarch/qemu-user-static/releases/download/v2.6.0/qemu-arm-static.tar.gz && \
-  tar xzf qemu-arm-static.tar.gz && \
-  popd
+    echo "Because architecture is $ARCH running some extra commands."
+    docker run --rm --privileged multiarch/qemu-user-static:register --reset
+    
+    # get qemu-arm-static binary
+    mkdir tmp
+    pushd tmp && \
+    curl -L -o qemu-arm-static.tar.gz https://github.com/multiarch/qemu-user-static/releases/download/v2.6.0/qemu-arm-static.tar.gz && \
+    tar xzf qemu-arm-static.tar.gz && \
+    popd
 fi
 
 # if the github branch is develop, build and push develop. Don't push a version tag anymore.
