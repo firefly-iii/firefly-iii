58ec0c4add4cef2377bc34fa340a98a1db89556a
James Cole <thegrumpydictator@gmail.com>
2019-11-03 07:19:12 +0100

    Change some environment variables for easier handling.

--- .deploy/docker/travis.sh
+++ .deploy/docker/travis.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
-echo $TRAVIS_DIST
+echo "I am building channel ${CHANNEL} for version ${VERSION} on architecture ${ARCH}."
+
 echo '{"experimental":true}' | sudo tee /etc/docker/daemon.json
 mkdir $HOME/.docker
 touch $HOME/.docker/config.json
 echo '{"experimental":"enabled"}' | sudo tee $HOME/.docker/config.json
 sudo service docker restart
-docker version -f '{{.Server.Experimental}}'
-docker version
 
 # build everything
 .deploy/docker/build-$ARCH.sh
