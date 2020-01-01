414e8d7153254ff0bc4831e893bd19fd4579f862
James Cole <thegrumpydictator@gmail.com>
2020-01-01 14:27:08 +0100

    Refactor data method.

--- app/Api/V1/Requests/AccountStoreRequest.php
+++ app/Api/V1/Requests/AccountStoreRequest.php
@@ -47,6 +47,53 @@ class AccountStoreRequest extends Request
     }
 
     /**
+     * @return array
+     */
+    public function getAllAccountData(): array
+    {
+        $active          = true;
+        $includeNetWorth = true;
+        if (null !== $this->get('active')) {
+            $active = $this->boolean('active');
+        }
+        if (null !== $this->get('include_net_worth')) {
+            $includeNetWorth = $this->boolean('include_net_worth');
+        }
+        $data = [
+            'name'                    => $this->string('name'),
+            'active'                  => $active,
+            'include_net_worth'       => $includeNetWorth,
+            'account_type'            => $this->string('type'),
+            'account_type_id'         => null,
+            'currency_id'             => $this->integer('currency_id'),
+            'currency_code'           => $this->string('currency_code'),
+            'virtual_balance'         => $this->string('virtual_balance'),
+            'iban'                    => $this->string('iban'),
+            'BIC'                     => $this->string('bic'),
+            'account_number'          => $this->string('account_number'),
+            'account_role'            => $this->string('account_role'),
+            'opening_balance'         => $this->string('opening_balance'),
+            'opening_balance_date'    => $this->date('opening_balance_date'),
+            'cc_type'                 => $this->string('credit_card_type'),
+            'cc_Monthly_payment_date' => $this->string('monthly_payment_date'),
+            'notes'                   => $this->nlString('notes'),
+            'interest'                => $this->string('interest'),
+            'interest_period'         => $this->string('interest_period'),
+        ];
+        // append Location information.
+        $data = $this->appendLocationData($data);
+
+        if ('liability' === $data['account_type']) {
+            $data['opening_balance']      = bcmul($this->string('liability_amount'), '-1');
+            $data['opening_balance_date'] = $this->date('liability_start_date');
+            $data['account_type']         = $this->string('liability_type');
+            $data['account_type_id']      = null;
+        }
+
+        return $data;
+    }
+
+    /**
      * The rules that the incoming request must be matched against.
      *
      * @return array
