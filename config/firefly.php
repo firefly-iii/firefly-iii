a06b23debaa3c7dd15d406370e80ea97ac15ecd3
James Cole <james@firefly-iii.org>
2020-01-24 05:22:15 +0100

    New version.

--- config/firefly.php
+++ config/firefly.php
@@ -136,7 +136,7 @@ return [
         'export' => true,
     ],
     'encryption'             => null === env('USE_ENCRYPTION') || true === env('USE_ENCRYPTION'),
-    'version'                => '5.0.0',
+    'version'                => '5.0.1',
     'api_version'            => '1.0.0',
     'db_version'             => 12,
     'maxUploadSize'          => 15242880,
