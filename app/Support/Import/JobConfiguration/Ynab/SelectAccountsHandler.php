4958f6e4a6a27293ca8bb3bd28e95af0ee5fbfbb
James Cole <thegrumpydictator@gmail.com>
2020-02-16 13:56:52 +0100

    Update email address.

--- app/Support/Import/JobConfiguration/Ynab/SelectAccountsHandler.php
+++ app/Support/Import/JobConfiguration/Ynab/SelectAccountsHandler.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * SelectAccountsHandler.php
- * Copyright (c) 2019 thegrumpydictator@gmail.com
+ * Copyright (c) 2019 james@firefly-iii.org
  *
  * This file is part of Firefly III (https://github.com/firefly-iii).
  *
