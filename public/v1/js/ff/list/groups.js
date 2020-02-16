d0d5dea5ce669fd04fdcecbdeec2eab93bdf51c3
James Cole <thegrumpydictator@gmail.com>
2020-02-16 13:59:41 +0100

    Update email address.

--- public/v1/js/ff/list/groups.js
+++ public/v1/js/ff/list/groups.js
@@ -1,6 +1,6 @@
 /*
  * groups.js
- * Copyright (c) 2019 thegrumpydictator@gmail.com
+ * Copyright (c) 2019 james@firefly-iii.org
  *
  * This file is part of Firefly III (https://github.com/firefly-iii).
  *
