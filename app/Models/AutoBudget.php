81d7b7b6a16c613111545d256a1925398b0adbfe
James Cole <thegrumpydictator@gmail.com>
2020-03-13 21:15:21 +0100

    New model and migration.

---
+++ app/Models/AutoBudget.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * AutoBudget.php
+ * Copyright (c) 2020 thegrumpydictator@gmail.com
+ *
+ * This file is part of Firefly III (https://github.com/firefly-iii).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace FireflyIII;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * Class AutoBudget
+ */
+class AutoBudget extends Model
+{
+    /** @var int When the auto-budget resets every period automatically. */
+    public const AUTO_BUDGET_RESET = 1;
+    /** @var int When the auto-budget adds an amount every period automatically */
+    public const AUTO_BUDGET_ROLLOVER = 2;
+
+    use SoftDeletes;
+    //
+}
