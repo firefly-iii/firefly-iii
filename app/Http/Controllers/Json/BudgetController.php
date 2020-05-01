dba7d052965bda1edad290700d48b0be00181453
Lucas Guima <lucasguima.felipe@gmail.com>
2020-05-01 17:29:50 -0300

    Update the Budgets icon around the site

--- app/Http/Controllers/Json/BudgetController.php
+++ app/Http/Controllers/Json/BudgetController.php
@@ -63,7 +63,7 @@ class BudgetController extends Controller
         $this->middleware(
             function ($request, $next) {
                 app('view')->share('title', (string) trans('firefly.budgets'));
-                app('view')->share('mainTitleIcon', 'fa-tasks');
+                app('view')->share('mainTitleIcon', 'fa-pie-chart');
                 $this->repository         = app(BudgetRepositoryInterface::class);
                 $this->opsRepository      = app(OperationsRepositoryInterface::class);
                 $this->abRepository       = app(AvailableBudgetRepositoryInterface::class);
