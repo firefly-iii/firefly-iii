37b35661be67b0935cf03b4cf022c91acf905825
Florian Dupret <florian.dupret@gmail.com>
2020-05-25 19:24:21 +0200

    most email notifications translatable

--- app/Mail/AdminTestMail.php
+++ app/Mail/AdminTestMail.php
@@ -62,6 +62,6 @@ class AdminTestMail extends Mailable
     public function build(): self
     {
         return $this->view('emails.admin-test-html')->text('emails.admin-test-text')
-                    ->subject('A test message from your Firefly III installation');
+                    ->subject(trans('email.admin_test_subject'));
     }
 }
