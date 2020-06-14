0449430ec3230f97c06d6703cd733e553a2ce882
James Cole <thegrumpydictator@gmail.com>
2020-06-14 19:24:23 +0200

    I messed up

---
+++ frontend/src/bootstrap-basic.js
@@ -0,0 +1,7 @@
+
+// export jquery for others scripts to use
+window.$ = window.jQuery = require('jquery');
+
+// admin stuff
+require('bootstrap');
+
--- frontend/src/bootstrap.js
+++ frontend/src/bootstrap.js
@@ -1,8 +1,34 @@
 // imports
+import Vue from 'vue';
+import VueI18n from 'vue-i18n'
+import * as uiv from 'uiv';
 
 // export jquery for others scripts to use
 window.$ = window.jQuery = require('jquery');
 
+// axios
+window.axios = require('axios');
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+// CSRF
+let token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
+
 // admin stuff
+require('jquery-ui');
 require('bootstrap');
 
+require('./dist/js/adminlte');
+require('overlayscrollbars');
+
+// vue
+window.vuei18n = VueI18n;
+window.uiv =uiv;
+Vue.use(vuei18n);
+Vue.use(uiv);
+window.Vue = Vue;
