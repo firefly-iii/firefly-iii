62494e794324dcc4886453d77e46a55b3298bf32
James Cole <james@firefly-iii.org>
2020-07-31 19:18:40 +0200

    Fix format.

--- .ci/phpstan.sh
+++ .ci/phpstan.sh
@@ -4,6 +4,8 @@
 composer install --no-suggest --no-scripts --no-ansi &> /dev/null
 
 # Do static code analysis.
-./vendor/bin/phpstan analyse -c .ci/phpstan.neon --no-progress --errorFormat=raw > phpstan.txt
+./vendor/bin/phpstan analyse -c .ci/phpstan.neon --no-progress --error-format=raw > phpstan.txt
+
+cat phpstan.txt
 
 exit 0
