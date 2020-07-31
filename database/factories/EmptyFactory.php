7dcc76f217f2797b9850701c95bcd2796eb31650
James Cole <james@firefly-iii.org>
2020-07-31 07:28:59 +0200

    Add empty factory.

---
+++ database/factories/EmptyFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use Faker\Generator as Faker;
+use FireflyIII\Model;
+
+$factory->define(Model::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
