d2fd858d186a8fb143277b3495f0fe2ab4f206b4
James Cole <james@firefly-iii.org>
2020-07-31 19:15:26 +0200

    Analyse less code, optimise run.

--- .ci/phpstan.sh
+++ .ci/phpstan.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 # Install composer packages
-composer install --no-suggest --no-scripts --no-ansi
+composer install --no-suggest --no-scripts --no-ansi &> /dev/null
 
 # Do static code analysis.
-./vendor/bin/phpstan analyse -c .ci/phpstan.neon > phpstan.txt
+./vendor/bin/phpstan analyse -c .ci/phpstan.neon --no-progress --errorFormat=raw > phpstan.txt
+
+exit 0
