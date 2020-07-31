749236bc8dd166e7965d1923981fbe4757f1ef7b
James Cole <james@firefly-iii.org>
2020-07-31 19:10:02 +0200

    Push better PHPStan config.

--- .ci/phpstan.sh
+++ .ci/phpstan.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 
-FF_DIR=$PWD
+# Install composer packages
+composer install --no-suggest --no-scripts --no-ansi
 
-# single line install command
-composer global require hirak/prestissimo \
-                        --no-plugins --no-scripts
-
-cd $FF_DIR
-
-echo "Changed back to '$FF_DIR'"
-
-composer install
-./vendor/bin/phpstan analyse -c .ci/phpstan.neon
+# Do static code analysis.
+./vendor/bin/phpstan analyse -c .ci/phpstan.neon > phpstan.txt
