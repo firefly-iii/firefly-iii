fbd46c683eb553420e771bdbc521e225dd84e6bd
James Cole <james@firefly-iii.org>
2020-07-31 17:41:21 +0200

    Try again with local packages.

--- .ci/phpstan.sh
+++ .ci/phpstan.sh
@@ -4,12 +4,6 @@ echo $PWD
 
 # single line install command
 composer global require hirak/prestissimo \
-                        phpstan/phpstan \
-                        ergebnis/phpstan-rules \
-                        nunomaduro/larastan \
-                        phpstan/phpstan-deprecation-rules \
-                        thecodingmachine/phpstan-strict-rules \
-                        nette/coding-standard \
                         --no-plugins --no-scripts
 
-~/.config/composer/vendor/bin/phpstan analyse -c .ci/phpstan.neon
+./vendor/bin/phpstan analyse -c .ci/phpstan.neon
