3d5230228ed3bd09e27002b77e75778ed1402da6
James Cole <james@firefly-iii.org>
2020-08-01 05:49:45 +0200

    Fix env file, fix logging

--- .ci/phpstan.sh
+++ .ci/phpstan.sh
@@ -3,6 +3,8 @@
 # Install composer packages
 composer install --no-suggest --no-scripts --no-ansi
 
+# enable test .env file.
+cp .ci/.env.ci ../.env
 
 # Do static code analysis.
 ./vendor/bin/phpstan analyse -c .ci/phpstan.neon --no-progress
