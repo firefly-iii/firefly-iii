a2eedb3742f655a909a8f90ee9097065518526d6
James Cole <james@firefly-iii.org>
2020-10-04 09:19:36 +0200

    Fix #3898

--- app/Http/Requests/ObjectGroupFormRequest.php
+++ app/Http/Requests/ObjectGroupFormRequest.php
@@ -23,6 +23,7 @@ declare(strict_types=1);
 namespace FireflyIII\Http\Requests;
 
 use FireflyIII\Models\ObjectGroup;
+use FireflyIII\Support\Request\ConvertsDataTypes;
 use Illuminate\Foundation\Http\FormRequest;
 
 /**
@@ -30,6 +31,7 @@ use Illuminate\Foundation\Http\FormRequest;
  */
 class ObjectGroupFormRequest extends FormRequest
 {
+    use ConvertsDataTypes;
     /**
      * Verify the request.
      *
