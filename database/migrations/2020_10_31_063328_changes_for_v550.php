0104d46206655a03d0f0f0af58fb3e56b974cb09
James Cole <james@firefly-iii.org>
2020-10-31 06:47:36 +0100

    Update jobs table.

---
+++ database/migrations/2020_10_31_063328_changes_for_v550.php
@@ -0,0 +1,76 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * Class ChangesForV550
+ */
+class ChangesForV550 extends Migration
+{
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('jobs');
+        Schema::create(
+            'jobs',
+            static function (Blueprint $table) {
+                // straight from Laravel (this is the OLD table)
+                $table->bigIncrements('id');
+                $table->string('queue');
+                $table->longText('payload');
+                $table->tinyInteger('attempts')->unsigned();
+                $table->tinyInteger('reserved')->unsigned();
+                $table->unsignedInteger('reserved_at')->nullable();
+                $table->unsignedInteger('available_at');
+                $table->unsignedInteger('created_at');
+                $table->index(['queue', 'reserved', 'reserved_at']);
+            }
+        );
+        Schema::table(
+            'failed_jobs', function (Blueprint $table) {
+            $table->dropColumn('uuid');
+        }
+        );
+        Schema::dropIfExists('failed_jobs');
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::drop('jobs');
+        // this is the NEW table
+        Schema::create(
+            'jobs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('queue')->index();
+            $table->longText('payload');
+            $table->unsignedTinyInteger('attempts');
+            $table->unsignedInteger('reserved_at')->nullable();
+            $table->unsignedInteger('available_at');
+            $table->unsignedInteger('created_at');
+        }
+        );
+
+        Schema::create(
+            'failed_jobs', function (Blueprint $table) {
+            $table->id();
+            $table->string('uuid')->unique();
+            $table->text('connection');
+            $table->text('queue');
+            $table->longText('payload');
+            $table->longText('exception');
+            $table->timestamp('failed_at')->useCurrent();
+        }
+        );
+    }
+}
