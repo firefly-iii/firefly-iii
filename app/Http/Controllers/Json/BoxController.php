5dddb5e0df822fb055140389087a9388b51ef72e
Florian Dupret <florian.dupret@gmail.com>
2021-01-02 14:33:19 +0100

    Fix 'spent per day' budget box

--- app/Http/Controllers/Json/BoxController.php
+++ app/Http/Controllers/Json/BoxController.php
@@ -97,7 +97,8 @@ class BoxController extends Controller
         // also calculate spent per day.
         $spent       = $opsRepository->sumExpenses($start, $end, null, null, $currency);
         $spentAmount = $spent[(int) $currency->id]['sum'] ?? '0';
-        $days        = min( $today->diffInDays($start), $end->diffInDays($start) ) + 1;
+
+        $days = $today->between($start, $end) ? $today->diffInDays($start) + 1  : $end->diffInDays($start) + 1;
         $spentPerDay = bcdiv($spentAmount, (string) $days);
         if ($availableBudgets->count() > 0) {
             $display           = 0; // assume user overspent
