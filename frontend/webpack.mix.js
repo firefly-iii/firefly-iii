0dc3068f7f21b3ff65883606f4029479e28dce27
James Cole <james@firefly-iii.org>
2021-04-03 07:22:21 +0200

    Rebuild frontend and update packages.

--- frontend/webpack.mix.js
+++ frontend/webpack.mix.js
@@ -1,3 +1,23 @@
+/*
+ * webpack.mix.js
+ * Copyright (c) 2021 james@firefly-iii.org
+ *
+ * This file is part of Firefly III (https://github.com/firefly-iii).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 const mix = require('laravel-mix');
 
 /*
@@ -23,34 +43,33 @@ mix.webpackConfig({
                   });
 
 
-
-
-
 // dashboard and empty page
-mix.js('src/pages/dashboard.js', 'public/js').vue();
+mix.js('src/pages/dashboard.js', 'public/js').vue({version: 2});
 // accounts.
-mix.js('src/pages/accounts/index.js', 'public/js/accounts').vue();
-mix.js('src/pages/accounts/show.js', 'public/js/accounts').vue();
+mix.js('src/pages/accounts/index.js', 'public/js/accounts').vue({version: 2});
+mix.js('src/pages/accounts/show.js', 'public/js/accounts').vue({version: 2});
 
 
 // transactions.
-mix.js('src/pages/transactions/create.js', 'public/js/transactions')
-mix.js('src/pages/transactions/edit.js', 'public/js/transactions')
+mix.js('src/pages/transactions/create.js', 'public/js/transactions').vue({version: 2});
+mix.js('src/pages/transactions/edit.js', 'public/js/transactions').vue({version: 2});
 
 // static pages
-mix.js('src/pages/empty.js', 'public/js').vue();
-mix.js('src/pages/register.js', 'public/js')
+mix.js('src/pages/empty.js', 'public/js').vue({version: 2});
+mix.js('src/pages/register.js', 'public/js').vue({version: 2})
 
 
 mix.extract().sourceMaps();
 
-mix.sass('src/app.scss', 'public/css', [
-        //
-    ]);
+mix.sass('src/app.scss', 'public/css', {
+    sassOptions: {
+        outputStyle: 'compressed'
+    }
+});
 
 // move to right dir
-mix.copy('public/js','../public/v2/js')
-    .copy('fonts','../public/fonts')
-    .copy('images','../public/images')
-    .copy('public/css','../public/v2/css');
+mix.copy('public/js', '../public/v2/js')
+    .copy('fonts', '../public/fonts')
+    .copy('images', '../public/images')
+    .copy('public/css', '../public/v2/css');
 
