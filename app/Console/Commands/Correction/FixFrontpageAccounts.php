3184a8536e97018a6b0a59e2020f979c290cbc71
James Cole <james@firefly-iii.org>
2021-04-22 18:33:53 +0200

    Add another command.

---
+++ app/Console/Commands/Correction/FixFrontpageAccounts.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace FireflyIII\Console\Commands\Correction;
+
+use FireflyIII\Models\AccountType;
+use FireflyIII\Models\Preference;
+use FireflyIII\Repositories\Account\AccountRepositoryInterface;
+use FireflyIII\Support\Facades\Preferences;
+use FireflyIII\User;
+use Illuminate\Console\Command;
+
+/**
+ * Class FixFrontpageAccounts
+ */
+class FixFrontpageAccounts extends Command
+{
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Fixes a preference that may include deleted accounts or accounts of another type.';
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'firefly-iii:fix-frontpage-accounts';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle(): int
+    {
+        $users = User::get();
+        /** @var User $user */
+        foreach ($users as $user) {
+            $preference = Preferences::getForUser($user, 'frontPageAccounts', null);
+            if (null !== $preference) {
+                $this->fixPreference($preference);
+            }
+        }
+
+        return 0;
+    }
+
+    /**
+     * @param Preference $preference
+     */
+    private function fixPreference(Preference $preference): void
+    {
+        $fixed = [];
+        /** @var AccountRepositoryInterface $repository */
+        $repository = app(AccountRepositoryInterface::class);
+        if (null === $preference->user) {
+            return;
+        }
+        $repository->setUser($preference->user);
+        $data = $preference->data;
+        if (is_array($data)) {
+            /** @var string $accountId */
+            foreach ($data as $accountId) {
+                $accountId = (int)$accountId;
+                $account   = $repository->findNull($accountId);
+                if (null !== $account) {
+                    if (in_array($account->accountType->type, [AccountType::ASSET, AccountType::DEBT, AccountType::LOAN, AccountType::MORTGAGE], true)) {
+                        $fixed[] = $account->id;
+                        continue;
+                    }
+                }
+            }
+        }
+        Preferences::setForUser($preference->user, 'frontPageAccounts', $fixed);
+    }
+}
