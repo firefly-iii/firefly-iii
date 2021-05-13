7851bb476e8d702cee9c9eaad0b8250976f282aa
James Cole <james@firefly-iii.org>
2021-05-13 06:17:53 +0200

    Update some code.

--- app/Handlers/Events/UpdatedAccountEventHandler.php
+++ app/Handlers/Events/UpdatedAccountEventHandler.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 /*
  * UpdatedAccountEventHandler.php
  * Copyright (c) 2021 james@firefly-iii.org
--- app/Validation/Account/LiabilityValidation.php
+++ app/Validation/Account/LiabilityValidation.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 /*
  * LiabilityValidation.php
  * Copyright (c) 2021 james@firefly-iii.org
