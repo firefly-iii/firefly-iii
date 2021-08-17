716a90cf7ee48c3745eaa123ac90070f611010bb
James Cole <james@firefly-iii.org>
2021-08-17 17:07:51 +0200

    Expand show.

--- frontend/src/components/accounts/Show.vue
+++ frontend/src/components/accounts/Show.vue
@@ -20,13 +20,74 @@
 
 <template>
   <div>
-    I am a show
+    <div class="row">
+      <div class="col-lg-12 col-md-6 col-sm-12 col-xs-12">
+        <!-- Custom Tabs -->
+        <div class="card">
+          <div class="card-header d-flex p-0">
+            <h3 class="card-title p-3">Tabs</h3>
+            <ul class="nav nav-pills ml-auto p-2">
+              <li class="nav-item"><a class="nav-link active" href="#main_chart" data-toggle="tab">Chart</a></li>
+              <li class="nav-item"><a class="nav-link" href="#budgets" data-toggle="tab">Budgets</a></li>
+              <li class="nav-item"><a class="nav-link" href="#categories" data-toggle="tab">Categories</a></li>
+            </ul>
+          </div><!-- /.card-header -->
+          <div class="card-body">
+            <div class="tab-content">
+              <div class="tab-pane active" id="main_chart">
+                1: main chart
+              </div>
+              <!-- /.tab-pane -->
+              <div class="tab-pane" id="budgets">
+                2: tree map from/to budget
+              </div>
+              <!-- /.tab-pane -->
+              <div class="tab-pane" id="categories">
+                2: tree map from/to cat
+              </div>
+              <!-- /.tab-pane -->
+            </div>
+            <!-- /.tab-content -->
+          </div><!-- /.card-body -->
+        </div>
+        <!-- ./card -->
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-12 col-md-6 col-sm-12 col-xs-12">
+        <div class="card">
+          <div class="card-header">
+            <h3 class="card-title">
+              List
+            </h3>
+          </div>
+          <div class="card-body">
+            Transaction list
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-12 col-md-6 col-sm-12 col-xs-12">
+        <div class="card">
+          <div class="card-header">
+            <h3 class="card-title">
+              Blocks
+            </h3>
+          </div>
+          <div class="card-body">
+            Blocks
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Show"
+  name: "Show",
+
 }
 </script>
 
