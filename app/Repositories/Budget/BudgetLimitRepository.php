e73f04cf77be4a884de8931ba649b4f0244c5e7f
James Cole <james@firefly-iii.org>
2021-08-17 17:29:54 +0200

    Fix #4987

--- app/Repositories/Budget/BudgetLimitRepository.php
+++ app/Repositories/Budget/BudgetLimitRepository.php
@@ -88,6 +88,7 @@ class BudgetLimitRepository implements BudgetLimitRepositoryInterface
             )
             ->where('budget_limits.transaction_currency_id', $currency->id)
             ->whereNull('budgets.deleted_at')
+            ->where('budgets.active', true)
             ->where('budgets.user_id', $this->user->id);
         if (null !== $budgets && $budgets->count() > 0) {
             $query->whereIn('budget_limits.budget_id', $budgets->pluck('id')->toArray());
