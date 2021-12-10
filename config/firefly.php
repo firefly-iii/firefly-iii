5f777845a3e95abea5c420af8586f2522b5e7727
James Cole <james@firefly-iii.org>
2021-12-10 16:42:15 +0100

    Update version.

--- config/firefly.php
+++ config/firefly.php
@@ -101,7 +101,7 @@ return [
         'webhooks'     => true,
         'handle_debts' => true,
     ],
-    'version'                      => '5.6.5',
+    'version'                      => '5.6.6',
     'api_version'                  => '1.5.5',
     'db_version'                   => 18,
 
