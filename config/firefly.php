181e088ad56e49536b4319197f9bd70257995ef8
James Cole <james@firefly-iii.org>
2021-12-19 08:47:37 +0100

    Meta files for new release.

--- config/firefly.php
+++ config/firefly.php
@@ -101,7 +101,7 @@ return [
         'webhooks'     => true,
         'handle_debts' => true,
     ],
-    'version'                      => '5.6.7',
+    'version'                      => '5.6.8',
     'api_version'                  => '1.5.5',
     'db_version'                   => 18,
 
