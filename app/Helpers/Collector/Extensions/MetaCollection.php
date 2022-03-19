39eeea9c3cd62c43f0439a0ea9021b3bba1cf5b7
James Cole <james@firefly-iii.org>
2022-03-19 07:56:41 +0100

    Fix https://github.com/firefly-iii/firefly-iii/issues/5882

--- app/Helpers/Collector/Extensions/MetaCollection.php
+++ app/Helpers/Collector/Extensions/MetaCollection.php
@@ -226,7 +226,9 @@ trait MetaCollection
             $q1->where(function (Builder $q2) {
                 $q2->where('journal_meta.name', '=', 'external_url');
                 $q2->whereNull('journal_meta.data');
-            })->orWhereNull('journal_meta.name');
+            })->orWhere(function (Builder $q3) {
+                $q3->where('journal_meta.name', '!=', 'external_url');
+            });
         });
 
         return $this;
