9c19a08b173266529bb08dee50e58889b7ab0822
James Cole <james@firefly-iii.org>
2022-03-28 19:54:44 +0200

    Defaul empty stateful domains.

--- config/sanctum.php
+++ config/sanctum.php
@@ -14,15 +14,7 @@ return [
     |
     */
 
-    'stateful' => explode(
-        ',', env(
-               'SANCTUM_STATEFUL_DOMAINS', sprintf(
-                                             '%s%s',
-                                             'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
-                                             env('APP_URL') ? ',' . parse_url(env('APP_URL'), PHP_URL_HOST) : ''
-                                         )
-           )
-    ),
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS','')),
 
     /*
     |--------------------------------------------------------------------------
