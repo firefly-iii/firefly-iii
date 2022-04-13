342e9a263aad0fe1a8ef1dd520df52a605cec6ba
James Cole <james@firefly-iii.org>
2022-04-13 14:20:59 +0200

    Move constants to dedicated enum

--- app/Models/TransactionType.php
+++ app/Models/TransactionType.php
@@ -58,28 +58,12 @@ class TransactionType extends Model
 {
     use SoftDeletes;
 
-    /** @var string */
-    public const DEPOSIT = 'Deposit';
-    /** @var string */
-    public const INVALID = 'Invalid';
-    /** @var string */
-    public const LIABILITY_CREDIT = 'Liability credit';
-    /** @var string */
-    public const OPENING_BALANCE = 'Opening balance';
-    /** @var string */
-    public const RECONCILIATION = 'Reconciliation';
-    /** @var string */
-    public const TRANSFER = 'Transfer';
-    /** @var string */
-    public const WITHDRAWAL = 'Withdrawal';
-    /** @var string[] */
     protected $casts
         = [
             'created_at' => 'datetime',
             'updated_at' => 'datetime',
             'deleted_at' => 'datetime',
         ];
-    /** @var string[] */
     protected $fillable = ['type'];
 
     /**
