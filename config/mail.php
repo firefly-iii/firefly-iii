6c945bfd600c31bfbcf233a2472f154fb0e16304
James Cole <james@firefly-iii.org>
2022-05-31 05:56:38 +0200

    Fix https://github.com/firefly-iii/firefly-iii/issues/5996

--- config/mail.php
+++ config/mail.php
@@ -44,6 +44,7 @@ return [
             'username'   => env('MAIL_USERNAME'),
             'password'   => env('MAIL_PASSWORD'),
             'timeout'    => null,
+            'verify_peer' => null !== env('MAIL_ENCRYPTION')
         ],
 
         'ses' => [
