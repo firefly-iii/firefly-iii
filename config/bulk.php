ff91fe432c9c91aa0e2cc9ba9fee8a02a7efcb08
Jan Willhaus <mail@janwillhaus.de>
2022-10-01 22:39:25 +0200

    Make bulk rules clauses match bulk TransactionController params

--- config/bulk.php
+++ config/bulk.php
@@ -27,10 +27,10 @@ use FireflyIII\Enums\ClauseType;
 return [
     ClauseType::TRANSACTION => [
         ClauseType::WHERE  => [
-            'source_account_id' => 'required|numeric|belongsToUser:accounts,id',
+            'account_id' => 'required|numeric|belongsToUser:accounts,id',
         ],
         ClauseType::UPDATE => [
-            'destination_account_id' => 'required|numeric|belongsToUser:accounts,id',
+            'account_id' => 'required|numeric|belongsToUser:accounts,id',
         ],
     ],
 ];
