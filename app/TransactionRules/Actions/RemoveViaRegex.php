2ecf17061c407adc35c844d6d608b9088844e3d2
Blogshot <15980143+Blogshot@users.noreply.github.com>
2023-01-07 19:25:16 +0100

    Create RemoveViaRegex.php
    Implement an action to remove a string from a transaction's description via RegEx

---
+++ app/TransactionRules/Actions/RemoveViaRegex.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * RemoveViaRegex.php
+ * Copyright (c) 2023 sascha@knott.ac
+ *
+ * This file is part of Firefly III (https://github.com/firefly-iii).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+declare(strict_types=1);
+
+namespace FireflyIII\TransactionRules\Actions;
+
+use DB;
+use FireflyIII\Models\RuleAction;
+
+/**
+ * Class RemoveViaRegex.
+ */
+class RemoveViaRegex implements ActionInterface
+{
+    /** @var RuleAction The rule action */
+    private $action;
+
+    /**
+     * TriggerInterface constructor.
+     *
+     * @param RuleAction $action
+     */
+    public function __construct(RuleAction $action)
+    {
+        $this->action = $action;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function actOnArray(array $journal): bool
+    {
+        $action_value = $this->action->action_value
+        $before = $journal['description']
+
+        // remove via regex occurrence
+        $replace = '';
+        $after = preg_replace($action_value, $replace, $before);
+
+        DB::table('transaction_journals')->where('id', $journal['transaction_journal_id'])->limit(1)->update(['description' => $after]);
+
+        return true;
+    }
+}
