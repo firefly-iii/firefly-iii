46cb58fd05dc36c3d1c75ba4dd76c4f2fbbe430d
James Cole <james@firefly-iii.org>
2023-01-21 07:03:28 +0100

    Test PR
    OK

--- resources/lang/cs_CZ/api.php
+++ resources/lang/cs_CZ/api.php
@@ -14,6 +14,8 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
+ 
+ Test
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
