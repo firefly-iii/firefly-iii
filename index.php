125fed30ead7fcbf599f02e23e3257ab12719477
Tadek Chavez <mail@tadek.me>
2024-02-20 23:33:15 +1100

    testing repo connetivity

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * Adding line testing repo
  */
 
 declare(strict_types=1);
