e0a89bb5fe6cc1ff822eb0c8d6c2b4da13716227
James Cole <james@firefly-iii.org>
2024-02-26 20:08:14 +0100

    Rebuild JS

--- resources/assets/js/components/transactions/CreateTransaction.vue
+++ resources/assets/js/components/transactions/CreateTransaction.vue
@@ -746,12 +746,14 @@ export default {
                             case 'budget_id':
                             case 'bill_id':
                             case 'description':
-                            case 'type':
-                                this.transactions[transactionIndex].errors.source_account = [this.$t('firefly.select_source_account')];
-                                this.transactions[transactionIndex].errors.destination_account = [this.$t('firefly.select_dest_account')];
-                                break;
                             case 'tags':
                                 this.transactions[transactionIndex].errors[fieldName] = errors.errors[key];
+                                break;
+                            case 'type':
+                                if(errors.errors[key].length > 0) {
+                                    this.transactions[transactionIndex].errors.source_account = [this.$t('firefly.select_source_account')];
+                                    this.transactions[transactionIndex].errors.destination_account = [this.$t('firefly.select_dest_account')];
+                                }
                                 break;
                             case 'source_name':
                             case 'source_id':
