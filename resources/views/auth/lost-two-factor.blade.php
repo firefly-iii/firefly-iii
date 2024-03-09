84d3ad47641bb1e30b2d6cd2455855d4cb67d415
James Cole <james@firefly-iii.org>
2024-03-09 13:12:33 +0100

    Remove unused local files.

--- resources/views/auth/lost-two-factor.blade.php
+++ resources/views/auth/lost-two-factor.blade.php
@@ -1,17 +1,8 @@
 @extends('layout.v2.error')
+@section('status_code','')
+@section('status','2FA')
+@section('sub_title', __('firefly.two_factor_lost_header'))
 @section('content')
-<div class="row">
-    <div class="col">
-        <h1><a href="{{ route('index') }}"><strong>Firefly</strong> III</a></h1>
-    </div>
-</div>
-
-<div class="row">
-    <div class="col">
-        <h2>{{ __('firefly.two_factor_lost_header') }}</h2>
-    </div>
-</div>
-
 <div class="row">
     <div class="col">
         <p>
