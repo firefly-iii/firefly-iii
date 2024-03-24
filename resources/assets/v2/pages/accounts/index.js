bcfb134b6e1a8f15741ebecbdd2ff9c0e70c658b
James Cole <james@firefly-iii.org>
2024-03-24 11:43:37 +0100

    Clean up

--- resources/assets/v2/pages/accounts/index.js
+++ resources/assets/v2/pages/accounts/index.js
@@ -43,7 +43,6 @@ let sortDirection = '';
 const params = new Proxy(new URLSearchParams(window.location.search), {
     get: (searchParams, prop) => searchParams.get(prop),
 });
-// Get the value of "some_key" in eg "https://example.com/?some_key=some_value"
 sortingColumn = params.column ?? '';
 sortDirection = params.direction ?? '';
 
