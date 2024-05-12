e49dbefddd3fcb47e9b8cf25c67503d3d9f18753
James Cole <james@firefly-iii.org>
2024-05-12 08:10:12 +0200

    Make the combined combination unique.

--- database/migrations/2024_05_12_060551_create_account_balance_table.php
+++ database/migrations/2024_05_12_060551_create_account_balance_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -15,13 +14,13 @@ return new class extends Migration
             Schema::create('account_balances', function (Blueprint $table) {
                 $table->id();
                 $table->timestamps();
-                $table->string('title',100)->nullable();
+                $table->string('title', 100)->nullable();
                 $table->integer('account_id', false, true);
                 $table->integer('transaction_currency_id', false, true);
                 $table->decimal('balance', 32, 12);
                 $table->foreign('account_id')->references('id')->on('accounts')->onDelete('cascade');
                 $table->foreign('transaction_currency_id')->references('id')->on('transaction_currencies')->onDelete('cascade');
-                $table->unique(['account_id','transaction_currency_id'],'unique_account_currency');
+                $table->unique(['account_id', 'transaction_currency_id', 'title'], 'unique_account_currency');
             });
         }
     }
