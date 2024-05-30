9bab4b93bb0a133dd248689dff2e3218337d357e
Sylice <152557058+0xSylice@users.noreply.github.com>
2024-05-30 15:52:55 -0400

    Update AccountBalanceCalculator.php
    bcadd (string) fixes SQLite
    
    Signed-off-by: Sylice <152557058+0xSylice@users.noreply.github.com>

--- app/Support/Models/AccountBalanceCalculator.php
+++ app/Support/Models/AccountBalanceCalculator.php
@@ -120,13 +120,13 @@ class AccountBalanceCalculator
 
             // first create for normal currency:
             $entry               = $this->getAccountBalanceByAccount($account, $transactionCurrency);
-            $entry->balance      = bcadd($entry->balance, $sumAmount);
+            $entry->balance      = bcadd((string) $entry->balance, (string) $sumAmount);
             $entry->save();
 
             // then do foreign amount, if present:
             if ($foreignCurrency > 0) {
                 $entry          = $this->getAccountBalanceByAccount($account, $foreignCurrency);
-                $entry->balance = bcadd($entry->balance, $sumForeignAmount);
+                $entry->balance = bcadd((string) $entry->balance, (string) $sumForeignAmount);
                 $entry->save();
             }
         }
@@ -181,8 +181,8 @@ class AccountBalanceCalculator
             $journalId                               = (int) $row->id;
 
             // new amounts:
-            $amounts[$account][$transactionCurrency] = bcadd($amounts[$account][$transactionCurrency] ?? '0', $sumAmount ?? '0');
-            $amounts[$account][$foreignCurrency]     = bcadd($amounts[$account][$foreignCurrency] ?? '0', $sumForeignAmount ?? '0');
+            $amounts[$account][$transactionCurrency] = bcadd((string) $amounts[$account][$transactionCurrency] ?? '0', (string) $sumAmount ?? '0');
+            $amounts[$account][$foreignCurrency]     = bcadd((string) $amounts[$account][$foreignCurrency] ?? '0', (string) $sumForeignAmount ?? '0');
 
             // first create for normal currency:
             $entry                                   = self::getAccountBalanceByJournal('balance_after_journal', $account, $journalId, $transactionCurrency);
