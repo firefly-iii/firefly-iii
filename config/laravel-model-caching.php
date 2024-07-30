531a3a4b6c51ca4a0b779b8db805eb86f6bd1582
James Cole <james@firefly-iii.org>
2024-07-30 06:14:43 +0200

    Forced disable of model caching

--- config/laravel-model-caching.php
+++ config/laravel-model-caching.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 return [
     'cache-prefix'        => '',
-    'enabled'             => env('MODEL_CACHE_ENABLED', true),
+    'enabled'             => env('MODEL_CACHE_ENABLED', false),
     'use-database-keying' => env('MODEL_CACHE_USE_DATABASE_KEYING', true),
     'store'               => env('MODEL_CACHE_STORE'),
 ];
