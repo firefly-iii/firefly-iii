57b064f590a84a72d6400042037f92d4c8df1a0a
Sobuno <jesper@sobuno.dk>
2025-01-01 14:18:19 +0100

    Remove incorrect LICENSE message at top of file

--- tests/unit/Support/Search/AbstractQueryParserInterfaceParseQueryTest.php
+++ tests/unit/Support/Search/AbstractQueryParserInterfaceParseQueryTest.php
@@ -1,25 +1,5 @@
 <?php
 
-/*
- * NavigationAddPeriodTest.php
- * Copyright (c) 2023 Antonio Spinelli <https://github.com/tonicospinelli>
- *
- * This file is part of Firefly III (https://github.com/firefly-iii).
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 declare(strict_types=1);
 
 namespace Tests\unit\Support\Search;
