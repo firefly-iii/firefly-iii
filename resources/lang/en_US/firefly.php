f8ded668693c52e2b4ee234ceb18ce7f8df94146
James Cole <james@firefly-iii.org>
2025-02-01 12:53:34 +0100

    Fix https://github.com/firefly-iii/firefly-iii/issues/9721

--- resources/lang/en_US/firefly.php
+++ resources/lang/en_US/firefly.php
@@ -1466,7 +1466,7 @@ return [
     // exchange rates
     'menu_exchange_rates_index'                               => 'Exchange rates',
     'header_exchange_rates'                                   => 'Exchange rates',
-    'exchange_rates_intro'                                    => 'Firefly III supports downloading and using exchange rates. Read more about this in <a href="https://docs.firefly-iii.org/LOL_NOT_FINISHED_YET_TODO">the documentation</a>.',
+    'exchange_rates_intro'                                    => 'Firefly III supports downloading and using exchange rates. Read more about this in <a href="https://docs.firefly-iii.org/explanation/financial-concepts/exchange-rates/">the documentation</a>.',
     'exchange_rates_from_to'                                  => 'Between {from} and {to} (and the other way around)',
     'header_exchange_rates_rates'                             => 'Exchange rates',
     'exchange_rates_intro_rates'                              => 'Firefly III uses the following exchange rates. The inverse is automatically calculated when it is not provided. If no exchange rate exists for the date of the transaction, Firefly III will go back in time to find one. If none are present, the rate "1" will be used.',
