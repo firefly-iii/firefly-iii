72132a19b0b3a57416d9ce5ad13c188129c3fb2c
Sander Dorigo <git@sanderdorigo.nl>
2025-02-04 08:30:27 +0100

    Fix #9736

--- app/Http/Controllers/Account/ReconcileController.php
+++ app/Http/Controllers/Account/ReconcileController.php
@@ -113,7 +113,6 @@ class ReconcileController extends Controller
         $end->endOfDay();
 
         $startDate       = clone $start;
-        $startDate->subDay();
         $startBalance    = Steam::bcround(Steam::finalAccountBalance($account, $startDate)['balance'], $currency->decimal_places);
         $endBalance      = Steam::bcround(Steam::finalAccountBalance($account, $end)['balance'], $currency->decimal_places);
         $subTitleIcon    = config(sprintf('firefly.subIconsByIdentifier.%s', $account->accountType->type));
