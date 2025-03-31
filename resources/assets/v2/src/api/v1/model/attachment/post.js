3a43ce6546b5fa88655e51c2ff38e0388eb49abd
Sander Dorigo <git@sanderdorigo.nl>
2025-03-31 09:15:03 +0200

    ok final one

--- resources/assets/v2/src/api/v1/model/attachment/post.js
+++ resources/assets/v2/src/api/v1/model/attachment/post.js
@@ -18,7 +18,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-import {api} from "../../../boot/axios";
+import {api} from "../../../../boot/axios";
 
 export default class Post {
     post(fileName, attachableType, attachableId) {
