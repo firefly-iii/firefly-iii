5bd87036b0fae8c6a688e25d0c3bee6f15e564a0
James Cole <james@firefly-iii.org>
2025-06-01 13:32:18 +0200

    Config works now. Does not feel solved yet, but still.

--- resources/assets/v2/vite.config.js
+++ resources/assets/v2/vite.config.js
@@ -36,8 +36,8 @@ export default defineConfig(({command, mode, isSsrBuild, isPreview}) => {
     let https = null;
     if (command === 'serve') {
         https = {
-            key: fs.readFileSync(`/Users/sander/Sites/vm/tls-certificates/wildcard.sd.internal.key`),
-            cert: fs.readFileSync(`/Users/sander/Sites/vm/tls-certificates/wildcard.sd.internal.crt`),
+            key: fs.readFileSync(`/vagrant/tls-certificates/wildcard.sd.internal.key`),
+            cert: fs.readFileSync(`/vagrant/tls-certificates/wildcard.sd.internal.crt`),
         };
     }
 
@@ -83,7 +83,7 @@ export default defineConfig(({command, mode, isSsrBuild, isPreview}) => {
 
         server: {
             cors: true,
-            origin: 'https://localhost:5173',
+            origin: 'https://192.168.96.154:5173',
             watch: {
                 usePolling: true,
             },
