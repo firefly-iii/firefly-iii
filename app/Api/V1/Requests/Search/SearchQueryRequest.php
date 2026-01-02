54676715c02e4ae93110c943a6ed4dd951095945
James Cole <james@firefly-iii.org>
2026-01-02 16:38:46 +0100

    Create new request for search.

---
+++ app/Api/V1/Requests/Search/SearchQueryRequest.php
@@ -0,0 +1,50 @@
+<?php
+/*
+ * SearchQueryRequest.php
+ * Copyright (c) 2026 james@firefly-iii.org
+ *
+ * This file is part of Firefly III (https://github.com/firefly-iii).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace FireflyIII\Api\V1\Requests\Search;
+
+use FireflyIII\Api\V1\Requests\ApiRequest;
+use Illuminate\Contracts\Validation\Validator;
+
+class SearchQueryRequest extends ApiRequest
+{
+    public function rules(): array
+    {
+        return [
+            'query' => sprintf('min:0|max:500|%s', $this->required),
+        ];
+    }
+
+
+    public function withValidator(Validator $validator): void
+    {
+        $validator->after(
+            function (Validator $validator): void {
+                if ($validator->failed()) {
+                    return;
+                }
+                $query = $this->convertString('query');
+                $this->attributes->set('query', $query);
+            }
+        );
+    }
+
+}
---
+++ app/Api/V1/Requests/Search/TransactionSearchRequest.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ * SearchRequest.php
+ * Copyright (c) 2026 james@firefly-iii.org
+ *
+ * This file is part of Firefly III (https://github.com/firefly-iii).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace FireflyIII\Api\V1\Requests\Search;
+
+use FireflyIII\Api\V1\Requests\AggregateFormRequest;
+use FireflyIII\Api\V1\Requests\PaginationRequest;
+use FireflyIII\Models\TransactionJournal;
+
+class TransactionSearchRequest extends AggregateFormRequest
+{
+
+    #[\Override]
+    protected function getRequests(): array
+    {
+        return [
+            [PaginationRequest::class, 'sort_class' => TransactionJournal::class],
+            SearchQueryRequest::class,
+            // [ObjectTypeApiRequest::class, 'object_type' => Account::class],
+        ];
+    }
+}
