995f1c38cf00431af83e2b70ddadf211b95b8dd2
Diego Barreiro Perez <diego@barreiro.dev>
2026-02-14 16:45:11 +0000

    Add "Download selected" CSV export option to transaction bulk actions

--- routes/web.php
+++ routes/web.php
@@ -1320,6 +1320,7 @@ Route::group(
     static function (): void {
         Route::get('edit/{journalList?}', ['uses' => 'MassController@edit', 'as' => 'edit']);
         Route::get('delete/{journalList?}', ['uses' => 'MassController@delete', 'as' => 'delete']);
+        Route::get('download/{journalList?}', ['uses' => 'MassController@download', 'as' => 'download']);
         Route::post('update', ['uses' => 'MassController@update', 'as' => 'update']);
         Route::post('destroy', ['uses' => 'MassController@destroy', 'as' => 'destroy']);
     }
