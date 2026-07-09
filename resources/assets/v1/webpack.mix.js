83d2595ebcadbbdbd378d1e56052547abea75924
James Cole <james@firefly-iii.org>
2026-07-09 20:12:27 +0200

    Clean up dependencies for old v1 support code.

--- resources/assets/v1/webpack.mix.js
+++ resources/assets/v1/webpack.mix.js
@@ -24,7 +24,7 @@ let mix = require('laravel-mix');
 mix.webpackConfig({
     resolve: {
         alias: {
-            'vue$': 'vue/dist/vue.runtime.common.js'
+            // 'vue$': 'vue/dist/vue.runtime.common.js'
         },
         fallback: {
             "stream": require.resolve("stream-browserify"),
@@ -36,21 +36,21 @@ mix.webpackConfig({
 });
 
 mix.js('src/app.js', 'build');
-mix.js('src/app_vue.js', 'build').vue({version: 2});
+// mix.js('src/app_vue.js', 'build').vue({version: 2});
 // mix.js('src/create_transaction.js', 'build').vue({version: 2});
 // mix.js('src/edit_transaction.js', 'build').vue({version: 2});
-mix.js('src/profile.js', 'build').vue({version: 2});
+// mix.js('src/profile.js', 'build').vue({version: 2});
 //
 // // webhooks
 // mix.js('src/webhooks/index.js', 'build/webhooks').vue({version: 2});
-mix.js('src/webhooks/create.js', 'build/webhooks').vue({version: 2});
-mix.js('src/webhooks/edit.js', 'build/webhooks').vue({version: 2});
-mix.js('src/webhooks/show.js', 'build/webhooks').vue({version: 2}).copy('build','../../../public/v1/js')
+// mix.js('src/webhooks/create.js', 'build/webhooks').vue({version: 2});
+// mix.js('src/webhooks/edit.js', 'build/webhooks').vue({version: 2});
+// mix.js('src/webhooks/show.js', 'build/webhooks').vue({version: 2}).copy('build','../../../public/v1/js')
 
 // exchange rates
-mix.js('src/exchange-rates/index.js', 'build/exchange-rates').vue({version: 2});
-mix.js('src/exchange-rates/rates.js', 'build/exchange-rates').vue({version: 2});
+// mix.js('src/exchange-rates/index.js', 'build/exchange-rates').vue({version: 2});
+// mix.js('src/exchange-rates/rates.js', 'build/exchange-rates').vue({version: 2});
 
 // administrations
-mix.js('src/administrations/index.js', 'build/administrations').vue({version: 2});
-mix.js('src/administrations/edit.js', 'build/administrations').vue({version: 2});
+// mix.js('src/administrations/index.js', 'build/administrations').vue({version: 2});
+// mix.js('src/administrations/edit.js', 'build/administrations').vue({version: 2});
