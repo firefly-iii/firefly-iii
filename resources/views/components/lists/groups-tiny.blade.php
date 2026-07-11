0cd3068942e0de46ac9e19fb2bd3c01e51023779
James Cole <james@firefly-iii.org>
2026-07-11 19:52:34 +0200

    Add icon to list.

--- resources/views/components/lists/groups-tiny.blade.php
+++ resources/views/components/lists/groups-tiny.blade.php
@@ -1,14 +1,31 @@
-<div class="list-group">
+<table class="table">
     @foreach($transactions as $transaction)
-
-    <a class="list-group-item list-group-item-action d-flex justify-content-between align-items-center" href="{{ route('transactions.show', [$transaction['transaction_group_id']]) }}">
-        @if('' !== (string) $transaction['transaction_group_title'])
-        {{ $transaction['transaction_group_title'] }}:
-        @endif
-        {{ $transaction['description'] }}
-            <span class="small">
-                <x-generic.amount :transaction="$transaction" />
-            </span>
-    </a>
+<tr>
+    <td style="width:10%;">
+        <x-elements.transaction-type-icon :type="$transaction['transaction_type_type']" />
+    </td>
+    <td>
+        <a class="" href="{{ route('transactions.show', [$transaction['transaction_group_id']]) }}">
+            @if('' !== (string) $transaction['transaction_group_title'])
+                {{ $transaction['transaction_group_title'] }}:
+                @endif
+                {{ $transaction['description'] }}
+        </a>
+    </td>
+    <td class="text-end" style="width:30%;">
+        <span class="small">
+            <x-generic.amount :transaction="$transaction" />
+        </span>
+    </td>
+</tr>
     @endforeach
+
+
+
+        <span>
+
+
+
+    </a>
 </div>
+</table>
