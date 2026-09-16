10d215724a7e1a8b685ca0c3b63b0b57c178da82
James Cole <james@firefly-iii.org>
2026-09-16 21:26:24 +0200

    Smaller labels.

--- app/Api/V1/Controllers/Chart/BudgetController.php
+++ app/Api/V1/Controllers/Chart/BudgetController.php
@@ -320,12 +320,14 @@ final class BudgetController extends Controller
         // is always an array
         $return      = [];
         foreach ($rows as $row) {
+
+            $label = sprintf('%s (%s - %s)', $budget->name, $row['start']->isoFormat($this->monthAndDayFormat), $row['end']->isoFormat($this->monthAndDayFormat);
+            if($row['start']->isSameDay($start) && $row['end']->isSameDay($end)) {
+                $label = $budget->name;
+            }
+
             $current  = [
-                'label'                           => sprintf(
-                    '%s (%s - %s)',
-                    $budget->name,
-                    $row['start']->isoFormat($this->monthAndDayFormat),
-                    $row['end']->isoFormat($this->monthAndDayFormat)
+                'label'                           => $label,
                 ),
                 'currency_id'                     => (string) $row['currency_id'],
                 'currency_name'                   => $row['currency_name'],
