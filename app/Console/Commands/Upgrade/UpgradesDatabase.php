c61f8a79acdece935ef7757039eb344f27de81ad
Sander Dorigo <git@sanderdorigo.nl>
2026-09-21 09:17:52 +0200

    Add new command

--- app/Console/Commands/Upgrade/UpgradesDatabase.php
+++ app/Console/Commands/Upgrade/UpgradesDatabase.php
@@ -30,6 +30,7 @@ use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
 use Safe\Exceptions\InfoException;
 
+
 use function Safe\set_time_limit;
 
 try {
@@ -41,7 +42,7 @@ try {
 class UpgradesDatabase extends Command
 {
     use ShowsFriendlyMessages;
-
+    use GeneratesInstallationId;
     protected $description = 'Upgrades the database to the latest version.';
     protected $signature   = 'firefly-iii:upgrade-database {--F|force : Force all upgrades.}';
 
@@ -88,6 +89,7 @@ class UpgradesDatabase extends Command
         }
         // index will set FF3 version.
         AppConfiguration::set('ff3_build_time', (int) config('firefly.build_time'));
+        $this->generateInstallationId();
 
         return 0;
     }
